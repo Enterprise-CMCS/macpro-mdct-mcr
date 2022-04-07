225d262e3ea6d59a802c385ba1b27de5baba9c36
Braxton Excell <braxton.excell@gmail.com>
2022-04-07 12:58:33 -0500

    fix up cucumber config

--- tests/cypress/support/pages/LoginPage.js
+++ tests/cypress/support/pages/LoginPage.js
@@ -1,10 +1,8 @@
 const cognitoUserEmail = "//input[@name='email']";
 const cognitoUserPassword = "//input[@name='password']";
-const cognitoLoginButton =
-  "(//button[@class='chakra-button css-9n6wlp'])[2]";
+const cognitoLoginButton = "(//button[@class='chakra-button css-9n6wlp'])[2]";
 
 export class LoginPage {
-
   enterEmailwithCognitoLogin() {
     cy.xpath(cognitoUserEmail).type("stateuser1@test.com");
   }
@@ -13,35 +11,13 @@ export class LoginPage {
     cy.xpath(cognitoUserPassword).type("p@55W0rd!");
   }
 
-  clickcognitoLoginButton() {
-    cy.xpath(cognitoLoginButton).click();
-  }
-
-  enterEmailwithCognitoLogin() {
-    cy.xpath(cognitoUserEmail).type("stateuser2@test.com");
-  }
-
-  enterPasswordwithCognitoLogin() {
-    cy.xpath(cognitoUserPassword).type("p@55W0rd!");
-  }
-
-  clickcognitoLoginButton() {
-    cy.xpath(cognitoLoginButton).click();
-  }
-
-  loginasAStateUserWithCognito() {
+  stateUserCognitoLogin() {
     cy.xpath(cognitoUserEmail).type("stateuser1@test.com");
     cy.xpath(cognitoUserPassword).type("p@55W0rd!");
     cy.xpath(cognitoLoginButton).click();
   }
 
-  loginasAStateUserTwoWithCognito() {
-    cy.xpath(cognitoUserEmail).type("stateuser2@test.com");
-    cy.xpath(cognitoUserPassword).type("p@55W0rd!");
-    cy.xpath(cognitoLoginButton).click();
-  }
-
-  loginasApproverCognito() {
+  approverCognitoLogin() {
     cy.xpath(cognitoUserEmail).type("adminuser@test.com");
     cy.xpath(cognitoUserPassword).type("p@55W0rd!");
     cy.xpath(cognitoLoginButton).click();
--- tests/cypress/tests/a11y/commonPages.spec.js
+++ tests/cypress/tests/a11y/commonPages.spec.js
@@ -1,10 +1,10 @@
-describe("Check A11y on Common Pages", () => {
+describe("Baseline integration test (includes state user login)", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.login();
   });
 
-  it("Check a11y on Home Page", () => {
+  it("Check homepage a11y", () => {
     cy.checkA11yOfPage();
   });
 });
