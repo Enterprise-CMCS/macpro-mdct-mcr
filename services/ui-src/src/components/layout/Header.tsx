9eb6ed7c00619429689a74fe5e5725ec6abfa2ae
Garrett Rabian <gmr@ip-10-232-47-126.ec2.internal>
2022-04-22 16:10:10 -0400

    add header to roughly match figma

--- services/ui-src/src/components/layout/Header.tsx
+++ services/ui-src/src/components/layout/Header.tsx
@@ -1,41 +1,136 @@
 import { Link as RouterLink } from "react-router-dom";
 // components
-import { Box, Button, Container, Flex, Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+  Text,
+  useTheme,
+} from "@chakra-ui/react";
 import UsaBanner from "@cmsgov/design-system/dist/components/UsaBanner/UsaBanner";
 // assets
-import { FaQuestionCircle } from "react-icons/fa";
+import {
+  BsBoxArrowRight,
+  BsChevronDown,
+  BsFillPersonFill,
+  BsQuestionCircleFill,
+} from "react-icons/bs";
 import appLogo from "../../assets/logo_qmr.png";
 
 interface Props {
   handleLogout: () => void;
 }
 
-export const Header = ({ handleLogout }: Props) => (
-  <Box data-testid="header" zIndex={3}>
-    <UsaBanner />
-    <Box bg="palette.main">
-      <Container maxW="7xl">
-        <Flex py="4" alignItems="center">
-          <RouterLink to="/">
-            <img
-              src={appLogo}
-              alt="QMR Logo"
-              style={{ maxWidth: "100px" }}
-              data-testid="app-logo"
-            />
-          </RouterLink>
-          <Spacer flex={6} />
-          <Button variant="link" color="palette.white" onClick={handleLogout}>
-            Logout
-          </Button>
-          <RouterLink to="/faq" title="link to help page">
-            <FaQuestionCircle
-              color="palette.white"
-              style={{ fontSize: "1.4rem", margin: ".5rem" }}
-            />
-          </RouterLink>
-        </Flex>
-      </Container>
+export const Header = ({ handleLogout }: Props) => {
+  const theme = useTheme();
+  return (
+    <Box data-testid="header" zIndex={3}>
+      <UsaBanner />
+      <Box bg="palette.main_darker">
+        <Container maxW="7xl">
+          <Flex py="4" alignItems="center">
+            <RouterLink to="/" style={{ textDecoration: "none" }}>
+              <img
+                src={appLogo}
+                alt="QMR Logo"
+                style={{ maxWidth: "100px" }}
+                data-testid="app-logo"
+              />
+            </RouterLink>
+            <Spacer flex={6} />
+            <RouterLink
+              to="/faq"
+              title="link to help page"
+              style={{ textDecoration: "none" }}
+            >
+              <Flex align="center">
+                <BsQuestionCircleFill
+                  color={theme.colors.palette.white}
+                  style={{ fontSize: "1.4rem", margin: ".5rem" }}
+                />
+                <Text color="palette.white" fontWeight="bold">
+                  Get Help
+                </Text>
+              </Flex>
+            </RouterLink>
+            <Menu>
+              <MenuButton
+                bg="palette.main_darker"
+                color="palette.white"
+                fontWeight="bold"
+                as={Button}
+                rightIcon={<BsChevronDown />}
+                _hover={{ bg: "red" }}
+                borderRadius="0px"
+                p="0px"
+              >
+                <Flex align="center">
+                  <BsFillPersonFill
+                    color={theme.colors.palette.white}
+                    style={{ fontSize: "1.4rem", margin: ".5rem" }}
+                  />
+                  <Text color="palette.white" fontWeight="bold">
+                    User
+                  </Text>
+                </Flex>
+              </MenuButton>
+              <MenuList bg="palette.main_darker">
+                <MenuItem _focus={{ bg: "red" }}>
+                  <RouterLink
+                    to="/faq"
+                    title="link to help page"
+                    style={{ textDecoration: "none" }}
+                  >
+                    <Flex align="center">
+                      <BsQuestionCircleFill
+                        color={theme.colors.palette.white}
+                        style={{ fontSize: "1.4rem", margin: ".5rem" }}
+                      />
+                      <Text color="palette.white" fontWeight="bold">
+                        Get Help
+                      </Text>
+                    </Flex>
+                  </RouterLink>
+                </MenuItem>
+                <MenuItem _focus={{ bg: "red" }}>
+                  <RouterLink
+                    to="/acct"
+                    title="link to account page"
+                    style={{ textDecoration: "none" }}
+                  >
+                    <Flex align="center">
+                      <BsFillPersonFill
+                        color={theme.colors.palette.white}
+                        style={{ fontSize: "1.4rem", margin: ".5rem" }}
+                      />
+                      <Text color="palette.white" fontWeight="bold">
+                        Manage Account
+                      </Text>
+                    </Flex>
+                  </RouterLink>
+                </MenuItem>
+                <MenuItem _focus={{ bg: "red" }} onClick={handleLogout}>
+                  <Flex align="center">
+                    <BsBoxArrowRight
+                      color={theme.colors.palette.white}
+                      style={{ fontSize: "1.4rem", margin: ".5rem" }}
+                    />
+                    <Text color="palette.white" fontWeight="bold">
+                      Log Out
+                    </Text>
+                  </Flex>
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Container>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
