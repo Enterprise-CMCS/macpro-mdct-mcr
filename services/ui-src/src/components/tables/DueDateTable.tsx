3725e4a67ab70c46b3e4275067e6f0d1b4522131
Garrett Rabian <garrett@coforma.io>
2022-05-04 16:10:16 -0400

    fix table header cap and table container resizing

--- services/ui-src/src/components/tables/DueDateTable.tsx
+++ services/ui-src/src/components/tables/DueDateTable.tsx
@@ -1,13 +1,5 @@
 // components
-import {
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
+import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 // assets
 import MCPARDueDatesData from "./data/MCPARDueDates.json";
 
@@ -18,61 +10,59 @@ const rowMap: { [templateName: string]: any } = {
 export const DueDateTable = ({ templateName }: Props) => {
   const tableValues = rowMap[templateName];
   return (
-    <TableContainer data-testid="due-date-table">
-      <Table variant="striped" size="sm">
-        <Thead>
-          <Tr>
-            <Th sx={sx.tableHeader}>{tableValues.header.columnOneTitle}</Th>
-            <Th sx={sx.tableHeader}>{tableValues.header.columnTwoTitle}</Th>
-            <Th sx={sx.tableHeader}>{tableValues.header.columnThreeTitle}</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td sx={sx.tableData}>{tableValues.bodyRowOne.columnOneValue}</Td>
-            <Td sx={sx.tableData}>{tableValues.bodyRowOne.columnTwoValue}</Td>
-            <Td sx={sx.tableDataDueDate}>
-              {tableValues.bodyRowOne.columnThreeValue}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td sx={sx.tableData}>{tableValues.bodyRowTwo.columnOneValue}</Td>
-            <Td sx={sx.tableData}>{tableValues.bodyRowTwo.columnTwoValue}</Td>
-            <Td sx={sx.tableDataDueDate}>
-              {tableValues.bodyRowTwo.columnThreeValue}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td sx={sx.tableData}>{tableValues.bodyRowThree.columnOneValue}</Td>
-            <Td sx={sx.tableData}>{tableValues.bodyRowThree.columnTwoValue}</Td>
-            <Td sx={sx.tableDataDueDate}>
-              {tableValues.bodyRowThree.columnThreeValue}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td sx={sx.tableData}>{tableValues.bodyRowFour.columnOneValue}</Td>
-            <Td sx={sx.tableData}>{tableValues.bodyRowFour.columnTwoValue}</Td>
-            <Td sx={sx.tableDataDueDate}>
-              {tableValues.bodyRowFour.columnThreeValue}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td sx={sx.tableData}>{tableValues.bodyRowFive.columnOneValue}</Td>
-            <Td sx={sx.tableData}>{tableValues.bodyRowFive.columnTwoValue}</Td>
-            <Td sx={sx.tableDataDueDate}>
-              {tableValues.bodyRowFive.columnThreeValue}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td sx={sx.tableData}>{tableValues.bodyRowSix.columnOneValue}</Td>
-            <Td sx={sx.tableData}>{tableValues.bodyRowSix.columnTwoValue}</Td>
-            <Td sx={sx.tableDataDueDate}>
-              {tableValues.bodyRowSix.columnThreeValue}
-            </Td>
-          </Tr>
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <Table variant="striped" size="sm" data-testid="due-date-table">
+      <Thead>
+        <Tr>
+          <Th sx={sx.tableHeader}>{tableValues.header.columnOneTitle}</Th>
+          <Th sx={sx.tableHeader}>{tableValues.header.columnTwoTitle}</Th>
+          <Th sx={sx.tableHeader}>{tableValues.header.columnThreeTitle}</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        <Tr>
+          <Td sx={sx.tableData}>{tableValues.bodyRowOne.columnOneValue}</Td>
+          <Td sx={sx.tableData}>{tableValues.bodyRowOne.columnTwoValue}</Td>
+          <Td sx={sx.tableDataDueDate}>
+            {tableValues.bodyRowOne.columnThreeValue}
+          </Td>
+        </Tr>
+        <Tr>
+          <Td sx={sx.tableData}>{tableValues.bodyRowTwo.columnOneValue}</Td>
+          <Td sx={sx.tableData}>{tableValues.bodyRowTwo.columnTwoValue}</Td>
+          <Td sx={sx.tableDataDueDate}>
+            {tableValues.bodyRowTwo.columnThreeValue}
+          </Td>
+        </Tr>
+        <Tr>
+          <Td sx={sx.tableData}>{tableValues.bodyRowThree.columnOneValue}</Td>
+          <Td sx={sx.tableData}>{tableValues.bodyRowThree.columnTwoValue}</Td>
+          <Td sx={sx.tableDataDueDate}>
+            {tableValues.bodyRowThree.columnThreeValue}
+          </Td>
+        </Tr>
+        <Tr>
+          <Td sx={sx.tableData}>{tableValues.bodyRowFour.columnOneValue}</Td>
+          <Td sx={sx.tableData}>{tableValues.bodyRowFour.columnTwoValue}</Td>
+          <Td sx={sx.tableDataDueDate}>
+            {tableValues.bodyRowFour.columnThreeValue}
+          </Td>
+        </Tr>
+        <Tr>
+          <Td sx={sx.tableData}>{tableValues.bodyRowFive.columnOneValue}</Td>
+          <Td sx={sx.tableData}>{tableValues.bodyRowFive.columnTwoValue}</Td>
+          <Td sx={sx.tableDataDueDate}>
+            {tableValues.bodyRowFive.columnThreeValue}
+          </Td>
+        </Tr>
+        <Tr>
+          <Td sx={sx.tableData}>{tableValues.bodyRowSix.columnOneValue}</Td>
+          <Td sx={sx.tableData}>{tableValues.bodyRowSix.columnTwoValue}</Td>
+          <Td sx={sx.tableDataDueDate}>
+            {tableValues.bodyRowSix.columnThreeValue}
+          </Td>
+        </Tr>
+      </Tbody>
+    </Table>
   );
 };
 
@@ -85,6 +75,7 @@ const sx = {
     color: "black",
     fontWeight: "semibold",
     fontSize: "sm",
+    textTransform: "none",
   },
   tableData: {
     color: "black",
