aa5dea791ce92f405fd05bdec81920886fa712ab
Braxton Excell <braxton.excell@gmail.com>
2022-05-12 16:06:14 -0500

    add the necessary window object mock

--- services/ui-src/src/utils/testing/setupJest.tsx
+++ services/ui-src/src/utils/testing/setupJest.tsx
@@ -5,6 +5,25 @@ import "jest-axe/extend-expect";
 
 global.React = React;
 
+/*
+ * Mocks window.matchMedia
+ * https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+ */
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useParams: () => ({
