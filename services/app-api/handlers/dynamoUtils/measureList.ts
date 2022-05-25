2f758de94572d48082b03af5c52b60fd64ba9971
Garrett Rabian <garrett@coforma.io>
2022-05-25 12:21:47 -0600

    remove unused files

--- services/app-api/handlers/dynamoUtils/measureList.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-interface Measure {
-  [year: number]: MeasureMetaData[];
-}
-
-export interface MeasureMetaData {
-  description: string;
-  title: string;
-}
