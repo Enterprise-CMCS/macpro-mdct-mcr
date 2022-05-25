b5024e6447bfc939ccf3e8b4066d9d2f822b6690
Garrett Rabian <garrett@coforma.io>
2022-05-25 15:24:23 -0600

    fix profile tests

--- services/ui-src/src/views/Profile/Profile.tsx
+++ services/ui-src/src/views/Profile/Profile.tsx
@@ -16,43 +16,45 @@ export const Profile = () => {
   const { email, given_name, family_name, userRole, state } = userDetails();
   const navigate = useNavigate();
   return (
-    <Flex sx={sx.root} data-testid="profile">
-      <Heading as="h1" size="xl" sx={sx.heading}>
-        Account Info
-      </Heading>
-      <Flex sx={sx.variantRow}>
-        <Text sx={sx.fieldName}>Email</Text>
-        <Text>{email}</Text>
-      </Flex>
-      <Flex>
-        <Text sx={sx.fieldName}>First Name</Text>
-        <Text>{given_name}</Text>
-      </Flex>
-      <Flex sx={sx.variantRow}>
-        <Text sx={sx.fieldName}>Last Name</Text>
-        <Text>{family_name}</Text>
-      </Flex>
-      <Flex>
-        <Text sx={sx.fieldName}>Role</Text>
-        <Text>{userRole}</Text>
-      </Flex>
-      {state && (
+    <>
+      <Flex sx={sx.root} data-testid="profile">
+        <Heading as="h1" size="xl" sx={sx.heading}>
+          Account Info
+        </Heading>
         <Flex sx={sx.variantRow}>
-          <Text sx={sx.fieldName} data-testid="statetestid">
-            State
-          </Text>
-          <Text>{state}</Text>
+          <Text sx={sx.fieldName}>Email</Text>
+          <Text>{email}</Text>
         </Flex>
-      )}
-      <Button
-        colorScheme="colorSchemes.main"
-        data-testid="admin-button"
-        sx={sx.adminButton}
-        onClick={() => navigate("/admin")}
-      >
-        Banner editor
-      </Button>
-    </Flex>
+        <Flex>
+          <Text sx={sx.fieldName}>First Name</Text>
+          <Text>{given_name}</Text>
+        </Flex>
+        <Flex sx={sx.variantRow}>
+          <Text sx={sx.fieldName}>Last Name</Text>
+          <Text>{family_name}</Text>
+        </Flex>
+        <Flex>
+          <Text sx={sx.fieldName}>Role</Text>
+          <Text>{userRole}</Text>
+        </Flex>
+        {state && (
+          <Flex sx={sx.variantRow}>
+            <Text sx={sx.fieldName} data-testid="statetestid">
+              State
+            </Text>
+            <Text>{state}</Text>
+          </Flex>
+        )}
+        <Button
+          colorScheme="colorSchemes.main"
+          data-testid="admin-button"
+          sx={sx.adminButton}
+          onClick={() => navigate("/admin")}
+        >
+          Banner editor
+        </Button>
+      </Flex>
+    </>
   );
 };
 
