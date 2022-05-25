73cdec4038b774fb03d241e2b8c2a990e54625c8
Garrett Rabian <garrett@coforma.io>
2022-05-25 11:28:03 -0600

    log cleanup

--- services/app-api/handlers/banners/get.ts
+++ services/app-api/handlers/banners/get.ts
@@ -3,15 +3,12 @@ import dynamoDb from "../../utils/dynamo/dynamodb-lib";
 import { StatusCodes } from "../../utils/types/types";
 
 export const getBanner = handler(async (event, _context) => {
-  console.log("in getBanner handler"); // eslint-disable-line
   const params = {
     TableName: process.env.BANNER_TABLE_NAME!,
     Key: {
       key: event?.pathParameters?.bannerId!,
     },
   };
-  console.log("getBanner params:", params); // eslint-disable-line
   const queryResponse = await dynamoDb.get(params);
-  console.log("query response from dynamodb get", queryResponse); // eslint-disable-line
   return { status: StatusCodes.SUCCESS, body: queryResponse };
 });
--- services/app-api/utils/auth/authorization.ts
+++ services/app-api/utils/auth/authorization.ts
@@ -11,17 +11,10 @@ interface DecodedToken {
 }
 
 const loadCognitoValues = async () => {
-  console.log("trying to load cognito values"); // eslint-disable-line
   if (
     process.env.COGNITO_USER_POOL_ID &&
     process.env.COGNITO_USER_POOL_CLIENT_ID
   ) {
-    // eslint-disable-next-line
-    console.log(
-      "env vars not empty: ",
-      process.env.COGNITO_USER_POOL_ID,
-      process.env.COGNITO_USER_POOL_CLIENT_ID
-    ); // eslint-disable-line
     return {
       userPoolId: process.env.COGNITO_USER_POOL_ID,
       userPoolClientId: process.env.COGNITO_USER_POOL_CLIENT_ID,
@@ -32,23 +25,6 @@ const loadCognitoValues = async () => {
     const userPoolIdParamName = "/" + stage + "/ui-auth/cognito_user_pool_id";
     const userPoolClientIdParamName =
       "/" + stage + "/ui-auth/cognito_user_pool_client_id";
-    // eslint-disable-next-line
-    console.log(
-      "cognito param names:",
-      userPoolIdParamName,
-      userPoolClientIdParamName
-    );
-    // eslint-disable-next-line
-    console.log(
-      "trim and standard match values",
-      userPoolIdParamName,
-      userPoolIdParamName.trim()
-    );
-    // eslint-disable-next-line
-    console.log(
-      "trim and standard match bool",
-      userPoolIdParamName === userPoolIdParamName.trim()
-    );
     const userPoolIdParams = {
       Name: userPoolIdParamName,
     };
@@ -59,13 +35,6 @@ const loadCognitoValues = async () => {
     const userPoolClientId = await ssm
       .getParameter(userPoolClientIdParams)
       .promise();
-    console.log("fetched both params", userPoolClientId, userPoolId); // eslint-disable-line
-    // eslint-disable-next-line
-    console.log(
-      "values: ",
-      userPoolId.Parameter?.Value,
-      userPoolClientId.Parameter?.Value
-    );
     if (userPoolId.Parameter?.Value && userPoolClientId.Parameter?.Value) {
       process.env["COGNITO_USER_POOL_ID"] = userPoolId.Parameter?.Value;
       process.env["COGNITO_USER_POOL_CLIENT_ID"] =
@@ -75,7 +44,6 @@ const loadCognitoValues = async () => {
         userPoolClientId: userPoolClientId.Parameter.Value,
       };
     } else {
-      console.log("ssm didn't work. throwing error"); // eslint-disable-line
       throw new Error("cannot load cognito values");
     }
   }
@@ -83,7 +51,6 @@ const loadCognitoValues = async () => {
 
 export const isAuthorized = async (event: APIGatewayProxyEvent) => {
   const cognitoValues = await loadCognitoValues();
-  console.log("got cognito values", cognitoValues); // eslint-disable-line
   // Verifier that expects valid access tokens:
   const verifier = CognitoJwtVerifier.create({
     userPoolId: cognitoValues.userPoolId,
