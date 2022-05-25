a43819fab5ab5b7de9b184839507b6ae41ae5d52
Garrett Rabian <garrett@coforma.io>
2022-05-25 09:23:20 -0600

    logs

--- services/app-api/handlers/handler-lib.ts
+++ services/app-api/handlers/handler-lib.ts
@@ -18,20 +18,27 @@ export default function handler(lambda: LambdaFunction) {
     // Start debugger
     debug.init(event, context);
 
+    console.log("in handler lib. event: ", event); // eslint-disable-line
+
     if (await isAuthorized(event)) {
+      console.log("event is authorized. Trying to invoke lambda"); // eslint-disable-line
       try {
         // Run the Lambda
         const { status, body } = await lambda(event, context);
+        console.log("lambda finished. status and body: ", status, body); // eslint-disable-line
         return buildResponse(status, body);
       } catch (e: any) {
         // Print debug messages
         debug.flush(e);
+
+        console.log("lambda threw exception:", e.message); // eslint-disable-line
 
         const body = { error: e.message };
         return internalServerError(body);
       }
     } else {
       const body = { error: UNAUTHORIZED_MESSAGE };
+      console.log("lambda unauthorized", body); // eslint-disable-line
       return buildResponse(StatusCodes.UNAUTHORIZED, body);
     }
   };
--- services/app-api/utils/auth/authorization.ts
+++ services/app-api/utils/auth/authorization.ts
@@ -11,16 +11,29 @@ interface DecodedToken {
 }
 
 const loadCognitoValues = async () => {
+  console.log("trying to load cognito values"); // eslint-disable-line
   if (
     process.env.COGNITO_USER_POOL_ID &&
     process.env.COGNITO_USER_POOL_CLIENT_ID
   ) {
+    // eslint-disable-next-line
+    console.log(
+      "env vars not empty: ",
+      process.env.COGNITO_USER_POOL_ID,
+      process.env.COGNITO_USER_POOL_CLIENT_ID
+    ); // eslint-disable-line
     return {
       userPoolId: process.env.COGNITO_USER_POOL_ID,
       userPoolClientId: process.env.COGNITO_USER_POOL_CLIENT_ID,
     };
   } else {
+    console.log("trying ssm"); // eslint-disable-line
     const ssm = new SSM();
+    console.log("ssm inited", ssm); // eslint-disable-line
+    // eslint-disable-next-line
+    console.log(
+      "check string interpolation ${process.env.STAGE!}/ui-auth/cognito_user_pool_id"
+    );
     const userPoolIdParams = {
       Name: "${process.env.STAGE!}/ui-auth/cognito_user_pool_id",
     };
@@ -31,19 +44,29 @@ const loadCognitoValues = async () => {
     const userPoolClientId = await ssm
       .getParameter(userPoolClientIdParams)
       .promise();
+    console.log("fetched both params", userPoolClientId, userPoolId); // eslint-disable-line
+    // eslint-disable-next-line
+    console.log(
+      "values: ",
+      userPoolId.Parameter?.Value,
+      userPoolClientId.Parameter?.Value
+    ); // eslint-disable-line
     if (userPoolId.Parameter?.Value && userPoolClientId.Parameter?.Value) {
       return {
         userPoolId: userPoolId.Parameter.Value,
         userPoolClientId: userPoolClientId.Parameter.Value,
       };
     } else {
+      console.log("ssm didn't work. throwing error"); // eslint-disable-line
       throw new Error("cannot load cognito values");
     }
   }
 };
 
 export const isAuthorized = async (event: APIGatewayProxyEvent) => {
+  console.log("running isAuthorized with event:", event); // eslint-disable-line
   const cognitoValues = await loadCognitoValues();
+  console.log("got cognito values", cognitoValues); // eslint-disable-line
   // Verifier that expects valid access tokens:
   const verifier = CognitoJwtVerifier.create({
     userPoolId: cognitoValues.userPoolId,
