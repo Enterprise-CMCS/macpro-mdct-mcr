7dac79d8e0dc4461a67ad66867d72dbc3a21b708
Garrett Rabian <garrett@coforma.io>
2022-05-25 12:39:52 -0600

    add back for sls

---
+++ services/app-api/handlers/measures/get.ts
@@ -0,0 +1,33 @@
+import handler from "../../libs/handler-lib";
+import dynamoDb from "../../libs/dynamodb-lib";
+import { convertToDynamoExpression } from "../dynamoUtils/convertToDynamoExpressionVars";
+import { createCompoundKey } from "../dynamoUtils/createCompoundKey";
+
+export const listMeasures = handler(async (event, _context) => {
+  const state = event.pathParameters?.state;
+  const year = event.pathParameters?.year as string;
+  const coreSet = event.pathParameters?.coreSet;
+
+  const params = {
+    TableName: process.env.measureTableName!,
+    ...convertToDynamoExpression(
+      { state: state, year: parseInt(year), coreSet: coreSet },
+      "list"
+    ),
+  };
+  const queryValue = await dynamoDb.scan(params);
+  return queryValue;
+});
+
+export const getMeasure = handler(async (event, _context) => {
+  const dynamoKey = createCompoundKey(event);
+  const params = {
+    TableName: process.env.measureTableName!,
+    Key: {
+      compoundKey: dynamoKey,
+      coreSet: event.pathParameters!.coreSet!,
+    },
+  };
+  const queryValue = await dynamoDb.get(params);
+  return queryValue;
+});
