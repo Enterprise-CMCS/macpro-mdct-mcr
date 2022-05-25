5b04bf41b4a82e6e22b92cc95cd5e641df3be68e
Garrett Rabian <garrett@coforma.io>
2022-05-25 11:07:35 -0600

    remove fake banner

--- services/ui-src/src/components/banners/AdminBanner.tsx
+++ services/ui-src/src/components/banners/AdminBanner.tsx
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 // utils
-import { convertDateEtToUtc } from "utils/time/time";
 import { AdminBannerData } from "utils/types/types";
 import { bannerId } from "../../utils/constants/constants";
 // api
@@ -11,18 +10,6 @@ import {
 } from "utils/api/requestMethods/banner";
 
 const ADMIN_BANNER_ID = bannerId;
-const midnight = { hour: 0, minute: 0, second: 0 };
-const oneSecondToMidnight = { hour: 23, minute: 59, second: 59 };
-const temporaryBanner: AdminBannerData = {
-  key: ADMIN_BANNER_ID,
-  title: "Welcome to the new Managed Care Reporting tool!",
-  description: "Each state must submit one report per program.",
-  startDate: convertDateEtToUtc({ year: 2022, month: 1, day: 1 }, midnight),
-  endDate: convertDateEtToUtc(
-    { year: 2022, month: 12, day: 31 },
-    oneSecondToMidnight
-  ),
-};
 
 const checkBannerActivityStatus = (
   startDate: number,
@@ -56,12 +43,6 @@ export const AdminBanner = () => {
     await writeBanner(newBannerData);
     await fetchAdminBanner();
   };
-
-  useEffect(() => {
-    // TODO: remove temporary write before phase1 deployment
-    writeBanner(temporaryBanner);
-    fetchAdminBanner();
-  }, []);
 
   if (bannerData) {
     bannerData.isActive = checkBannerActivityStatus(
