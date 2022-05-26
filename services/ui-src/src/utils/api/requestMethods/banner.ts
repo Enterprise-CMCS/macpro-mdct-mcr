62d4c03a37eddc76e972150155a9a7714a7ab516
Garrett <57802560+gmrabian@users.noreply.github.com>
2022-05-26 07:00:06 -0600

    add banner methods (#2567)

---
+++ services/ui-src/src/utils/api/requestMethods/banner.ts
@@ -0,0 +1,38 @@
+import { API } from "aws-amplify";
+import { getRequestHeaders } from "./getRequestHeaders";
+import { AdminBannerData } from "../../types/types";
+
+async function getBanner(bannerKey: string) {
+  const requestHeaders = await getRequestHeaders();
+  const request = {
+    headers: { ...requestHeaders },
+  };
+
+  const response = await API.get("banners", `/banners/${bannerKey}`, request);
+  return response;
+}
+
+async function writeBanner(bannerData: AdminBannerData) {
+  const requestHeaders = await getRequestHeaders();
+  const request = {
+    headers: { ...requestHeaders },
+    body: bannerData,
+  };
+  const response = await API.post(
+    "banners",
+    `/banners/${bannerData.key}`,
+    request
+  );
+  return response;
+}
+
+async function deleteBanner(bannerKey: string) {
+  const requestHeaders = await getRequestHeaders();
+  const request = {
+    headers: { ...requestHeaders },
+  };
+  const response = await API.del("banners", `/banners/${bannerKey}`, request);
+  return response;
+}
+
+export { getBanner, writeBanner, deleteBanner };
---
+++ services/ui-src/src/utils/constants/constants.ts
@@ -0,0 +1,1 @@
+export const bannerId = "admin-banner-id";
