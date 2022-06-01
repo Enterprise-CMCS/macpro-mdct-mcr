88149bfdf2cb18224980addc88e226d3f184b291
Brax Excell <braxton.excell@gmail.com>
2022-06-01 10:53:10 -0500

    update unix epoch times

--- services/ui-src/src/views/Admin/Admin.test.tsx
+++ services/ui-src/src/views/Admin/Admin.test.tsx
@@ -9,8 +9,8 @@ const mockBannerData = {
   key: "bannerId",
   title: "Yes here I am a banner",
   description: "I have a description too",
-  startDate: 1641016800000, // 1/1/2022
-  endDate: 1672552799000, // 12/31/2022
+  startDate: 1640995200000, // 1/1/2022 00:00:00 UTC
+  endDate: 1672531199000, // 12/31/2022 23:59:59 UTC
   fetchAdminBanner: jest.fn(() => {}),
   writeAdminBanner: jest.fn(() => {}),
   deleteAdminBanner: jest.fn(() => {}),
