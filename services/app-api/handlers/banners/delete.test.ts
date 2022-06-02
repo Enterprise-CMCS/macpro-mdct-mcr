c02c91dc498aee116da142632baec17e6b38ac0b
Garrett Rabian <garrett@coforma.io>
2022-06-02 13:43:45 -0600

    add get and delete tests

---
+++ services/app-api/handlers/banners/delete.test.ts
@@ -0,0 +1,77 @@
+import { deleteBanner } from "./delete";
+
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { proxyEvent } from "../../utils/testing/proxyEvent";
+import { StatusCodes } from "../../utils/types/types";
+import {
+  NO_KEY_ERROR_MESSAGE,
+  UNAUTHORIZED_MESSAGE,
+} from "../../utils/constants/constants";
+
+jest.mock("../../utils/dynamo/dynamodb-lib", () => ({
+  __esModule: true,
+  default: {
+    delete: jest.fn(),
+  },
+}));
+
+jest.mock("../../utils/auth/authorization", () => ({
+  __esModule: true,
+  isAuthorized: jest.fn().mockReturnValue(true),
+  hasPermissions: jest.fn().mockReturnValueOnce(false).mockReturnValue(true),
+}));
+
+jest.mock("../../utils/debugging/debug-lib", () => ({
+  __esModule: true,
+  init: jest.fn(),
+  flush: jest.fn(),
+}));
+
+const testEvent: APIGatewayProxyEvent = {
+  ...proxyEvent,
+  headers: { "cognito-identity-id": "test" },
+  pathParameters: { bannerId: "testKey" },
+};
+
+describe("Test Delete Banner", () => {
+  beforeEach(() => {
+    process.env["BANNER_TABLE_NAME"] = "fakeBannerTable";
+  });
+
+  test("Test not authorized to delete banner", async () => {
+    const res = await deleteBanner(testEvent, null);
+
+    expect(res.statusCode).toBe(403);
+    expect(res.body).toContain(UNAUTHORIZED_MESSAGE);
+  });
+
+  test("Test Successful Banner Deletion", async () => {
+    const res = await deleteBanner(testEvent, null);
+
+    expect(res.statusCode).toBe(StatusCodes.SUCCESS);
+    expect(res.body).toContain("fakeBannerTable");
+    expect(res.body).toContain("testKey");
+  });
+
+  test("Test bannerKey not provided", async () => {
+    const noKeyEvent: APIGatewayProxyEvent = {
+      ...testEvent,
+      pathParameters: {},
+    };
+    const res = await deleteBanner(noKeyEvent, null);
+
+    expect(res.statusCode).toBe(500);
+    expect(res.body).toContain(NO_KEY_ERROR_MESSAGE);
+  });
+
+  test("Test bannerKey empty", async () => {
+    const noKeyEvent: APIGatewayProxyEvent = {
+      ...testEvent,
+      pathParameters: { bannerId: "" },
+    };
+    const res = await deleteBanner(noKeyEvent, null);
+
+    expect(res.statusCode).toBe(500);
+    expect(res.body).toContain(NO_KEY_ERROR_MESSAGE);
+  });
+});
