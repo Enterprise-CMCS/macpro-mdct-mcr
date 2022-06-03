59ce7bfa9150d338ae5f078e9b623e74f3e1acf9
Brax Excell <braxton.excell@gmail.com>
2022-06-03 09:32:04 -0500

    upgrade AdminBanner to use useContext and add related tests (#2677)
    * switch AdminBanner to useContext
    
    * cleanup
    
    * wip
    
    * fix failing in local
    
    * all tests currently passing
    
    * gtemp
    
    * add useMemo to fix re-rendering bug
    
    * add amplify mock
    
    * remove ApiProvider
    
    * more fixies
    
    * current state of curgs
    
    * omg all the tests pass
    
    * make banner status checker a util
    
    * add util tests
    
    * add more tests for Admin
    
    * add more ABP tests
    
    * add more tests for Admin
    
    Co-authored-by: Garrett Rabian <garrett@coforma.io>

--- services/ui-src/src/components/app/AppRoutes.tsx
+++ services/ui-src/src/components/app/AppRoutes.tsx
@@ -1,32 +1,27 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Admin, Help, Home, NotFound, Profile } from "../../views";
 // components
-import { AdminBanner } from "components/banners/AdminBanner";
+import { AdminBannerProvider } from "components";
 // utils
 import { UserRoles } from "utils/types/types";
 
 export const AppRoutes = ({ userRole }: Props) => {
   const isAdmin = userRole === UserRoles.ADMIN;
-  const adminBanner = AdminBanner();
 
   return (
     <main id="main-wrapper" data-testid="main-wrapper">
-      <Routes>
-        <Route path="/" element={<Home adminBanner={adminBanner} />} />
-        <Route
-          path="/admin"
-          element={
-            !isAdmin ? (
-              <Navigate to="/profile" />
-            ) : (
-              <Admin adminBanner={adminBanner} />
-            )
-          }
-        />
-        <Route path="/help" element={<Help />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <AdminBannerProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/admin"
+            element={!isAdmin ? <Navigate to="/profile" /> : <Admin />}
+          />
+          <Route path="/help" element={<Help />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AdminBannerProvider>
     </main>
   );
 };
