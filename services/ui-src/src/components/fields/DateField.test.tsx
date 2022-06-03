d5437088aa61466d5447195a81a945f4cc937832
Braxton Excell <braxton.excell@gmail.com>
2022-06-03 14:42:15 -0500

    finish textfield tests, add more datefield tests

--- services/ui-src/src/components/fields/DateField.test.tsx
+++ services/ui-src/src/components/fields/DateField.test.tsx
@@ -1,33 +1,43 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
 //components
 import { FormProvider } from "react-hook-form";
 import { DateField } from "components";
 
+const errorVerbiage = "date-field-error";
+
 jest.mock("react-hook-form", () => ({
   ...jest.requireActual("react-hook-form"),
-  Controller: () => <></>,
   useForm: () => ({
     control: () => ({}),
   }),
   useFormContext: () => ({
-    formState: {
-      errors: {},
-    },
     register: () => {},
+    setValue: () => {},
+    getValues: () => {},
+    formState: {
+      errors: {
+        testDateField: {
+          message: errorVerbiage,
+        },
+      },
+    },
   }),
 }));
 
 const form = require("react-hook-form").useForm;
+const mockOnChangeHandler = jest.fn();
 
 const dateFieldComponent = (
   <FormProvider {...form}>
     <form>
       <div>
         <DateField
-          name="startDate"
-          label="Start date"
+          name="testDateField"
+          label="test-date-field"
           labelId="test-date-field"
+          onChange={mockOnChangeHandler}
         />
       </div>
     </form>
@@ -39,6 +49,21 @@ describe("Test DateField component", () => {
     const result = render(dateFieldComponent);
     const dateField = result.container.querySelector("#test-date-field");
     expect(dateField).toBeVisible();
+  });
+
+  test("Error message shows when there's an error", async () => {
+    render(dateFieldComponent);
+    expect(screen.queryByText(errorVerbiage)).toBeInTheDocument();
+  });
+
+  test("onChange event fires handler", async () => {
+    render(dateFieldComponent);
+    const result = render(dateFieldComponent);
+    const dateFieldMonthInput: HTMLInputElement =
+      result.container.querySelector('[name="testDateFieldMonth"]')!;
+    await userEvent.type(dateFieldMonthInput, "1");
+    expect(dateFieldMonthInput).toHaveValue("1");
+    expect(mockOnChangeHandler).toHaveBeenCalled();
   });
 });
 
--- services/ui-src/src/components/fields/TextField.test.tsx
+++ services/ui-src/src/components/fields/TextField.test.tsx
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
 //components
 import { FormProvider } from "react-hook-form";
 import { TextField } from "components";
+
+const errorVerbiage = "text-field-error";
 
 jest.mock("react-hook-form", () => ({
   ...jest.requireActual("react-hook-form"),
@@ -10,22 +13,29 @@ jest.mock("react-hook-form", () => ({
     control: () => ({}),
   }),
   useFormContext: () => ({
+    setValue: () => {},
     formState: {
-      errors: {},
+      errors: {
+        testTextField: {
+          message: errorVerbiage,
+        },
+      },
     },
   }),
 }));
 
 const form = require("react-hook-form").useForm;
+const mockOnChangeHandler = jest.fn();
 
 const textFieldComponent = (
   <FormProvider {...form}>
     <form>
       <TextField
-        name="testname"
+        name="testTextField"
         label="test-label"
         placeholder="test-placeholder"
         data-testid="test-text-field"
+        onChange={mockOnChangeHandler}
       />
     </form>
   </FormProvider>
@@ -36,6 +46,20 @@ describe("Test TextField component", () => {
     render(textFieldComponent);
     const textField = screen.getByTestId("test-text-field");
     expect(textField).toBeVisible();
+  });
+
+  test("Error message shows when there's an error", async () => {
+    render(textFieldComponent);
+    expect(screen.queryByText(errorVerbiage)).toBeInTheDocument();
+  });
+
+  test("onChange event fires handler", async () => {
+    render(textFieldComponent);
+    const textFieldInput: HTMLInputElement =
+      screen.getByTestId("test-text-field");
+    await userEvent.type(textFieldInput, "testinput");
+    expect(textFieldInput).toHaveValue("testinput");
+    expect(mockOnChangeHandler).toHaveBeenCalled();
   });
 });
 
