c4cd984de1425a92cdcea067fd697ec75bb2c7a7
Braxton Excell <braxton.excell@gmail.com>
2022-06-04 20:23:02 -0500

    useBreakpoint to 100

--- services/ui-src/src/utils/useBreakpoint.test.ts
+++ services/ui-src/src/utils/useBreakpoint.test.ts
@@ -1,4 +1,5 @@
-import { convertBreakpoints } from "./useBreakpoint";
+import { useMediaQuery } from "@chakra-ui/react";
+import { convertBreakpoints, makeMediaQueryClasses } from "./useBreakpoint";
 
 jest.mock("@chakra-ui/react", () => ({
   __esModule: true,
@@ -10,10 +11,13 @@ jest.mock("@chakra-ui/react", () => ({
       xl: "100em",
     },
   }),
+  useMediaQuery: jest.fn((array: boolean[]) => array),
 }));
 
+const mockedUseMQ = useMediaQuery as unknown as jest.Mock<typeof useMediaQuery>;
+
 describe("Test useBreakpoint convertBreakpoints method", () => {
-  test("breakpoints are converted from em to px correctly", () => {
+  test("Breakpoints are converted from em to px correctly", () => {
     const pxBreaks = convertBreakpoints();
     expect(pxBreaks).toEqual({
       sm: 560,
@@ -21,5 +25,35 @@ describe("Test useBreakpoint convertBreakpoints method", () => {
       lg: 1200,
       xl: 1600,
     });
+  });
+});
+
+describe("Test useBreakpoint makeMediaQueryClasses method", () => {
+  test("Mobile media query class calculated correctly ", () => {
+    // return value if window.innerWidth <=35em|560px
+    mockedUseMQ.mockImplementation((): any => [true, false, false, false]);
+    const mqClasses = makeMediaQueryClasses();
+    expect(mqClasses).toEqual("mobile");
+  });
+
+  test("Tablet media query class calculated correctly ", () => {
+    // return value if window.innerWidth >35em|560px && <=55em|880px
+    mockedUseMQ.mockImplementation((): any => [false, true, false, false]);
+    const mqClasses = makeMediaQueryClasses();
+    expect(mqClasses).toEqual("tablet");
+  });
+
+  test("Desktop media query class calculated correctly ", () => {
+    // return value if window.innerWidth >55em|880px
+    mockedUseMQ.mockImplementation((): any => [false, false, true, false]);
+    const mqClasses = makeMediaQueryClasses();
+    expect(mqClasses).toEqual("desktop");
+  });
+
+  test("Ultrawide media query class calculated correctly ", () => {
+    // return value if window.innerWidth >100em|1600px
+    mockedUseMQ.mockImplementation((): any => [false, false, true, true]);
+    const mqClasses = makeMediaQueryClasses();
+    expect(mqClasses).toEqual("desktop ultrawide");
   });
 });
