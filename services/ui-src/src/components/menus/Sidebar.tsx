f5de0e414bad099c9f4612dd4f23d88a04151a5f
Garrett Rabian <garrett@coforma.io>
2022-06-30 10:29:32 -0700

    further cleanup

--- services/ui-src/src/components/menus/Sidebar.tsx
+++ services/ui-src/src/components/menus/Sidebar.tsx
@@ -4,40 +4,32 @@ import { ArrowIcon, CheckCircleIcon } from "@cmsgov/design-system";
 
 interface LinkItemProps {
   name: string;
+  path: string;
 }
 
 const LinkItems: Array<LinkItemProps> = [
-  { name: "Get Started" },
-  { name: "A: Program Information" },
-  { name: "B: State-Level Indicators" },
-  { name: "C: Program-Level Indicators" },
-  { name: "D: Plan-Level Indicators" },
-  { name: "E: BSS Entity Indicators" },
-  { name: "Review & Submit" },
+  { name: "Get Started", path: "get-started" },
+  { name: "A: Program Information", path: "program-information" },
+  { name: "B: State-Level Indicators", path: "state-level-indicators" },
+  { name: "C: Program-Level Indicators", path: "program-level-indicators" },
+  { name: "D: Plan-Level Indicators", path: "plan-level-indicators" },
+  { name: "E: BSS Entity Indicators", path: "bss-entity-indicators" },
+  { name: "Review & Submit", path: "review-and-submit" },
 ];
 
 // from https://chakra-templates.dev/navigation/sidebar
 export const Sidebar = () => {
   return (
     <Box sx={sx.root}>
-      <Flex
-        h="20"
-        alignItems="center"
-        mx="8"
-        justifyContent="space-between"
-        borderBottom="1px solid gray"
-      >
-        <Text fontSize="xl" fontWeight="bold" minW="11.5rem">
-          MCPAR Report Submission Form
-        </Text>
+      <Flex sx={sx.sideNavTopFlex}>
+        <Text sx={sx.sideNavHeader}>MCPAR Report Submission Form</Text>
         <CloseButton />
       </Flex>
       {LinkItems.map((link) => (
-        <NavItem key={link.name} itemName={link.name}>
+        <NavItem key={link.name} path={link.path}>
           {link.name}
         </NavItem>
       ))}
-      {/* <SidebarContent onClose={() => onClose} /> */}
     </Box>
   );
 };
@@ -51,22 +43,14 @@ const CloseButton = () => {
 };
 
 interface NavItemProps extends FlexProps {
-  itemName: string;
+  path: string;
   children: ReactText;
 }
-const NavItem = ({ itemName, children, ...rest }: NavItemProps) => {
-  const linkPath = window.location + "/" + itemName;
+const NavItem = ({ path, children, ...rest }: NavItemProps) => {
+  const linkPath = window.location + "/" + path;
   return (
     <Link href={linkPath} textColor="palette.gray_darkest">
-      <Flex
-        align="center"
-        paddingY="0.5rem"
-        marginLeft="1.25rem"
-        role="group"
-        {...rest}
-        borderBottom="1px solid gray"
-        sx={sx.temp2}
-      >
+      <Flex sx={sx.navItemFlex} {...rest}>
         <CheckCircleIcon viewBox="10 10 200 200" />
         {children}
       </Flex>
@@ -80,6 +64,18 @@ const sx = {
     bg: "palette.gray_lightest",
     maxW: "15rem",
   },
+  sideNavTopFlex: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderBottom: "1px solid",
+    borderColor: "palette.gray_lighter",
+    padding: "1rem 1.5rem",
+  },
+  sideNavHeader: {
+    fontSize: "xl",
+    fontWeight: "bold",
+    minW: "11.5rem",
+  },
   temp: {
     ".ds-c-icon--arrow-left": {
       marginBottom: "3.5rem",
@@ -90,7 +86,13 @@ const sx = {
       width: "10px",
     },
   },
-  temp2: {
+  navItemFlex: {
+    align: "center",
+    paddingY: "0.5rem",
+    paddingLeft: "1.25rem",
+    role: "group",
+    borderBottom: "1px solid",
+    borderColor: "palette.gray_lighter",
     ".ds-c-icon--check-circle": {
       fill: "palette.gray_lighter",
     },
