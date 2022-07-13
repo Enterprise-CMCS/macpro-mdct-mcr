549151f47b283695cf6f117eff5fc941c5feda8f
Garrett Rabian <garrett@coforma.io>
2022-07-13 11:23:55 -0700

    add basic page layout

--- services/ui-src/src/verbiage/forms/mcparReportPages.ts
+++ services/ui-src/src/verbiage/forms/mcparReportPages.ts
@@ -1,9 +1,11 @@
 import sectionA_pointofcontact from "./apoc.json";
 import sectionA_reportingperiod from "./arp.json";
+import sectionA_bssentities from "./abss.json";
 
 export const mcparReportPages = [
   sectionA_pointofcontact,
   sectionA_reportingperiod,
+  sectionA_bssentities,
 ];
 
 export const mcparReportPageOrder = mcparReportPages.map((page) => page.path);
