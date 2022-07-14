d7c332597e6caa3a2f8f6c335abe8c68f3f843c2
Nicholas Summers <nick@coforma.io>
2022-07-14 10:21:44 -0400

    Fix e2e test

--- tests/cypress/tests/e2e/admin.spec.js
+++ tests/cypress/tests/e2e/admin.spec.js
@@ -37,9 +37,9 @@ describe("Admin banner integration tests", () => {
     const submitButton = "[type='submit']";
     cy.get(submitButton).focus().click();
 
-    const startDateText = "07/14/2022";
+    const startDateText = "7/14/2022";
     cy.contains(startDateText).should("be.visible");
-    const endDateText = "07/14/2026";
+    const endDateText = "7/14/2026";
     cy.contains(endDateText).should("be.visible");
 
     cy.contains(bannerWriteErrorMessage).should("not.exist");
