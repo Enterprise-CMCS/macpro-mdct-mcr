475407cd17d23825e7f3a84ba80f222d1a69704e
Braxton Excell <braxton.excell@gmail.com>
2022-07-18 09:02:11 -0500

    add radio to test file

--- services/ui-src/src/forms/mcpar/test.schema.ts
+++ services/ui-src/src/forms/mcpar/test.schema.ts
@@ -1,6 +1,7 @@
 import { object, string, array } from "yup";
 
 export default object({
+  // checkbox example
   test1: array()
     .required("Field is required")
     .min(1, "Must make at least one selection"),
@@ -12,6 +13,21 @@ export default object({
         .min(1, "Must make at least one selection"),
   }),
   "test1-o1-c-o1-c": string().when("test1-o1-c", {
+    is: (value: any) => value && value.indexOf("option1-1") != -1,
+    then: (schema: any) => schema.required("Field is required"),
+  }),
+  // radio example
+  test2: array()
+    .required("Field is required")
+    .min(1, "Must make at least one selection"),
+  "test2-o1-c": array().when("test2", {
+    is: (value: any) => value && value.indexOf("option1") != -1,
+    then: (schema: any) =>
+      schema
+        .required("Field is required")
+        .min(1, "Must make at least one selection"),
+  }),
+  "test2-o1-c-o1-c": string().when("test2-o1-c", {
     is: (value: any) => value && value.indexOf("option1-1") != -1,
     then: (schema: any) => schema.required("Field is required"),
   }),
