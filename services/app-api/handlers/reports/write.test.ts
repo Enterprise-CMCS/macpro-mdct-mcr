7ed052559b3346873a9128780986896b16a2e6ae
Garrett Rabian <garrett@coforma.io>
2022-07-26 15:03:32 -0700

    add tests

--- services/app-api/handlers/reports/write.test.ts
+++ services/app-api/handlers/reports/write.test.ts
@@ -6,6 +6,7 @@ import {
   NO_KEY_ERROR_MESSAGE,
   UNAUTHORIZED_MESSAGE,
 } from "../../utils/constants/constants";
+import { getReport } from "./get";
 
 jest.mock("../../utils/dynamo/dynamodb-lib", () => ({
   __esModule: true,
@@ -15,7 +16,7 @@ jest.mock("../../utils/dynamo/dynamodb-lib", () => ({
 }));
 
 jest.mock("../../utils/auth/authorization", () => ({
-  isAuthorized: jest.fn().mockReturnValue(true),
+  isAuthorized: jest.fn().mockResolvedValue(true),
   hasPermissions: jest.fn().mockReturnValueOnce(false).mockReturnValue(true),
 }));
 
@@ -24,9 +25,19 @@ jest.mock("../../utils/debugging/debug-lib", () => ({
   flush: jest.fn(),
 }));
 
+jest.mock("./get");
+const mockedGetReport = getReport as jest.MockedFunction<typeof getReport>;
+
 const testEvent: APIGatewayProxyEvent = {
   ...proxyEvent,
   body: `{"report":{"field1":"value1","field2":"value2","num1":0,"array":["array1", "array2"]}}`,
+  headers: { "cognito-identity-id": "test" },
+  pathParameters: { reportId: "ZA3141" },
+};
+
+const secondWriteEvent: APIGatewayProxyEvent = {
+  ...proxyEvent,
+  body: `{"report":{"newField1":"newValue1","newField2":"newValue2","newNum1":1,"newArray":["newArray1", "newArray2"]}}`,
   headers: { "cognito-identity-id": "test" },
   pathParameters: { reportId: "ZA3141" },
 };
@@ -45,12 +56,39 @@ describe("Test writeReport API method", () => {
   });
 
   test("Test Successful Run of report Creation", async () => {
+    mockedGetReport.mockResolvedValue({
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "string",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: "{}",
+    });
     const res = await writeReport(testEvent, null);
 
     const body = JSON.parse(res.body);
     expect(res.statusCode).toBe(StatusCodes.SUCCESS);
     expect(body.report.field1).toContain("value1");
     expect(body.report.num1).toBeCloseTo(0);
+  });
+
+  test("Test Successful Run of report update", async () => {
+    mockedGetReport.mockResolvedValue({
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "string",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: `{"report":{"field1":"value1","field2":"value2","num1":0,"array":["array1", "array2"]}}`,
+    });
+
+    const secondResponse = await writeReport(secondWriteEvent, null);
+    const secondBody = JSON.parse(secondResponse.body);
+    expect(secondResponse.statusCode).toBe(StatusCodes.SUCCESS);
+    expect(secondBody.report.newField1).toContain("newValue1");
+    expect(secondBody.report.newNum1).toBeCloseTo(1);
+    expect(secondBody.report.field1).toContain("value1");
+    expect(secondBody.report.num1).toBeCloseTo(0);
   });
 
   test("Test reportKey not provided throws 500 error", async () => {
