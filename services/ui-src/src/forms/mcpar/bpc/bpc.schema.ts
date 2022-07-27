2200593f4c0e696dfff36e9f9fa5248aaec93f64
Nicholas Summers <nick@coforma.io>
2022-07-27 10:56:59 -0400

    Add new fields to B1

--- services/ui-src/src/forms/mcpar/bpc/bpc.schema.ts
+++ services/ui-src/src/forms/mcpar/bpc/bpc.schema.ts
@@ -1,3 +1,11 @@
-import { object } from "yup";
+import { numberFormatRegex } from "../../../constants";
+import { object, string } from "yup";
 
-export default object({});
+export default object({
+  "bpc-1": string()
+    .required("Number field is required")
+    .matches(numberFormatRegex, "Please input a number"),
+  "bpc-2": string()
+    .required("Number field is required")
+    .matches(numberFormatRegex, "Please input a number"),
+});
