a1c24cd5f341d3da61f7f05a8c3ef33b039832ca
Nicholas Summers <nick@coforma.io>
2022-07-27 10:58:57 -0400

    Fix required text

--- services/ui-src/src/forms/mcpar/bpc/bpc.schema.ts
+++ services/ui-src/src/forms/mcpar/bpc/bpc.schema.ts
@@ -3,9 +3,9 @@ import { object, string } from "yup";
 
 export default object({
   "bpc-1": string()
-    .required("Number field is required")
+    .required("Field is required")
     .matches(numberFormatRegex, "Please input a number"),
   "bpc-2": string()
-    .required("Number field is required")
+    .required("Field is required")
     .matches(numberFormatRegex, "Please input a number"),
 });
