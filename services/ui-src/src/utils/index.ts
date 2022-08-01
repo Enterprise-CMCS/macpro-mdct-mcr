0699661ee80df4a63de612fb83a9549654971a27
Braxton Excell <braxton.excell@gmail.com>
2022-07-31 19:22:39 -0500

    tune up

--- services/ui-src/src/utils/index.ts
+++ services/ui-src/src/utils/index.ts
@@ -19,7 +19,7 @@ export * from "./reports/routing";
 export * from "./other/email";
 export * from "./other/focusElement";
 export * from "./other/mask";
-export * from "./other/parsing";
+export * from "./other/parseCustomHtml";
 export * from "./other/scrollToTop";
 export * from "./other/time";
 export * from "./other/useBreakpoint";
