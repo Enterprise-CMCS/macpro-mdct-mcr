dfd53e080e5a5366ef2a26e46ad7cd7037dd1922
Garrett Rabian <garrett@coforma.io>
2022-08-02 12:23:46 -0700

    human readable

--- services/ui-src/src/forms/mcpar/aap/aap.schema.ts
+++ services/ui-src/src/forms/mcpar/aap/aap.schema.ts
@@ -4,8 +4,6 @@ export default object({
   "aap-1": array()
     .min(1)
     .of(
-      string()
-        .required("Plan name is required")
-        .typeError("Plan name must be a string")
+      string().required("Plan name is required").typeError("Invalid plan name")
     ),
 });
