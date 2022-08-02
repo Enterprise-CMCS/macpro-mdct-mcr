f3d307f45a4234fec8c94f129b7cad8e3dd96348
Garrett Rabian <garrett@coforma.io>
2022-08-02 11:33:38 -0700

    aap schema

--- services/ui-src/src/forms/mcpar/aap/aap.schema.ts
+++ services/ui-src/src/forms/mcpar/aap/aap.schema.ts
@@ -1,3 +1,11 @@
-import { object } from "yup";
+import { array, object, string } from "yup";
 
-export default object({});
+export default object({
+  "aap-1": array()
+    .min(1)
+    .of(
+      string()
+        .required("Plan name is required")
+        .typeError("Plan name must be a string")
+    ),
+});
