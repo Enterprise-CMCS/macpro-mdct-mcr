8002823143e1c98b9c5bc928c907c95c7410046b
Garrett <57802560+gmrabian@users.noreply.github.com>
2022-08-03 09:30:29 -0700

    add bedr fields (#3285)

--- services/ui-src/src/forms/mcpar/bedr/bedr.schema.ts
+++ services/ui-src/src/forms/mcpar/bedr/bedr.schema.ts
@@ -1,3 +1,19 @@
-import { object } from "yup";
+import { object, string, array } from "yup";
 
-export default object({});
+export default object({
+  "bedr-1": array()
+    .required("Data validation entity is required")
+    .min(1, "Must make at least one selection"),
+  "bedr-2": array().when("bedr-1", {
+    is: (value: any) => value && value.indexOf("Proprietary system(s)") != -1,
+    then: (schema: any) =>
+      schema
+        .required("Field is required")
+        .min(1, "Must make at least one selection"),
+  }),
+  "bedr-1-o7-text": string().when("bedr-1", {
+    is: (value: any) =>
+      value && value.indexOf("Other, specify (required)") != -1,
+    then: (schema: any) => schema.required("Field is required"),
+  }),
+});
