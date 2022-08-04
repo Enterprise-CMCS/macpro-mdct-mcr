6463209769b3a0fb0581e0e3ab3c932449b015e0
Garrett Rabian <garrett@coforma.io>
2022-08-04 09:15:25 -0700

    add choice initialize method to json bloat

--- services/ui-src/src/utils/forms/forms.ts
+++ services/ui-src/src/utils/forms/forms.ts
@@ -10,7 +10,26 @@ import {
   TextAreaField,
 } from "components";
 // types
-import { AnyObject, FormField } from "types";
+import { AnyObject, FieldChoice, FormField } from "types";
+
+export const initializeChoiceFieldControl = (fields: FormField[]) => {
+  // find each field that contains choices (checkbox or radio)
+  fields.forEach((field: FormField) => {
+    if (field.props?.choices) {
+      // check non true choices as false
+      field.props.choices.forEach((choice: FieldChoice) => {
+        if (choice.checked != true) {
+          choice.checked = false;
+        }
+        // recurse for child choices
+        if (choice.children) {
+          initializeChoiceFieldControl(choice.children);
+        }
+      });
+    }
+  });
+  return fields;
+};
 
 // return created elements from provided fields
 export const formFieldFactory = (fields: FormField[], isNested?: boolean) => {
@@ -24,6 +43,7 @@ export const formFieldFactory = (fields: FormField[], isNested?: boolean) => {
     text: TextField,
     textarea: TextAreaField,
   };
+  fields = initializeChoiceFieldControl(fields);
   return fields.map((field) => {
     const componentFieldType = fieldToComponentMap[field.type];
     return React.createElement(componentFieldType, {
