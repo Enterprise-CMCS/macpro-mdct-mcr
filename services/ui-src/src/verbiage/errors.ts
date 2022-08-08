3671491dfa78d091e1e32883c2a5fdbd49d9aa69
Brax Excell <braxton.excell@gmail.com>
2022-08-08 16:48:11 -0500

    templatize field schemas (#3349)
    * templatize field schemas
    
    * add errors, remove nested prop
    
    * created nested field wrapper
    
    * fix array validation
    
    * fix failing tests
    
    * tweak form json

--- services/ui-src/src/verbiage/errors.ts
+++ services/ui-src/src/verbiage/errors.ts
@@ -6,6 +6,17 @@ export const bannerErrors = {
     "Current banner could not be deleted. Please contact support.",
 };
 
+export const schemaValidationErrors = {
+  REQUIRED_GENERIC: "A response is required",
+  REQUIRED_CHECKBOX: "Select at least one response",
+  INVALID_GENERIC: "Response must be valid",
+  INVALID_NUMBER: "Response must be a valid number",
+  INVALID_EMAIL: "Response must be a valid email address",
+  INVALID_URL: "Response must be a valid hyperlink/URL",
+  INVALID_DATE: "Response must be a valid date",
+  INVALID_END_DATE: "End date can't be before start date",
+};
+
 export const reportErrors = {
   GET_REPORT_DATA_FAILED: "Report could not be loaded. Please contact support.",
   SET_REPORT_DATA_FAILED: "Data could not be saved. Please contact support.",
