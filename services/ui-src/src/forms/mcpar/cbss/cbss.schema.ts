268aed7e65b558ca31558478be85485d5e037568
karla-vm <karla@coforma.io>
2022-08-11 14:47:00 -0400

    Section C: BSS

--- services/ui-src/src/forms/mcpar/cbss/cbss.schema.ts
+++ services/ui-src/src/forms/mcpar/cbss/cbss.schema.ts
@@ -1,3 +1,8 @@
-import { object } from "yup";
+import { object, string } from "yup";
 
-export default object({});
+export default object({
+  "cbss-1": string(),
+  "cbss-2": string(),
+  "cbss-3": string(),
+  "cbss-4": string(),
+});
