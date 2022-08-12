818b7890ef182222b88dde05807f7a9d61fb946b
karla-vm <karla@coforma.io>
2022-08-12 16:50:34 -0400

    Section B: Program Integrity

--- services/ui-src/src/forms/mcpar/bpi/bpi.schema.ts
+++ services/ui-src/src/forms/mcpar/bpi/bpi.schema.ts
@@ -1,3 +1,15 @@
 import { object } from "yup";
+import { radio, text } from "utils/forms/schemas";
 
-export default object({});
+export default object({
+  "bpi-1": text(),
+  "bpi-2": radio(),
+  "bpi-3": text(),
+  "bpi-4": text(),
+  "bpi-5": text(),
+  "bpi-6": text(),
+  "bpi-7a": radio(),
+  "bpi-8a": radio(),
+  "bpi-9a": radio(),
+  "bpi-10": text(),
+});
