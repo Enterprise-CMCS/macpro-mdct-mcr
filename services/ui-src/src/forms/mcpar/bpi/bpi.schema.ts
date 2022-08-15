766fc6f499df8682a0e840176a9d421cf9d7284a
karla-vm <karla@coforma.io>
2022-08-15 11:37:10 -0400

    Nested fields for Section B

--- services/ui-src/src/forms/mcpar/bpi/bpi.schema.ts
+++ services/ui-src/src/forms/mcpar/bpi/bpi.schema.ts
@@ -1,5 +1,5 @@
 import { object } from "yup";
-import { radio, text } from "utils/forms/schemas";
+import { nested, radio, text } from "utils/forms/schemas";
 
 export default object({
   "bpi-1": text(),
@@ -9,7 +9,12 @@ export default object({
   "bpi-5": text(),
   "bpi-6": text(),
   "bpi-7a": radio(),
+  "bpi-7a-o1": nested(radio, "bpi-7a", "Yes"),
+  "bpi-7a-o1-choice": radio(),
+  "bpi-7a-o1-choice-a-text": nested(text, "bpi-7a-o1-choice", "Yes"),
   "bpi-8a": radio(),
+  "bpi-8a-o1-text": nested(text, "bpi-8a", "Yes"),
   "bpi-9a": radio(),
+  "bpi-9a-o1-text": nested(text, "bpi-9a", "Yes"),
   "bpi-10": text(),
 });
