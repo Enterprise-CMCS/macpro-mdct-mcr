738f78aefbe476c5cf63e56baa7a113e3229cff3
karla-vm <karla@coforma.io>
2022-08-16 17:10:34 -0400

    Styling, scrolling

--- services/ui-src/src/components/drawer/Drawer.tsx
+++ services/ui-src/src/components/drawer/Drawer.tsx
@@ -10,7 +10,9 @@ import {
   DrawerOverlay,
   DrawerContent,
   Flex,
+  Text,
 } from "@chakra-ui/react";
+//import { Form } from "components";
 import { CloseIcon } from "@cmsgov/design-system";
 
 //import { makeMediaQueryClasses } from "utils";
@@ -21,11 +23,11 @@ export const Drawer = ({ drawerState }: Props) => {
     <ChakraDrawer
       isOpen={drawerState.isOpen}
       onClose={drawerState.onClose}
+      size="full"
       placement="right"
-      size="md"
     >
       <DrawerOverlay />
-      <DrawerContent>
+      <DrawerContent sx={sx.drawerContent}>
         <Flex sx={sx.drawerCloseContainer}>
           <Button
             sx={sx.drawerClose}
@@ -36,9 +38,22 @@ export const Drawer = ({ drawerState }: Props) => {
             Close
           </Button>
         </Flex>
-        <DrawerHeader></DrawerHeader>
-        <DrawerBody></DrawerBody>
-        <DrawerFooter></DrawerFooter>
+        <DrawerHeader sx={sx.drawerHeader}>
+          <Text sx={sx.drawerHeaderText}>
+            Add a quality & performance measure
+          </Text>
+        </DrawerHeader>
+        <DrawerBody sx={sx.drawerBody}>
+          {/* TODO: Get Form details somehow */}
+          {/* <Form /> */}
+          <form>Placeholder</form>
+        </DrawerBody>
+        <DrawerFooter sx={sx.drawerFooter}>
+          <Button sx={sx.action}>Save</Button>
+          <Button sx={sx.close} onClick={drawerState.onClose}>
+            Close
+          </Button>
+        </DrawerFooter>
       </DrawerContent>
     </ChakraDrawer>
   );
@@ -47,28 +62,25 @@ export const Drawer = ({ drawerState }: Props) => {
 interface Props {
   drawerState: {
     isOpen: boolean;
-    onClose: any;
+    onClose?: any;
   };
   [key: string]: any;
 }
 
 const sx = {
-  //eslint-disable-next-line
-  //   modalContent: {
-  //     boxShadow: ".125rem .125rem .25rem",
-  //     borderRadius: "0",
-  //     maxWidth: "30rem",
-  //     marginX: "4rem",
-  //     padding: "2rem",
-  //   },
-  //   modalHeader: {
-  //     padding: "0",
-  //   },
-  //   modalHeaderText: {
-  //     padding: "0 4rem 0 0",
-  //     fontSize: "2xl",
-  //     fontWeight: "bold",
-  //   },
+  drawerContent: {
+    maxWidth: "35vw",
+    padding: "2rem",
+    overflow: "scroll",
+  },
+  drawerHeader: {
+    padding: "0",
+  },
+  drawerHeaderText: {
+    padding: "0 4rem 0 0",
+    fontSize: "2xl",
+    fontWeight: "bold",
+  },
   drawerCloseContainer: {
     alignItems: "center",
     justifycontent: "center",
@@ -87,39 +99,38 @@ const sx = {
       },
     },
   },
-  //eslint-disable-next-line
-  //   modalBody: {
-  //     paddingX: "0",
-  //     paddingY: "1rem",
-  //   },
-  //   modalFooter: {
-  //     justifyContent: "flex-start",
-  //     padding: "0",
-  //     paddingTop: "2rem",
-  //   },
-  //   action: {
-  //     justifyContent: "start",
-  //     marginTop: "1rem",
-  //     marginRight: "2rem",
-  //     span: {
-  //       marginLeft: "0.5rem",
-  //       marginRight: "-0.25rem",
-  //     },
-  //     "&.mobile": {
-  //       fontSize: "sm",
-  //     },
-  //   },
-  //   close: {
-  //     justifyContent: "start",
-  //     padding: ".5rem 1rem",
-  //     marginTop: "1rem",
-  //     span: {
-  //       marginLeft: "0rem",
-  //       marginRight: "0.5rem",
-  //     },
-  //     "&.mobile": {
-  //       fontSize: "sm",
-  //       marginRight: "0",
-  //     },
-  //   },
+  drawerBody: {
+    paddingX: "0",
+    paddingY: "1rem",
+  },
+  drawerFooter: {
+    justifyContent: "flex-start",
+    padding: "0",
+    paddingTop: "2rem",
+  },
+  action: {
+    justifyContent: "start",
+    marginTop: "1rem",
+    marginRight: "2rem",
+    span: {
+      marginLeft: "0.5rem",
+      marginRight: "-0.25rem",
+    },
+    "&.mobile": {
+      fontSize: "sm",
+    },
+  },
+  close: {
+    justifyContent: "start",
+    padding: ".5rem 1rem",
+    marginTop: "1rem",
+    span: {
+      marginLeft: "0rem",
+      marginRight: "0.5rem",
+    },
+    "&.mobile": {
+      fontSize: "sm",
+      marginRight: "0",
+    },
+  },
 };
