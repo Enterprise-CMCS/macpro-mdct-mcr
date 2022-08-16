8b5b5954b872f86cae8a936040ce74235f2673d8
juliuscaesar <caesarnuzzolo@Caesars-MBP.lan>
2022-08-16 16:14:03 -0400

    update nested reference

--- services/ui-src/src/forms/mcpar/cpc/cpc.schema.ts
+++ services/ui-src/src/forms/mcpar/cpc/cpc.schema.ts
@@ -16,7 +16,7 @@ export default object({
   "cpc-3": radio(),
   "cpc-3-o5-text": nested(text, "cpc-3", "Other, specify"),
   "cpc-4a": checkbox(),
-  "cpc-4a-o5-text": nested(text, "cpc-4", "None of the above"),
+  "cpc-4a-o5-text": nested(text, "cpc-4a", "None of the above"),
   "cpc-4b": text(),
   "cpc-5": number(),
   "cpc-6": text(),
