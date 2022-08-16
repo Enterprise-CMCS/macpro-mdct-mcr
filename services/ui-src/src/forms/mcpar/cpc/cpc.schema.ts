d6b72b279dd6fa3aac14926c02dd4c8093b62118
juliuscaesar <caesarnuzzolo@Caesars-MBP.lan>
2022-08-16 15:45:12 -0400

    pr updates

--- services/ui-src/src/forms/mcpar/cpc/cpc.schema.ts
+++ services/ui-src/src/forms/mcpar/cpc/cpc.schema.ts
@@ -6,17 +6,18 @@ import {
   number,
   radio,
   text,
+  url,
 } from "utils/forms/schemas";
 
 export default object({
   "cpc-1": text(),
-  "cpc-1-b": date(),
-  "cpc-2": text(),
+  "cpc-1-sub": date(),
+  "cpc-2": url(),
   "cpc-3": radio(),
   "cpc-3-o5-text": nested(text, "cpc-3", "Other, specify"),
-  "cpc-4": checkbox(),
-  "cpc-4-o5-text": nested(text, "cpc-4", "None of the above"),
-  "cpc-5": text(),
-  "cpc-6": number(),
-  "cpc-7": text(),
+  "cpc-4a": checkbox(),
+  "cpc-4a-o5-text": nested(text, "cpc-4", "None of the above"),
+  "cpc-4b": text(),
+  "cpc-5": number(),
+  "cpc-6": text(),
 });
