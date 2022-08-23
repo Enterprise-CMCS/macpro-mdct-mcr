f54a13551e72865671944313a4f4055b1ed2ad8a
juliuscaesar <caesarnuzzolo@Caesars-MBP.lan>
2022-08-23 12:29:35 -0400

    add fields

--- services/ui-src/src/forms/mcpar/ebssei/ebssei.schema.ts
+++ services/ui-src/src/forms/mcpar/ebssei/ebssei.schema.ts
@@ -1,3 +1,9 @@
 import { object } from "yup";
+import { checkbox, text } from "utils/forms/schemas";
 
-export default object({});
+export default object({
+  "ebessei-1": checkbox(),
+  "ebessei-1-13-text": text(),
+  "ebessei-2": checkbox(),
+  "ebessei-2-7-text": text(),
+});
