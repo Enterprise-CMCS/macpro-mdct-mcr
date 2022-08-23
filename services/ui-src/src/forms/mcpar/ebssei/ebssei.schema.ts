52607a310f33c8fb3ecb4808c332fa6d677542b0
juliuscaesar <caesarnuzzolo@Caesars-MBP.lan>
2022-08-23 14:07:43 -0400

    nested

--- services/ui-src/src/forms/mcpar/ebssei/ebssei.schema.ts
+++ services/ui-src/src/forms/mcpar/ebssei/ebssei.schema.ts
@@ -1,9 +1,9 @@
 import { object } from "yup";
-import { checkbox, text } from "utils/forms/schemas";
+import { checkbox, nested, text } from "utils/forms/schemas";
 
 export default object({
   "ebessei-1": checkbox(),
-  "ebessei-1-13-text": text(),
+  "ebessei-1-13-text": nested(text, "ebessei-1-o13", "Other"),
   "ebessei-2": checkbox(),
-  "ebessei-2-7-text": text(),
+  "ebessei-2-7-text": nested(text, "ebessei-2-o7", "Other"),
 });
