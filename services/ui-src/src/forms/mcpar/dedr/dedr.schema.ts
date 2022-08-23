64d4e3120617850f99ab6812a34191f35aa8c122
juliuscaesar <caesarnuzzolo@Caesars-MBP.lan>
2022-08-23 12:34:43 -0400

    oops

--- services/ui-src/src/forms/mcpar/dedr/dedr.schema.ts
+++ services/ui-src/src/forms/mcpar/dedr/dedr.schema.ts
@@ -1,8 +1,3 @@
 import { object } from "yup";
-import { number, text } from "utils/forms/schemas";
 
-export default object({
-  "dedr-1": text(),
-  "dedr-2": number(),
-  "dedr-3": number(),
-});
+export default object({});
