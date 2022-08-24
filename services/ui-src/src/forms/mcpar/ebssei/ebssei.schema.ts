01dac056edf8c12a27aa9163736bdbff36375e55
juliuscaesar <caesarnuzzolo@Caesars-MBP.lan>
2022-08-24 11:35:07 -0400

    internalLink and gray text

--- services/ui-src/src/forms/mcpar/ebssei/ebssei.schema.ts
+++ services/ui-src/src/forms/mcpar/ebssei/ebssei.schema.ts
@@ -2,8 +2,8 @@ import { object } from "yup";
 import { checkbox, nested, text } from "utils/forms/schemas";
 
 export default object({
-  "ebessei-1": checkbox(),
-  "ebessei-1-13-text": nested(text, "ebessei-1-o13", "Other"),
-  "ebessei-2": checkbox(),
-  "ebessei-2-7-text": nested(text, "ebessei-2-o7", "Other"),
+  "ebssei-1": checkbox(),
+  "ebssei-1-o13-text": nested(text, "ebssei-1", "Other"),
+  "ebssei-2": checkbox(),
+  "ebssei-2-o7-text": nested(text, "ebssei-2", "Other"),
 });
