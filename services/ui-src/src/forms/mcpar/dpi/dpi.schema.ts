455b589cb2e9e1eb0bc0c5b04d785655c911d9b1
juliuscaesar <caesarnuzzolo@Caesars-MBP.lan>
2022-08-26 16:00:56 -0400

    updates

--- services/ui-src/src/forms/mcpar/dpi/dpi.schema.ts
+++ services/ui-src/src/forms/mcpar/dpi/dpi.schema.ts
@@ -8,17 +8,17 @@ export default object({
   "dpi-4": number(),
   "dpi-5": number(),
   "dpi-6": radio(),
-  "dpi-7-o1": nested(
+  "dpi-7a": nested(
     number,
     "dpi-6",
     "Makes referrals to the Medicaid Fraud Control Unit (MFCU) only"
   ),
-  "dpi-7-o2": nested(
+  "dpi-7b": nested(
     number,
     "dpi-6",
     "Makes referrals to the State Medicaid Agency (SMA) and MFCU concurrently"
   ),
-  "dpi-7-o3": nested(
+  "dpi-7c": nested(
     number,
     "dpi-6",
     "Makes some referrals to the SMA and others directly to the MFCU"
