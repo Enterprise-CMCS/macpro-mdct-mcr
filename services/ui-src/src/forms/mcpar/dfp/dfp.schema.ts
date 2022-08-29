bec467de7594d8b9ea69e86cf4b278a9534dbe1d
juliuscaesar <caesarnuzzolo@Caesars-MBP.lan>
2022-08-29 16:15:48 -0400

    dfp and some other stuff

--- services/ui-src/src/forms/mcpar/dfp/dfp.schema.ts
+++ services/ui-src/src/forms/mcpar/dfp/dfp.schema.ts
@@ -1,3 +1,19 @@
 import { object } from "yup";
+import {
+  date,
+  endDate,
+  nested,
+  number,
+  radio,
+  text,
+} from "utils/forms/schemas";
 
-export default object({});
+export default object({
+  "dfp-1": number(),
+  "dfp-2": radio(),
+  "dfp-2-o5-text": nested(text, "dfp-2", "Other"),
+  "dfp-3": text(),
+  "dfp-4": radio(),
+  "dfp-5-start": nested(date, "dfp-4", "Yes"),
+  "dfp-5-end": nested(() => endDate("dfp-5-start"), "dfp-4", "Yes"),
+});
