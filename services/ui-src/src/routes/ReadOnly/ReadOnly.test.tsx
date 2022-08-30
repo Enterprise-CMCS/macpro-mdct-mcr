cec10ec8f1f0475bbfe214615487513f1f35094c
karla-vm <karla@coforma.io>
2022-08-30 14:58:32 -0400

    Unit tests for submitting

--- services/ui-src/src/routes/ReadOnly/ReadOnly.test.tsx
+++ services/ui-src/src/routes/ReadOnly/ReadOnly.test.tsx
@@ -1,10 +1,16 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { axe } from "jest-axe";
+import userEvent from "@testing-library/user-event";
 // components
 import { ReadOnly } from "routes";
 // utils
 import { mockAdminUser, RouterWrappedComponent } from "utils/testing/setupJest";
 import { useUser } from "utils";
+
+// MOCKS
+
+jest.mock("utils/auth/useUser");
+const mockedUseUser = useUser as jest.MockedFunction<typeof useUser>;
 
 const mockVerbiage = {
   header: "mock header",
@@ -17,19 +23,32 @@ const readOnlyView = (
   </RouterWrappedComponent>
 );
 
-// MOCKS
+const fillOutForm = async (form: any) => {
+  // selectors for all the required fields
+  const dropdownInput = form.querySelector("[name='ads-state']")!;
+  // fill out form fields
+  fireEvent.change(dropdownInput, { target: { value: "CA" } });
+};
 
-jest.mock("utils/auth/useUser");
-const mockedUseUser = useUser as jest.MockedFunction<typeof useUser>;
+// TESTS
 
 describe("Test Read Only view", () => {
   beforeEach(() => {
     mockedUseUser.mockReturnValue(mockAdminUser);
-    render(readOnlyView);
   });
 
   test("Check that Read Only view renders", () => {
+    render(readOnlyView);
     expect(screen.getByTestId("read-only-view")).toBeVisible();
+  });
+
+  test("Form submits correctly", async () => {
+    const result = render(readOnlyView);
+    const form = result.container;
+    await fillOutForm(form);
+    const submitButton = screen.getByTestId("go-to-report-btn");
+    await userEvent.click(submitButton);
+    expect(window.location.pathname).toEqual("/mcpar/dashboard");
   });
 });
 
