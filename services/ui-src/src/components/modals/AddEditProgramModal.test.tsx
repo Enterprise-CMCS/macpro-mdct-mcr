80e12b76dfe3c7f8497c4e9d858f6b4ca4a261bb
Garrett Rabian <garrett@coforma.io>
2022-08-31 11:39:40 -0600

    add mock

--- services/ui-src/src/components/modals/AddEditProgramModal.test.tsx
+++ services/ui-src/src/components/modals/AddEditProgramModal.test.tsx
@@ -19,6 +19,7 @@ const mockReportMethods = {
   fetchReport: jest.fn(() => {}),
   updateReport: mockUpdateReport,
   fetchReportsByState: mockFetchReportsByState,
+  removeReport: jest.fn(() => {}),
 };
 
 const mockReportContext = {
