d2de042c50037c4d7813c0425ff2255bd164feb8
Garrett Rabian <garrett@coforma.io>
2022-08-31 09:35:45 -0600

    cleanup Home.tsx

--- services/ui-src/src/routes/Home/Home.tsx
+++ services/ui-src/src/routes/Home/Home.tsx
@@ -23,55 +23,52 @@ export const Home = () => {
   const showBanner = !!bannerData.key && bannerIsActive;
 
   // determine if landing page should be read-only
-  const { user } = useUser();
-  const { userRole } = user ?? {};
+  const { userRole } = useUser().user ?? {};
   const { intro, cards } = verbiage;
 
   return (
     <>
-      <>
-        <Collapse in={showBanner}>
-          <Banner bannerData={bannerData} />
-        </Collapse>
-        <BasicPage sx={sx.layout} data-testid="home-view">
-          {/* show standard view to state users */}
-          {userRole === UserRoles.STATE_USER ||
-          userRole === UserRoles.STATE_REP ? (
-            <>
-              <Box sx={sx.introTextBox}>
-                <Heading as="h1" sx={sx.headerText}>
-                  {intro.header}
-                </Heading>
-                <Text>
-                  {intro.body.preLinkText}
-                  <Link href={intro.body.linkLocation} isExternal>
-                    {intro.body.linkText}
-                  </Link>
-                  {intro.body.postLinkText}
-                </Text>
-              </Box>
-              <TemplateCard
-                templateName="MCPAR"
-                verbiage={cards.MCPAR}
-                cardprops={sx.card}
-              />
-              <TemplateCard
-                templateName="MLR"
-                verbiage={cards.MLR}
-                cardprops={sx.card}
-              />
-              <TemplateCard
-                templateName="NAAAR"
-                verbiage={cards.NAAAR}
-                cardprops={sx.card}
-              />
-            </>
-          ) : (
-            // show read-only view to non-state users
-            <ReadOnly verbiage={verbiage.readOnly} />
-          )}
-        </BasicPage>
-      </>
+      <Collapse in={showBanner}>
+        <Banner bannerData={bannerData} />
+      </Collapse>
+      <BasicPage sx={sx.layout} data-testid="home-view">
+        {/* show standard view to state users */}
+        {userRole === UserRoles.STATE_USER ||
+        userRole === UserRoles.STATE_REP ? (
+          <>
+            <Box sx={sx.introTextBox}>
+              <Heading as="h1" sx={sx.headerText}>
+                {intro.header}
+              </Heading>
+              <Text>
+                {intro.body.preLinkText}
+                <Link href={intro.body.linkLocation} isExternal>
+                  {intro.body.linkText}
+                </Link>
+                {intro.body.postLinkText}
+              </Text>
+            </Box>
+            <TemplateCard
+              templateName="MCPAR"
+              verbiage={cards.MCPAR}
+              cardprops={sx.card}
+            />
+            <TemplateCard
+              templateName="MLR"
+              verbiage={cards.MLR}
+              cardprops={sx.card}
+            />
+            <TemplateCard
+              templateName="NAAAR"
+              verbiage={cards.NAAAR}
+              cardprops={sx.card}
+            />
+          </>
+        ) : (
+          // show read-only view to non-state users
+          <ReadOnly verbiage={verbiage.readOnly} />
+        )}
+      </BasicPage>
     </>
   );
 };
