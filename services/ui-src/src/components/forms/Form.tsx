3bb8c7dbffbe9711b333823aec2ed767edfa2402
Nicholas Summers <nick@coforma.io>
2022-09-13 13:11:17 -0400

    Fix possible undefined error for fieldInput

--- services/ui-src/src/components/forms/Form.tsx
+++ services/ui-src/src/components/forms/Form.tsx
@@ -57,7 +57,7 @@ export const Form = ({
   // hydrate and create form fields using formFieldFactory
   const renderFormFields = (fields: FormField[]) => {
     const fieldsToRender = hydrateFormFields(fields, formData);
-    return formFieldFactory(fieldsToRender, fieldInputDisabled);
+    return formFieldFactory(fieldsToRender, fieldInputDisabled || false);
   };
 
   return (
