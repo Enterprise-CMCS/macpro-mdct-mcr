60b209f6b556702cf932989826be27447e8a93d2
Nicholas Summers <nick@coforma.io>
2022-09-21 13:12:05 -0400

    Fix tests

--- services/ui-src/src/components/fields/RadioField.test.tsx
+++ services/ui-src/src/components/fields/RadioField.test.tsx
@@ -62,7 +62,7 @@ describe("Test RadioField component", () => {
     await userEvent.click(firstRadio);
     expect(mockSetValue).toHaveBeenCalledWith(
       "radio_choices",
-      [{ key: "radio_choices", value: "A" }],
+      [{ key: "Choice 1", value: "A" }],
       {
         shouldValidate: true,
       }
