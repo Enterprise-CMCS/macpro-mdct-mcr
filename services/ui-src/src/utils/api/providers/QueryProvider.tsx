863b04a70864e3053cd1624c96947d619d7b252d
Nick Summers <ntsummers1@gmail.com>
2022-10-11 14:03:20 -0400

    MDCT-2004: Remove React-Query Dependency (#4314)
    * Remove react-query
    
    * Remove react-query dependency
    
    * Remove old comment
    
    Co-authored-by: Nicholas Summers <nick@coforma.io>

--- services/ui-src/src/utils/api/providers/QueryProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { QueryClient, QueryClientProvider } from "react-query";
-
-export const QueryProvider = ({ children }: any) => (
-  <QueryClientProvider client={new QueryClient()}>
-    {children}
-  </QueryClientProvider>
-);
