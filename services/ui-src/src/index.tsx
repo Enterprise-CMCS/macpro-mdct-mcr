1500c44875d1f248fc47cf007842015b65859c09
Nicholas Summers <nick@coforma.io>
2022-10-11 11:22:05 -0400

    Remove react-query

--- services/ui-src/src/index.tsx
+++ services/ui-src/src/index.tsx
@@ -1,11 +1,10 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
-// import { ReactQueryDevtools } from "react-query/devtools";
 import { Amplify } from "aws-amplify";
 import config from "config";
 // utils
-import { ApiProvider, QueryProvider, UserProvider } from "utils";
+import { ApiProvider, UserProvider } from "utils";
 // components
 import { App, Error } from "components";
 // styles
@@ -26,12 +25,10 @@ ReactDOM.render(
     <Router>
       <UserProvider>
         <ApiProvider>
-          <QueryProvider>
-            <ChakraProvider theme={theme}>
-              <App />
-            </ChakraProvider>
-            {/* <ReactQueryDevtools /> */}
-          </QueryProvider>
+          <ChakraProvider theme={theme}>
+            <App />
+          </ChakraProvider>
+          {/* <ReactQueryDevtools /> */}
         </ApiProvider>
       </UserProvider>
     </Router>
