752942b64894e10b611d4aff1bc510872660b07a
Braxton Excell <braxton.excell@gmail.com>
2022-10-12 12:48:18 -0500

    remove commented line

--- services/ui-src/src/components/app/SkipNav.test.tsx
+++ services/ui-src/src/components/app/SkipNav.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
 //components
 import { SkipNav } from "components";
