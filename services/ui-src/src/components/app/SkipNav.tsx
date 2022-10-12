e41b92f45a43923341fd4cf5cccf7cad1e6a9b1d
Braxton Excell <braxton.excell@gmail.com>
2022-10-11 19:58:43 -0500

    ha i win

--- services/ui-src/src/components/app/SkipNav.tsx
+++ services/ui-src/src/components/app/SkipNav.tsx
@@ -5,18 +5,10 @@ import { AnyObject } from "types";
 export const SkipNav = ({ id, href, text, sxOverride, ...props }: Props) => {
   return (
     <Link
-      // tabIndex={-1}
       id={id}
       sx={{ ...sx.skipNavLink, ...sxOverride }}
       href={href}
       className="ds-c-skip-nav"
-      onClick={() => {
-        // remove initial "#" from href string
-        const targetElementId = href.substring(1);
-        const targetElement: HTMLElement =
-          document.getElementById(targetElementId)!;
-        targetElement?.focus({ preventScroll: true });
-      }}
       {...props}
     >
       {text}
@@ -36,7 +28,7 @@ const sx = {
   skipNavLink: {
     background: "palette.white",
     zIndex: "skipLink",
-    // transition: "all 0s !important",
+    transition: "all 0s !important",
     "&:focus-visible": {
       top: 1,
     },
