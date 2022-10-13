d9f9d574e4b1dbc2146dcb60279aa74fd70fb582
Brax Excell <braxton@coforma.io>
2022-10-13 16:36:06 -0500

    create filtration method; implement (#4416)
    Co-authored-by: Nick Summers <ntsummers1@gmail.com>

--- services/ui-src/src/components/reports/ModalDrawerReportPage.tsx
+++ services/ui-src/src/components/reports/ModalDrawerReportPage.tsx
@@ -11,7 +11,7 @@ import {
   ReportPageIntro,
 } from "components";
 // utils
-import { getFormattedEntityData, useUser } from "utils";
+import { filterFormData, getFormattedEntityData, useUser } from "utils";
 import {
   AnyObject,
   EntityShape,
@@ -84,7 +84,7 @@ export const ModalDrawerReportPage = ({ route }: Props) => {
     drawerOnCloseHandler();
   };
 
-  const onSubmit = async (formData: AnyObject) => {
+  const onSubmit = async (enteredData: AnyObject) => {
     if (userIsStateUser || userIsStateRep) {
       setSubmitting(true);
       const reportKeys = {
@@ -95,9 +95,10 @@ export const ModalDrawerReportPage = ({ route }: Props) => {
       const selectedEntityIndex = report?.fieldData[entityType].findIndex(
         (entity: EntityShape) => entity.id === selectedEntity?.id
       );
+      const filteredFormData = filterFormData(enteredData, drawerForm.fields);
       const newEntity = {
         ...selectedEntity,
-        ...formData,
+        ...filteredFormData,
       };
       let newEntities = currentEntities;
       newEntities[selectedEntityIndex] = newEntity;
