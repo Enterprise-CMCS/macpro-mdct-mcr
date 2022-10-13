5e9d0026bda4bfcbf097f546c857be6312b94cd9
brittbujacich <britt.bujacich@gmail.com>
2022-10-13 11:52:11 -0600

    integration test

---
+++ tests/cypress/tests/e2e/state-user-dashboard.spec.js
@@ -0,0 +1,60 @@
+// element selectors
+const enterMcparOnline = '[data-testid="enter-mcpar-online"]';
+const secondButton = '[data-testid="second-enter-mcpar-button"]';
+const addProgram = '[data-testid="add-program"]';
+const enterProgram = '[data-testid="enter-program"]';
+const editProgramButtom = "[data-testid='edit-button']";
+const submitButton = "[data-testid='modal-submit-button']";
+const closeButton = "[data-testid='close-modal']";
+const programNameListItem = "[data-testid='program-name']";
+const leaveFormButton = "[data-testid='leave-form-button']";
+
+// selectors for all the required fields
+const titleInput = "[name='programName']";
+const startDateInput = "[name='reportingPeriodStartDate']";
+const endDateInput = "[name='reportingPeriodEndDate']";
+const checkbox = "[name='combinedData']";
+
+describe("state user creates a program", () => {
+  it("Fills out program modal without errors", () => {
+    //log in
+    cy.visit("/");
+    cy.authenticate("stateUser");
+    cy.get(enterMcparOnline).click();
+    cy.get(secondButton).click();
+    cy.get(addProgram).click();
+
+    // fill out form fields
+    cy.get(titleInput).type("program title");
+    cy.get(startDateInput).type("07142023");
+    cy.get(endDateInput).type("07142026");
+    cy.get(checkbox).focus().click();
+
+    cy.get(submitButton).click();
+  });
+
+  it("hydrates modal correctly", () => {
+    cy.get(editProgramButtom).first().click();
+
+    cy.get(titleInput).should("have.value", "program title");
+    cy.get(startDateInput).should("have.value", "07/14/2023");
+    cy.get(endDateInput).should("have.value", "07/14/2026");
+    cy.get(checkbox).should("be.checked");
+
+    cy.get(closeButton).click();
+  });
+
+  it("enters the program", () => {
+    cy.get(enterProgram).first().click();
+    cy.location("pathname").should("match", /point-of-contact/);
+    cy.get(leaveFormButton).first().click();
+  });
+
+  it("edits modal after program creation", () => {
+    cy.get(editProgramButtom).first().click();
+    cy.get(titleInput).clear().type("new name");
+    cy.get(submitButton).click();
+
+    cy.get(programNameListItem).first().should("have.value", "new name");
+  });
+});
