0cf0278104221f1192f9abe0b1395f5dd522cddb
Garrett Rabian <garrett@coforma.io>
2022-10-19 10:58:27 -0400

    add drawer and reportdrawer tests

--- services/ui-src/src/components/drawers/ReportDrawer.test.tsx
+++ services/ui-src/src/components/drawers/ReportDrawer.test.tsx
@@ -1,18 +1,33 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
 //components
 import { ReportDrawer } from "components";
-import { mockDrawerForm } from "utils/testing/setupJest";
+import {
+  mockAdminUser,
+  mockDrawerForm,
+  mockStateUser,
+} from "utils/testing/setupJest";
+// utils
+import { useUser } from "utils";
+// constants
+import { closeText, saveAndCloseText } from "../../constants";
+
+const mockOnClose = jest.fn();
+const mockOnSubmit = jest.fn();
 
 const mockDrawerDisclosure = {
   isOpen: true,
-  onClose: () => {},
+  onClose: mockOnClose,
 };
 
 const mockEntity = {
   id: "mock-id-1",
   "mock-modal-text-field": "mock input 1",
 };
+
+jest.mock("utils/auth/useUser");
+const mockedUseUser = useUser as jest.MockedFunction<typeof useUser>;
 
 const drawerComponent = (
   <ReportDrawer
@@ -21,17 +36,74 @@ const drawerComponent = (
     }}
     selectedEntity={mockEntity}
     form={mockDrawerForm}
-    onSubmit={() => {}}
+    onSubmit={mockOnSubmit}
     drawerDisclosure={mockDrawerDisclosure}
   />
 );
 
-// TODO: Test ReportDrawer rendering, opening, closing functionalities
+describe("Test ReportDrawer rendering", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Should render", async () => {
+    mockedUseUser.mockReturnValue(mockStateUser);
+    render(drawerComponent);
+  });
+
+  it("Should render save text for state user", async () => {
+    mockedUseUser.mockReturnValue(mockStateUser);
+    render(drawerComponent);
+    expect(screen.getByText(saveAndCloseText)).toBeVisible();
+  });
+
+  it("Should not render save text for admin user", async () => {
+    mockedUseUser.mockReturnValue(mockAdminUser);
+    render(drawerComponent);
+    expect(screen.queryByText(saveAndCloseText)).not.toBeInTheDocument();
+  });
+});
+
+describe("Test ReportDrawer fill form and close", () => {
+  beforeEach(() => {
+    mockedUseUser.mockReturnValue(mockStateUser);
+    render(drawerComponent);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("ReportDrawer form can be filled and saved for state user", async () => {
+    const testField = screen.getByRole("textbox", {
+      name: /mock drawer text field/i,
+    });
+    await userEvent.type(testField, "valid fill");
+    const saveCloseButton = screen.getByText(saveAndCloseText);
+    expect(saveCloseButton).toBeVisible();
+    await userEvent.click(saveCloseButton);
+    expect(mockOnSubmit).toHaveBeenCalledTimes(1);
+  });
+
+  it("ReportDrawer can be closed with close button", async () => {
+    const closeButton = screen.getByText(closeText);
+    expect(closeButton).toBeVisible();
+    await userEvent.click(closeButton);
+    expect(mockOnClose).toHaveBeenCalledTimes(1);
+  });
+
+  it("ReportDrawer can be closed with cancel button", async () => {
+    const cancelButton = screen.getByText("Cancel");
+    expect(cancelButton).toBeVisible();
+    await userEvent.click(cancelButton);
+    expect(mockOnClose).toHaveBeenCalledTimes(1);
+  });
+});
 
 describe("Test ReportDrawer accessibility", () => {
   it("Should not have basic accessibility issues", async () => {
+    mockedUseUser.mockReturnValue(mockStateUser);
     const { container } = render(drawerComponent);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
+    jest.clearAllMocks();
   });
 });
