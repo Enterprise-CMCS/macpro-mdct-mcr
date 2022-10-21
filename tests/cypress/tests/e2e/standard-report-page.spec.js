d1fe380661e80177a36bf75ce7529bb6682293bc
brittbujacich <britt.bujacich@gmail.com>
2022-10-21 14:45:03 -0600

    wip

--- tests/cypress/tests/e2e/standard-report-page.spec.js
+++ tests/cypress/tests/e2e/standard-report-page.spec.js
@@ -57,76 +57,142 @@ describe("state user enters a program", () => {
 
   it("fills out B: State-Level Indicators", () => {
     cy.get('input[name="state_statewideMedicaidEnrollment"]').type("1020");
-    cy.get('input[name="state_statewideMedicaidManagedCareEnrollment"]').type("1000");
+    cy.get('input[name="state_statewideMedicaidManagedCareEnrollment"]').type(
+      "1000"
+    );
     cy.findByRole("button", { name: "Save & continue" }).click();
 
     cy.get('[type="checkbox"]').check("State actuaries");
     cy.findByRole("button", { name: "Save & continue" }).click();
 
-    cy.get('textarea[name="state_focusedProgramIntegrityActivitiesConducted"]').type("nothing to note");
+    cy.get(
+      'textarea[name="state_focusedProgramIntegrityActivitiesConducted"]'
+    ).type("nothing to note");
     cy.get('[type="radio"]').check("Allow plans to retain overpayments");
-    cy.get('textarea[name="state_overpaymentStandardContractLanguageLocation"]').type("In plan details");
-    cy.get('textarea[name="state_overpaymentStandardDescription"]').type("State returns payments");
-    cy.get('textarea[name="state_overpaymentReportingMonitoringEfforts"]').type("State tracks compliance");
-    cy.get('textarea[name="state_beneficiaryCircumstanceChangeReconciliationEfforts"]').type("State team dedicated to these cases");
-    cy.get('input[name="state_providerTerminationReportingMonitoringEfforts"]').get('[type="radio"]').check("Yes");
-    cy.get('input[name="state_providerTerminationReportingMonitoringMetrics"]').get('[type="radio"]').check("Yes");
-    cy.get('textarea[name="state_providerTerminationReportingMonitoringMetricsDescription"]').type("metric here");
-    cy.get('input[name="state_excludedEntityIdentifiedInFederalDatabaseCheck"]').get('[type="radio"]').check("No");
-    cy.get('input[name="state_ownershipControlDisclosureWebsite"]').get('[type="radio"]').check("No");
-    cy.get('textarea[name="state_submittedDataAuditResults"]').type("https://www.auditwebsite.com");
+    cy.get(
+      'textarea[name="state_overpaymentStandardContractLanguageLocation"]'
+    ).type("In plan details");
+    cy.get('textarea[name="state_overpaymentStandardDescription"]').type(
+      "State returns payments"
+    );
+    cy.get('textarea[name="state_overpaymentReportingMonitoringEfforts"]').type(
+      "State tracks compliance"
+    );
+    cy.get(
+      'textarea[name="state_beneficiaryCircumstanceChangeReconciliationEfforts"]'
+    ).type("State team dedicated to these cases");
+    cy.get('input[name="state_providerTerminationReportingMonitoringEfforts"]')
+      .get('[type="radio"]')
+      .check("Yes");
+    cy.get('input[name="state_providerTerminationReportingMonitoringMetrics"]')
+      .get('[type="radio"]')
+      .check("Yes");
+    cy.get(
+      'textarea[name="state_providerTerminationReportingMonitoringMetricsDescription"]'
+    ).type("metric here");
+    cy.get('input[name="state_excludedEntityIdentifiedInFederalDatabaseCheck"]')
+      .get('[type="radio"]')
+      .check("No");
+    cy.get('input[name="state_ownershipControlDisclosureWebsite"]')
+      .get('[type="radio"]')
+      .check("No");
+    cy.get('textarea[name="state_submittedDataAuditResults"]').type(
+      "https://www.auditwebsite.com"
+    );
     cy.findByRole("button", { name: "Save & continue" }).click();
   });
 
   it("fills out section C", () => {
     cy.get('input[name="program_contractTitle"]').type("Contract Title");
-    cy.get('input[name="program_contractDate"]').type("07142023");    
-    cy.get('input[name="program_contractUrl"]').type("https://www.contracturl.com");
+    cy.get('input[name="program_contractDate"]').type("07142023");
+    cy.get('input[name="program_contractUrl"]').type(
+      "https://www.contracturl.com"
+    );
     cy.get('[type="radio"]').check("Other, specify");
     cy.get('textarea[name="program_type-otherText"]').type("Generic MCP here");
     cy.get('[type="checkbox"]').check("Dental");
-    cy.get('textarea[name="program_specialBenefitsAvailabilityVariation"]').type("N/A");
+    cy.get(
+      'textarea[name="program_specialBenefitsAvailabilityVariation"]'
+    ).type("N/A");
     cy.get('input[name="program_enrollment"]').type("100");
-    cy.get('textarea[name="program_enrollmentBenefitChanges"]').type("Changes to enrollment");
-    cy.findByRole("button", { name: "Save & continue" }).click();  
+    cy.get('textarea[name="program_enrollmentBenefitChanges"]').type(
+      "Changes to enrollment"
+    );
+    cy.findByRole("button", { name: "Save & continue" }).click();
     //III
-    cy.get('input[name="program_encounterDataUses"]').get('[type="checkbox"]').check("Quality/performance measurement");
-    cy.get('input[name="program_encounterDataUses"]').get('[type="checkbox"]').check("Timeliness of data corrections");
-    cy.get('input[name="program_encounterDataSubmissionCorrectionPerformanceEvaluationCriteria"]').get('[type="checkbox"]').check("Timeliness of data certifications");
-    cy.get('input[name="program_encounterDataSubmissionCorrectionPerformanceEvaluationCriteria"]').get('[type="checkbox"]').check("Use of correct file formats");
-    cy.get('textarea[name="program_encounterDataSubmissionCorrectionPerformanceEvaluationCriteriaContractLanguageLocation"]')
-      .type("References here");
-    cy.get('textarea[name="program_encounterDataSubmissionQualityFinancialPenaltiesContractLanguageLocation"]')
-      .type("Financial penalties");
-    cy.get('textarea[name="program_encounterDataQualityIncentives"]')
-      .type("N/A");
-    cy.get('textarea[name="program_encounterDataCollectionValidationBarriers"]')
-      .type("Barriers to collecting data");
+    cy.get('input[name="program_encounterDataUses"]')
+      .get('[type="checkbox"]')
+      .check("Quality/performance measurement");
+    cy.get('input[name="program_encounterDataUses"]')
+      .get('[type="checkbox"]')
+      .check("Timeliness of data corrections");
+    cy.get(
+      'input[name="program_encounterDataSubmissionCorrectionPerformanceEvaluationCriteria"]'
+    )
+      .get('[type="checkbox"]')
+      .check("Timeliness of data certifications");
+    cy.get(
+      'input[name="program_encounterDataSubmissionCorrectionPerformanceEvaluationCriteria"]'
+    )
+      .get('[type="checkbox"]')
+      .check("Use of correct file formats");
+    cy.get(
+      'textarea[name="program_encounterDataSubmissionCorrectionPerformanceEvaluationCriteriaContractLanguageLocation"]'
+    ).type("References here");
+    cy.get(
+      'textarea[name="program_encounterDataSubmissionQualityFinancialPenaltiesContractLanguageLocation"]'
+    ).type("Financial penalties");
+    cy.get('textarea[name="program_encounterDataQualityIncentives"]').type(
+      "N/A"
+    );
+    cy.get(
+      'textarea[name="program_encounterDataCollectionValidationBarriers"]'
+    ).type("Barriers to collecting data");
     cy.findByRole("button", { name: "Save & continue" }).click();
 
     //IV
     cy.get('textarea[name="program_criticalIncidentDefinition"]').type("N/A");
-    cy.get('textarea[name="program_standardAppealTimelyResolutionDefinition"]').type("No longer than 30 calendar days");
-    cy.get('textarea[name="program_expeditedAppealTimelyResolutionDefinition"]').type("No longer than 72 hours");
-    cy.get('textarea[name="program_grievanceTimelyResolutionDefinition"]').type("Within 90 days");
+    cy.get(
+      'textarea[name="program_standardAppealTimelyResolutionDefinition"]'
+    ).type("No longer than 30 calendar days");
+    cy.get(
+      'textarea[name="program_expeditedAppealTimelyResolutionDefinition"]'
+    ).type("No longer than 72 hours");
+    cy.get('textarea[name="program_grievanceTimelyResolutionDefinition"]').type(
+      "Within 90 days"
+    );
     cy.findByRole("button", { name: "Save & continue" }).click();
 
     //V
-    cy.get('textarea[name="program_networkAdequacyChallenges"]').type("Challenges here");
-    cy.get('textarea[name="program_networkAdequacyGapResponseEfforts"]').type("Addressing the gaps");
-    cy.findByRole("button", { name: "Save & continue" }).click(); 
+    cy.get('textarea[name="program_networkAdequacyChallenges"]').type(
+      "Challenges here"
+    );
+    cy.get('textarea[name="program_networkAdequacyGapResponseEfforts"]').type(
+      "Addressing the gaps"
+    );
+    cy.findByRole("button", { name: "Save & continue" }).click();
     cy.findByRole("button", { name: "Add access measure" }).click();
-    cy.get('[type="radio"]').check("General quantitative availability and accessibility standard");
-    cy.get('textarea[name="accessMeasure_standardDescription"]').type("Wait times");
+    cy.get('[type="radio"]').check(
+      "General quantitative availability and accessibility standard"
+    );
+    cy.get('textarea[name="accessMeasure_standardDescription"]').type(
+      "Wait times"
+    );
     cy.get('[type="radio"]').check("Appointment wait time");
     cy.get("button[type=submit]").contains("Save").click();
     cy.findByRole("button", { name: "Continue" }).click();
 
-     //IX
+    //IX
     cy.get('textarea[name="state_bssWebsite"]').type("websites here");
-    cy.get('textarea[name="state_bssEntityServiceAccessibility"]').type("In person, phone, email");
-    cy.get('textarea[name="state_bssEntityLtssProgramDataIssueAssistance"]').type("Assistance here");
-    cy.get('textarea[name="state_bssEntityPerformanceEvaluationMethods"]').type("Performance evaluations");
+    cy.get('textarea[name="state_bssEntityServiceAccessibility"]').type(
+      "In person, phone, email"
+    );
+    cy.get(
+      'textarea[name="state_bssEntityLtssProgramDataIssueAssistance"]'
+    ).type("Assistance here");
+    cy.get('textarea[name="state_bssEntityPerformanceEvaluationMethods"]').type(
+      "Performance evaluations"
+    );
     cy.findByRole("button", { name: "Save & continue" }).click();
 
     //X
@@ -134,8 +200,8 @@ describe("state user enters a program", () => {
     cy.findByRole("button", { name: "Save & continue" }).click();
   });
 
-  it ("Fills out Section D", () => {
-    // running into a problem here, the plans I created don't show up here 
+  it("Fills out Section D", () => {
+    // running into a problem here, the plans I created don't show up here
     cy.findByRole("button", { name: "Enter details" }).click();
     cy.get('[type="radio"]').check("Primary care");
     cy.get('[type="radio"]').check("Urban");
@@ -146,11 +212,10 @@ describe("state user enters a program", () => {
     cy.get("button[type=submit]").contains("Save & Close").click();
     cy.findByRole("button", { name: "Continue" }).click();
 
-    cy.get('button').contains('Enter');
+    cy.get("button").contains("Enter");
     cy.findAllByRole("button", { name: "Enter" }).first().click();
     cy.findAllByRole("button", { name: "Cancel" }).click();
     cy.findByRole("button", { name: "Continue" }).click();
-
   });
 
   it("submits the program", () => {
