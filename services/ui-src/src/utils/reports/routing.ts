341956d4f5a06daf9e8846097b6f566772fe674d
Garrett Rabian <garrett@coforma.io>
2022-11-01 11:29:50 -0400

    abstract routes from template

--- services/ui-src/src/utils/reports/routing.ts
+++ services/ui-src/src/utils/reports/routing.ts
@@ -1,24 +1,35 @@
 import { useLocation } from "react-router-dom";
+import { isMcparReportFormPage, mcparReportRoutesFlat } from "forms/mcpar";
 
-export const useFindRoute = (routeArray: any[], fallbackRoute: string) => {
+const getRoutingStructure = (pathname: string) => {
+  if (isMcparReportFormPage(pathname)) {
+    return { fallbackRoute: "/mcpar", routeArray: mcparReportRoutesFlat };
+  }
+  return { fallbackRoute: "/", routeArray: undefined };
+};
+
+export const useFindRoute = () => {
   const { pathname } = useLocation();
+  const { fallbackRoute, routeArray } = getRoutingStructure(pathname);
   let calculatedRoutes = {
     previousRoute: fallbackRoute,
     nextRoute: fallbackRoute,
   };
   // find current route and position in array
-  const currentRouteObject = routeArray.find(
-    (route: any) => route.path === pathname
-  );
-  const currentPosition = routeArray.indexOf(currentRouteObject);
-  if (currentRouteObject) {
-    // set previousRoute to previous path || base route
-    const previousRoute =
-      routeArray[currentPosition - 1]?.path || fallbackRoute;
-    calculatedRoutes.previousRoute = previousRoute;
-    // set nextRoute to next path || base route
-    const nextRoute = routeArray[currentPosition + 1]?.path || fallbackRoute;
-    calculatedRoutes.nextRoute = nextRoute;
+  if (routeArray) {
+    const currentRouteObject = routeArray.find(
+      (route: any) => route.path === pathname
+    );
+    if (currentRouteObject) {
+      const currentPosition = routeArray.indexOf(currentRouteObject);
+      // set previousRoute to previous path || base route
+      const previousRoute =
+        routeArray[currentPosition - 1]?.path || fallbackRoute;
+      calculatedRoutes.previousRoute = previousRoute;
+      // set nextRoute to next path || base route
+      const nextRoute = routeArray[currentPosition + 1]?.path || fallbackRoute;
+      calculatedRoutes.nextRoute = nextRoute;
+    }
   }
   return calculatedRoutes;
 };
