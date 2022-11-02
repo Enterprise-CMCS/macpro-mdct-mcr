2b3f6c3f7cfb12ba2c44d66c23f789f486c58960
Garrett Rabian <garrett@coforma.io>
2022-11-02 17:40:58 -0400

    more cleanup

--- services/ui-src/src/utils/validation/schemas.test.ts
+++ services/ui-src/src/utils/validation/schemas.test.ts
@@ -14,7 +14,7 @@ describe("Schemas", () => {
     "N/A",
     "Data not available",
   ];
-  const badNumberTestCases = ["abc", "N", "", "123:123", "!@#!@%"];
+  const badNumberTestCases = ["abc", "N", "", "!@#!@%"];
 
   const goodRatioTestCases = [
     "1:1",
@@ -48,12 +48,12 @@ describe("Schemas", () => {
     }
   };
 
-  test("Evalulate Number Schema using number scheme", () => {
+  test("Evaluate Number Schema using number scheme", () => {
     testNumberSchema(number(), goodNumberTestCases, true);
     testNumberSchema(number(), badNumberTestCases, false);
   });
 
-  test("Evalulate Number Schema using ratio scheme", () => {
+  test("Evaluate Number Schema using ratio scheme", () => {
     testNumberSchema(ratio(), goodRatioTestCases, true);
     testNumberSchema(ratio(), badRatioTestCases, false);
   });
