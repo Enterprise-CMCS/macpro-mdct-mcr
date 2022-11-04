acb8ceef539680764c4cd92dec903cddb1425214
karla-vm <karla@coforma.io>
2022-11-04 15:42:23 -0400

    let's see

--- tests/cypress/tests/e2e/admin-dashboard.spec.js
+++ tests/cypress/tests/e2e/admin-dashboard.spec.js
@@ -11,6 +11,7 @@ before(() => {
   cy.findByRole("button", { name: "Enter MCPAR online" }).click();
   cy.findAllByRole("button", { name: "Enter MCPAR online" }).click();
   cy.findByRole("button", { name: "Add managed care program" }).click();
+
   // fill out form fields
   cy.findByLabelText("Program name").type("program title");
   cy.get('input[name="reportingPeriodStartDate"]').type("07142023");
@@ -37,7 +38,7 @@ describe("Admin Dashboard integration tests", () => {
   it("Enter a report", () => {
     // enter report
     cy.location("pathname").should("match", /mcpar/);
-    cy.get(enterProgramButton).click({ multiple: true });
+    cy.findAllByRole("button", { name: "Enter" }).first().click();
     cy.location("pathname").should(
       "match",
       /^\/mcpar\/program-information\/point-of-contact$/i
@@ -46,7 +47,7 @@ describe("Admin Dashboard integration tests", () => {
 
   it("Archive a report successfully", () => {
     // archive a report
-    cy.findAllByRole("button", { name: "Archive" }).click({ multiple: true });
+    cy.findAllByRole("button", { name: "Archive" }).first().click();
     cy.get(enterProgramButton).should("be.disabled");
     cy.findByRole("button", { name: "Archive" }).should("not.exist");
   });
@@ -54,7 +55,7 @@ describe("Admin Dashboard integration tests", () => {
   it("Unarchive a report successfully", () => {
     // unarchive a report
     cy.findByRole("button", { name: "Unarchive" }).should("exist");
-    cy.findAllByRole("button", { name: "Unarchive" }).click({ multiple: true });
+    cy.findByRole("button", { name: "Unarchive" }).first().click();
     cy.get(enterProgramButton).should("not.be.disabled");
     cy.findByRole("button", { name: "Unarchive" }).should("not.exist");
     cy.findByRole("button", { name: "Archive" }).should("exist");
