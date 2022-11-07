81d66b7bb5d0d38b58285ff4726f0c9e60e8bf54
Garrett <57802560+gmrabian@users.noreply.github.com>
2022-11-07 17:02:27 -0500

    Cypress login (#4718)
    * faster tests?
    
    * auth sessions
    
    * upgrade action to 10
    
    * final cleanup
    
    * linting
    
    * merge main and fix config
    
    * move setup
    
    * try eq?
    
    * no idea
    
    * pls
    
    * bug not related to cypress
    
    * pr feedback
    
    * Thomas's magic
    
    * final checks

--- tests/cypress/tests/accessibility/profile.spec.js
+++ tests/cypress/tests/accessibility/profile.spec.js
@@ -1,14 +1,9 @@
 import { checkCurrentRouteAccessibility } from "../../support/accessibility";
-// selectors
-const menuButton = '[data-testid="header-menu-dropdown-button"]';
-const profileButton = '[data-testid="header-menu-option-manage-account"]';
 
 describe("Baseline /profile accessibility check", () => {
   beforeEach(() => {
-    cy.visit("/");
     cy.authenticate("stateUser");
-    cy.get(menuButton).click();
-    cy.get(profileButton).click();
+    cy.visit("/profile");
   });
 
   checkCurrentRouteAccessibility();
