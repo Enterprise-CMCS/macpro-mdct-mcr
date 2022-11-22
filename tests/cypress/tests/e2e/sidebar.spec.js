68482d0cfe75608ad7d8e3cea54aa12eed36a6c0
Nicholas Summers <nick@coforma.io>
2022-11-22 13:23:24 -0500

    Remove before and aftereach hooks due to failing action

--- tests/cypress/tests/e2e/sidebar.spec.js
+++ tests/cypress/tests/e2e/sidebar.spec.js
@@ -5,35 +5,24 @@ const expandCollapseSidebar = "Open/Close sidebar menu";
 const subsectionText = ".chakra-link .level-2";
 const sectionLink = ".chakra-link .level-1";
 
-beforeEach(() => {
-  cy.authenticate("stateUser");
-  cy.findByRole("button", { name: "Enter MCPAR online" }).click();
-  cy.findAllByRole("button", { name: "Enter MCPAR online" }).click();
-
-  // Create Report & nav to it
-  cy.findByRole("button", { name: "Add managed care program" }).click();
-  cy.findByLabelText("Program name").type(
-    "automated test - " + new Date().toISOString()
-  );
-  cy.get('input[name="reportingPeriodStartDate"]').type("07142023");
-  cy.get('input[name="reportingPeriodEndDate"]').type("07142026");
-  cy.findByRole("checkbox").focus().click();
-  cy.get("button[type=submit]").contains("Save").click();
-  cy.findAllByRole("button", { name: "Enter" }).first().click();
-});
-
-afterEach(() => {
-  cy.navigateToHomePage();
-  cy.authenticate("adminUser");
-  cy.get('[name="state"]').select("Minnesota");
-  cy.findAllByRole("button", { name: "Go to Report Dashboard" }).last().click();
-  cy.findAllByRole("button", { name: "Archive" }).last().click();
-  cy.contains("Archive").should("not.exist");
-  cy.contains("Unarchive").should("be.visible");
-});
-
 describe("Sidebar integration tests", () => {
   it("The sidebar can be navigated at multiple depths, references the selected items, and can be", () => {
+    // Sign in as a state user
+    cy.authenticate("stateUser");
+    cy.findByRole("button", { name: "Enter MCPAR online" }).click();
+    cy.findAllByRole("button", { name: "Enter MCPAR online" }).click();
+
+    // Create Report & nav to it
+    cy.findByRole("button", { name: "Add managed care program" }).click();
+    cy.findByLabelText("Program name").type(
+      "automated test - " + new Date().toISOString()
+    );
+    cy.get('input[name="reportingPeriodStartDate"]').type("07142023");
+    cy.get('input[name="reportingPeriodEndDate"]').type("07142026");
+    cy.findByRole("checkbox").focus().click();
+    cy.get("button[type=submit]").contains("Save").click();
+    cy.findAllByRole("button", { name: "Enter" }).first().click();
+
     // Expand next section, collapse first, nav to new page.
     cy.get(expandButton).first().click();
     cy.get(collapseButton).first().click();
@@ -67,5 +56,14 @@ describe("Sidebar integration tests", () => {
       .click()
       .parent()
       .should("have.class", "open");
+
+    cy.navigateToHomePage();
+    cy.authenticate("adminUser");
+    cy.get('[name="state"]').select("Minnesota");
+    cy.findAllByRole("button", { name: "Go to Report Dashboard" })
+      .last()
+      .click();
+    cy.findAllByRole("button", { name: "Archive" }).last().click();
+    cy.contains("Unarchive").should("be.visible");
   });
 });
