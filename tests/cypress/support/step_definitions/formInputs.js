69745fff9d25895f5f7dbf9ed739cf0b85c99936
Nick Summers <ntsummers1@gmail.com>
2022-11-23 10:08:32 -0500

    Britt and Nicks Most Excellent MCPAR Dashboard Integration Tests (#4805)
    * Add Cucumber/Gherkin tests for creating a program
    
    * Swap to Gherkin/Cucumber for MCPAR Dashboard and Form
    
    * Finish up Form input
    
    * text fic
    
    * text fix
    
    * fix
    
    * fix
    
    * test fix
    
    * update from some PR review comments
    
    * Thomas Feedback Edits
    
    * Removed now old integration tests
    
    * Add to github action
    
    * Clean up after self in Sidebar E2E test
    
    * Fix variable usage in Accessibility tests
    
    * Remove added quotes for Accessibility examples
    
    * Swap quotes on get for sidebar call
    
    * Swap quotes on get for sidebar call
    
    * Remove before and aftereach hooks due to failing action
    
    * Attempt to define more clear creation of programs
    
    * Navigate to homepage added for sidebar
    
    * Comment out for quicker screenshot debug
    
    Co-authored-by: Nicholas Summers <nick@coforma.io>
    Co-authored-by: Britt <britt.bujacich@gmail.com>
    Co-authored-by: Garrett <57802560+gmrabian@users.noreply.github.com>

--- tests/cypress/support/step_definitions/formInputs.js
+++ tests/cypress/support/step_definitions/formInputs.js
@@ -1,11 +1,67 @@
 import { When } from "@badeball/cypress-cucumber-preprocessor";
-When("these form elements are filled:", (dataTable) => {
+When("these form elements are edited:/filled:", (dataTable) => {
   dataTable.rawTable.forEach((row) => {
-    cy.get(`[name='${row[0]}']`).type(row[1]);
+    /*
+     * Repeated inputs have the same name, so it comes back as an array. Thus we need to grab
+     * which input in the array we need. Otherwise we can just query the name of the input
+     */
+    const repeatedInput = row?.[3];
+    const input = repeatedInput
+      ? cy.get(`input[name^="${row[0]}"]`)
+      : cy.get(`[name='${row[0]}']`);
+    const inputType = row[1];
+    const inputValue = row[2];
+    switch (inputType) {
+      case "singleCheckbox":
+        if (inputValue == "true") input.check();
+        else input.uncheck();
+        break;
+      case "radio":
+        input.check(inputValue);
+        break;
+      case "checkbox":
+        input.check(inputValue);
+        break;
+      case "dropdown":
+        input.select(inputValue);
+        break;
+      case "repeated":
+        input.eq(repeatedInput).clear().type(inputValue);
+        break;
+      default:
+        input.clear().type(inputValue);
+        break;
+    }
+  });
+});
+
+When("these form elements are prefilled and disabled:", (dataTable) => {
+  dataTable.rawTable.forEach((row) => {
+    switch (row[1]) {
+      default:
+        cy.get(`[name='${row[0]}']`)
+          .should("have.value", row[2])
+          .should("be.disabled");
+        break;
+    }
+  });
+});
+
+When("these form elements are prefilled:", (dataTable) => {
+  dataTable.rawTable.forEach((row) => {
+    switch (row[1]) {
+      default:
+        cy.get(`[name='${row[0]}']`).should("have.value", row[2]);
+        break;
+    }
   });
 });
 
 When("the form is submitted", () => {
   const submitButton = "[type='submit']";
   cy.get(submitButton).focus().click();
+});
+
+When("I click the {string} button", (name) => {
+  cy.findAllByRole("button", { name }).last().click();
 });
