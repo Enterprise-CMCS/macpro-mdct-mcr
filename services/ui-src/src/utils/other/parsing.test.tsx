ca6389331887f6da008e2d30feca0ac4bccd4ffd
Scott Wambach <scott@scottwamba.ch>
2022-12-01 13:51:18 -0600

    Update parsing.test.tsx

--- services/ui-src/src/utils/other/parsing.test.tsx
+++ services/ui-src/src/utils/other/parsing.test.tsx
@@ -64,13 +64,13 @@ describe("Test Parsing for PDF Preview Fields", () => {
       pdfPreviewTableNumberParse({ label: "A.1 Label", hint: "Hint" })
     ).toEqual({
       prefix: "A.1",
-      suffix: '<p class="heading">Label</p><p>Hint</p>',
+      suffix: "<p><strong>Label</strong></p><p>Hint</p>",
     });
   });
   test("The field names are separated properly without the hint", () => {
     expect(pdfPreviewTableNumberParse({ label: "A.1 Label" })).toEqual({
       prefix: "A.1",
-      suffix: '<p class="heading">Label</p>',
+      suffix: "<p><strong>Label</strong></p>",
     });
   });
 });
