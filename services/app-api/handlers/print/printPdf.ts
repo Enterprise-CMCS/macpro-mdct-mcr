ac4463a1ac59613dc0efc9a09b97d45ea3bf3f99
juliuscaesar <caesarnuzzolo@Coforma-WTWWRT94NR-Caesar-Nuzzolo.local>
2022-11-30 21:16:07 -0500

    add endpoint

---
+++ services/app-api/handlers/print/printPdf.ts
@@ -0,0 +1,46 @@
+import handler from "../handler-lib";
+import AWS from "aws-sdk";
+import axios from "axios";
+
+/**
+ * Calls the CMS Prince PDF Generator to generate a PDF of the provided HTML
+ */
+export const printPdf = handler(async (event, _context) => {
+  // Throw an error if there is no provided HTML
+  const body = event.body ? JSON.parse(event.body) : null;
+
+  if (!body || !body.encodedHtml) {
+    throw new Error("Missing required encodedHtml");
+  }
+
+  const host = process.env.PRINCE_API_HOST;
+  const path = process.env.PRINCE_API_PATH;
+
+  const params = {
+    method: "POST",
+    url: `https://${host}${path}`,
+    host: host,
+    path: path,
+    region: "us-east-1",
+    service: "execute-api",
+    body: body.encodedHtml,
+  };
+
+  // Sign auth, and massage the format for axios
+  const aws4 = require("aws4");
+  const credentials = AWS.config.credentials;
+  if (!credentials) {
+    throw new Error("No config found to make request to PDF API");
+  }
+  var signedRequest = aws4.sign(params, {
+    secretAccessKey: credentials.secretAccessKey,
+    accessKeyId: credentials.accessKeyId,
+    sessionToken: credentials.sessionToken,
+  });
+
+  delete signedRequest.body; // Remove body after signing, contained in data, don't need to duplicate it
+
+  // Execute
+  let response = await axios(signedRequest);
+  return { data: response.data };
+});
