e5063514797099343ca5ab80759eeaa1cedeef20
brittbujacich <britt.bujacich@gmail.com>
2022-12-12 14:26:03 -0700

    add print button to review page

--- services/ui-src/src/verbiage/pages/mcpar/mcpar-review-and-submit.ts
+++ services/ui-src/src/verbiage/pages/mcpar/mcpar-review-and-submit.ts
@@ -4,8 +4,6 @@ export default {
       header: "Review & Submit",
       infoHeader: "Ready to Submit?",
       info: "Double check that everything in your MCPAR Report is accurate. You will be able to make edits after submitting, and resubmit. Once you’ve reviewed your report, certify that it’s in compliance with 42 CFR § 438.66(e).",
-      printPageUrl: "/mcpar/export",
-      printButtonText: "Print",
     },
     modal: {
       structure: {
