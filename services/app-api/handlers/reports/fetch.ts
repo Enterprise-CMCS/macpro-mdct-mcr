1d31b6630e2d1f115d38fc9929336fb42a5a832a
karla-vm <karla@coforma.io>
2022-12-19 13:11:16 -0500

    extract fetch changes

--- services/app-api/handlers/reports/fetch.ts
+++ services/app-api/handlers/reports/fetch.ts
@@ -1,32 +1,59 @@
+import KSUID from "ksuid";
 import handler from "../handler-lib";
 import dynamoDb from "../../utils/dynamo/dynamodb-lib";
 import { AnyObject, StatusCodes } from "../../utils/types/types";
 import error from "../../utils/constants/constants";
+import { S3 } from "aws-sdk";
 
 export const fetchReport = handler(async (event, _context) => {
+  // console.log("Fetching Report");
   if (!event?.pathParameters?.state! || !event?.pathParameters?.id!) {
     throw new Error(error.NO_KEY);
   }
+  const state = event.pathParameters.state;
+  const reportId = event.pathParameters.id;
+
+  const fieldDataId: string = KSUID.randomSync().string;
+  const formTemplateId: string = KSUID.randomSync().string;
+
+  const s3 = new S3();
+  const templateParams = {
+    Bucket: "database-winter-storm-create-mcpar-446712541566",
+    Key: "/formTemplates/" + state + "/" + formTemplateId,
+  };
+
+  const template = await getObjectWrapper(s3, templateParams);
+
+  const dataParams = {
+    Bucket: "database-winter-storm-create-mcpar-446712541566",
+    Key: "/fieldData/" + state + "/" + fieldDataId,
+  };
+  const data = await getObjectWrapper(s3, dataParams);
+
   const params = {
     TableName: process.env.MCPAR_REPORT_TABLE_NAME!,
     Key: {
       state: event.pathParameters.state,
-      id: event.pathParameters.id,
+      id: reportId,
     },
   };
   const response = await dynamoDb.get(params);
 
   let status = StatusCodes.SUCCESS;
-  if (!response?.Item) {
+  if (!response?.Item || !template || !data) {
     status = StatusCodes.NOT_FOUND;
   }
+
+  const report: any = response.Item;
+
   return {
     status: status,
-    body: response.Item,
+    body: { ...report, formTemplate: template, fieldData: data },
   };
 });
 
 export const fetchReportsByState = handler(async (event, _context) => {
+  // console.log("Fetching Reports By State");
   if (!event?.pathParameters?.state!) {
     throw new Error(error.NO_KEY);
   }
@@ -60,15 +87,7 @@ export const fetchReportsByState = handler(async (event, _context) => {
   do {
     [startingKey, results] = await queryTable(startingKey);
 
-    /*
-     * Remove formTemplate and formData to get rid of excessive size that isn't needed
-     * on the dashboard when this call is used
-     */
     const items: AnyObject[] = results.Items;
-    items.forEach((item: any) => {
-      delete item.formTemplate;
-      delete item.formData;
-    });
     existingItems.push(...items);
   } while (startingKey);
 
@@ -77,3 +96,16 @@ export const fetchReportsByState = handler(async (event, _context) => {
     body: existingItems,
   };
 });
+
+const getObjectWrapper = (s3: S3, params: { Bucket: string; Key: string }) => {
+  return new Promise((resolve, reject) => {
+    s3.getObject(params, function (err: any, result: any) {
+      if (err) {
+        reject(err);
+      }
+      if (result) {
+        resolve(JSON.parse(result.Body));
+      }
+    });
+  });
+};
