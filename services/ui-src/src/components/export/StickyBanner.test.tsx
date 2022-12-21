364ddc5ce9c3ce08befcff61a7ec2595d5c43f83
karla-vm <karla@coforma.io>
2022-12-21 11:10:33 -0500

    coverage!

--- services/ui-src/src/components/export/StickyBanner.test.tsx
+++ services/ui-src/src/components/export/StickyBanner.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
 // components
 import { StickyBanner } from "./StickyBanner";
@@ -10,6 +11,13 @@ describe("Sticky Banner", () => {
     render(stickyBannerComponent);
     const banner = screen.getByTestId("stickyBanner");
     expect(banner).toBeVisible();
+  });
+
+  test("Download PDF button should be visible", async () => {
+    render(stickyBannerComponent);
+    const printButton = screen.getByText("Download PDF");
+    expect(printButton).toBeVisible();
+    await userEvent.click(printButton);
   });
 });
 
