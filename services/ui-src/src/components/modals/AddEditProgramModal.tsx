c7566039b94fb2b87ac044f3ba34b151e01d1cc7
karla-vm <karla@coforma.io>
2022-12-22 16:51:40 -0500

    cleaning up comment

--- services/ui-src/src/components/modals/AddEditProgramModal.tsx
+++ services/ui-src/src/components/modals/AddEditProgramModal.tsx
@@ -65,7 +65,6 @@ export const AddEditProgramModal = ({
         state: activeState,
         id: selectedReport.id,
       };
-      // TODO: FIX THIS
 
       // edit existing report
       await updateReport(reportKeys, {
