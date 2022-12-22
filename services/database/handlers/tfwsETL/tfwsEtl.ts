0acefa231ee6eb4f6d58ae08dffdcb22482bed48
Thomas Blackwell <thomas.blackwell@coforma.io>
2022-12-22 11:55:23 -0500

    ETL update, s3 pending

--- services/database/handlers/tfwsETL/tfwsEtl.ts
+++ services/database/handlers/tfwsETL/tfwsEtl.ts
@@ -2,9 +2,10 @@
 /* eslint-disable */
 import { DynamoDB, S3 } from "aws-sdk";
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
+const KSUID = require("ksuid");
 
 const TABLE_NAME = process.env.MCPAR_REPORT_TABLE_NAME!;
-//const BUCKET_NAME = process.env.MCPAR_FORM_BUCKET!;
+const BUCKET_NAME = process.env.MCPAR_FORM_BUCKET!;
 
 let dynamoClient: any;
 let s3Client: any;
@@ -27,8 +28,31 @@ export const handler = async (
         keepSearching,
         startingKey
       );
-      results.Items.forEach((item: { Key: any }) => console.log(item.Key));
-      console.log(`results`);
+      results.Items.forEach((report: any) => {
+        const formTemplate = report.formTemplate;
+        const fieldData = report.fieldData;
+        if (!formTemplate || !fieldData) {
+          console.error("Report doesn't have fieldData or formTemplate", {
+            state: report.state,
+            id: report.id,
+          });
+          return;
+        }
+
+        const formTemplateId = KSUID.randomSync().string;
+        const fieldDataId = KSUID.randomSync().string;
+
+        writeToS3("formTemplates", report.state, formTemplateId, formTemplate);
+        writeToS3("fieldData", report.state, fieldDataId, fieldData);
+
+        //Write new format back into DynamoDB
+        delete report.formTemplate;
+        delete report.fieldData;
+        report.formTemplateId = formTemplateId;
+        report.fieldDataId = fieldDataId;
+
+        writeItemToDb(report);
+      });
     } catch (err) {
       console.error(`Database scan failed for the table ${TABLE_NAME}
                      with startingKey ${startingKey} and the keepSearching flag is ${keepSearching}.
@@ -45,18 +69,63 @@ export const handler = async (
   };
 };
 
-const initialize = () => {
-  const awsConfig: any = {};
-  const endpoint = process.env.DYNAMODB_URL;
-  if (endpoint) {
-    awsConfig.endpoint = endpoint;
-    awsConfig.accessKeyId = "LOCAL_FAKE_KEY"; // pragma: allowlist secret
-    awsConfig.secretAccessKey = "LOCAL_FAKE_SECRET"; // pragma: allowlist secret
-  } else {
-    awsConfig["region"] = "us-east-1";
+const writeToS3 = async (
+  directory: string,
+  state: string,
+  name: string,
+  data: any
+) => {
+  const bucketParams = {
+    Bucket: BUCKET_NAME,
+    Key: `${directory}/${state}/${name}.json`,
+    Body: JSON.stringify(data),
+  };
+  console.debug("Writing to S3", bucketParams);
+  //TODO: await s3Client.putObject(bucketParams);
+};
+
+const writeItemToDb = async (item: any) => {
+  console.log("Writing changes to table: ", TABLE_NAME);
+
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      ...item,
+    },
+    ReturnValues: "ALL_OLD",
+  };
+  try {
+    const response = await dynamoClient.put(params).promise();
+    console.log(`Updated: ${item.state} - ${item.id}`);
+  } catch (e) {
+    console.error("error", e);
   }
-  dynamoClient = new DynamoDB.DocumentClient(awsConfig);
-  s3Client = new S3(awsConfig);
+};
+
+const initialize = () => {
+  const ddbConfig: any = {};
+  const s3Config: any = {};
+  const ddbEndpoint = process.env.DYNAMODB_URL;
+  const s3Endpoint = process.env.S3_URL;
+  if (ddbEndpoint) {
+    ddbConfig.endpoint = ddbEndpoint;
+    ddbConfig.accessKeyId = "LOCAL_FAKE_KEY"; // pragma: allowlist secret
+    ddbConfig.secretAccessKey = "LOCAL_FAKE_SECRET"; // pragma: allowlist secret
+  } else {
+    ddbConfig.region = "us-east-1";
+  }
+  dynamoClient = new DynamoDB.DocumentClient(ddbConfig);
+
+  if (s3Endpoint) {
+    s3Config.endpoint = s3Endpoint;
+    s3Config.region = "localhost";
+    s3Config.s3ForcePathStyle = true;
+    s3Config.accessKeyId = "S3RVER"; // pragma: allowlist secret
+    s3Config.secretAccessKey = "S3RVER"; // pragma: allowlist secret
+  } else {
+    s3Config.region = "us-east-1";
+  }
+  s3Client = new S3(s3Config);
 };
 
 const scanTable = async (
