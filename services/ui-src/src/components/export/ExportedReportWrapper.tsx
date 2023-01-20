0d24a3e3922590b6c271922f356ba80b89df277b
karla-vm <karla@coforma.io>
2023-01-20 15:01:12 -0500

    refactor wrapper

--- services/ui-src/src/components/export/ExportedReportWrapper.tsx
+++ services/ui-src/src/components/export/ExportedReportWrapper.tsx
@@ -14,32 +14,19 @@ import {
 } from "types";
 
 export const ExportedReportWrapper = ({ section }: Props) => {
-  switch (section.pageType) {
-    case PageTypes.STANDARD:
-      return (
-        <Box data-testid="exportedStandardReportSection" mt="2rem">
-          <ExportedReportFieldTable
-            section={section as StandardReportPageShape}
-          />
-        </Box>
-      );
-    case PageTypes.DRAWER:
-      return (
-        <Box data-testid="exportedDrawerReportSection" mt="2rem">
-          <ExportedReportFieldTable
-            section={section as DrawerReportPageShape}
-          />
-        </Box>
-      );
-    case PageTypes.MODAL_DRAWER:
-      return (
-        <ExportedModalDrawerReportSection
-          section={section as ModalDrawerReportPageShape}
-        />
-      );
-    default:
-      return <></>;
-  }
+  return section.pageType === PageTypes.STANDARD ? (
+    <Box data-testid="exportedStandardReportSection" mt="2rem">
+      <ExportedReportFieldTable section={section as StandardReportPageShape} />
+    </Box>
+  ) : section.pageType === PageTypes.DRAWER ? (
+    <Box data-testid="exportedDrawerReportSection" mt="2rem">
+      <ExportedReportFieldTable section={section as DrawerReportPageShape} />
+    </Box>
+  ) : (
+    <ExportedModalDrawerReportSection
+      section={section as ModalDrawerReportPageShape}
+    />
+  );
 };
 
 export interface Props {
