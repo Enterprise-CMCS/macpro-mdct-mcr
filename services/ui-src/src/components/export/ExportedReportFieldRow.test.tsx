2ddf7a700757482c387ef2c229285c6308b1719f
BearHanded <bcm3223@gmail.com>
2023-01-24 09:16:14 -0500

    some coverage for row component

--- services/ui-src/src/components/export/ExportedReportFieldRow.test.tsx
+++ services/ui-src/src/components/export/ExportedReportFieldRow.test.tsx
@@ -3,12 +3,13 @@ import { axe } from "jest-axe";
 import { ExportedReportFieldRow } from "./ExportedReportFieldRow";
 import { mockReportContext } from "utils/testing/setupJest";
 import { ReportContext } from "components";
+import { Table } from "@chakra-ui/react";
 
 const field = {
   id: "test",
   validation: "string",
-  type: "dynamic",
-  props: { hint: "hint" },
+  type: "drawer",
+  props: { hint: "hint", number: 123 },
 };
 const otherTextField = {
   id: "test-otherText",
@@ -16,35 +17,59 @@ const otherTextField = {
   type: "dynamic",
   props: { hint: "hint" },
 };
+const fieldWithLabel = {
+  id: "test",
+  validation: "string",
+  type: "drawer",
+  props: { hint: "hint", label: "test label" },
+};
 
 const exportRow = (
   <ReportContext.Provider value={mockReportContext}>
-    <ExportedReportFieldRow formField={field} pageType="drawer" />
+    <Table sx={{}}>
+      <ExportedReportFieldRow formField={field} pageType="drawer" />
+    </Table>
   </ReportContext.Provider>
 );
 
 const otherTextRow = (
   <ReportContext.Provider value={mockReportContext}>
-    <ExportedReportFieldRow formField={otherTextField} pageType="drawer" />
+    <Table sx={{}}>
+      <ExportedReportFieldRow formField={otherTextField} pageType="drawer" />
+    </Table>
+  </ReportContext.Provider>
+);
+
+const dynamicRow = (
+  <ReportContext.Provider value={mockReportContext}>
+    <Table sx={{}}>
+      <ExportedReportFieldRow formField={fieldWithLabel} pageType="drawer" />
+    </Table>
   </ReportContext.Provider>
 );
 
 describe("ExportedReportFieldRow", () => {
-  test.skip("Is present", async () => {
+  test("Is present", async () => {
     render(exportRow);
-    const row = screen.getByTestId("export-row");
+    const row = screen.getByTestId("exportRow");
     expect(row).toBeVisible();
   });
 
-  test.skip("is not visible when ", async () => {
+  test("is not visible when ", async () => {
     render(otherTextRow);
-    const row = screen.getByTestId("export-row");
-    expect(row).not.toBeVisible();
+    const row = screen.queryByTestId("exportRow");
+    expect(row).toBeNull();
+  });
+
+  test("displays alternate prop fields", async () => {
+    render(dynamicRow);
+    const row = screen.getByTestId("exportRow");
+    expect(row).toBeVisible();
   });
 });
 
-describe("Test ExportedReportBanner accessibility", () => {
-  it.skip("Should not have basic accessibility issues", async () => {
+describe("Test ExportedReportFieldRow accessibility", () => {
+  it("Should not have basic accessibility issues", async () => {
     const { container } = render(exportRow);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
