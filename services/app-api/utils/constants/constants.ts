ef48f8849a170f873c121e96609737283f36c547
Oren Weingrod <124941331+oweingrod-coforma@users.noreply.github.com>
2023-02-23 11:26:33 -0500

    Clean up reporting APIs. (#11082)
    * Add new error constants for Dynamo errors
    
    Error constants
    
    nerw constant
    
    * Refactor reports/create.ts to handle errors more cleanly
    
    Fix create route
    
    * Refactor reports/archive.ts to handle errors more cleanly
    
    Archive test should be calling `archive` route.
    
    Previously, tests were calling `update` route.
    
    * Refactor reports/fetch.ts to handle errors more cleanly
    
    * Missing a required argument should return a `400` not a `500`.
    
    Update test asserts to expect new status codes and error messages.
    
    * Refactor reports/update.ts to handle errors more clearly
    
    Reorganize code to move returned errors closer to their conditional checks. Adds additional error handling for S3 and Dynamo, and removes (likely) un-needed logic to archive reports from this route.
    
    Remove duplicate `update` test.
    
    * Archive route should ensure credentials
    
    * Fetch handler was looking for wrong path parameter
    
    * Add new discrete handler for archiving reports.
    
    * Point archive button to the archive handler
    
    Instead of the update handler

--- services/app-api/utils/constants/constants.ts
+++ services/app-api/utils/constants/constants.ts
@@ -1,19 +1,24 @@
 export const error = {
   // generic errors
   UNAUTHORIZED: "User is not authorized to access this resource.",
-  NO_KEY: "Must provide key for table",
-  MISSING_DATA: "Missing required data",
-  INVALID_DATA: "Provided data is not valid",
-  NO_MATCHING_RECORD: "No matching record found",
+  NO_KEY: "Must provide key for table.",
+  MISSING_DATA: "Missing required data.",
+  INVALID_DATA: "Provided data is not valid.",
+  NO_MATCHING_RECORD: "No matching record found.",
+  SERVER_ERROR: "An unspecified server error occured.",
   // bucket errors
   S3_OBJECT_CREATION_ERROR: "Report could not be created due to an S3 error.",
   S3_OBJECT_UPDATE_ERROR: "Report could not be updated due to an S3 error.",
+  // dynamo errors
+  DYNAMO_CREATION_ERROR: "Report could not be created due to a database error.",
+  DYNAMO_UPDATE_ERROR: "Report could not be updated due to a database error.",
   // template errors
-  NO_TEMPLATE_NAME: "Must request template for download",
-  INVALID_TEMPLATE_NAME: "Requested template does not exist or does not match",
-  NOT_IN_DATABASE: "Record not found in database",
-  MISSING_FORM_TEMPLATE: "Form Template not found in S3",
-  MISSING_FIELD_DATA: "Field Data not found in S3",
+  NO_TEMPLATE_NAME: "Must request template for download.",
+  INVALID_TEMPLATE_NAME: "Requested template does not exist or does not match.",
+  NOT_IN_DATABASE: "Record not found in database.",
+  MISSING_FORM_TEMPLATE: "Form Template not found in S3.",
+  MISSING_FIELD_DATA: "Field Data not found in S3.",
+  ALREADY_ARCHIVED: "Cannot update archived report.",
 };
 
 export const buckets = {
--- services/ui-src/src/types/index.ts
+++ services/ui-src/src/types/index.ts
@@ -164,6 +164,7 @@ export interface ReportShape extends ReportMetadataShape {
 export interface ReportContextMethods {
   fetchReport: Function;
   fetchReportsByState: Function;
+  archiveReport: Function;
   createReport: Function;
   updateReport: Function;
   clearReportSelection: Function;
