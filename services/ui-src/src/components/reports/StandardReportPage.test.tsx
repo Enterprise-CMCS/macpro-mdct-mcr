c489ffd35acac98e93f3e5d1861a99f7b6915252
karla-vm <karla@coforma.io>
2023-02-27 09:39:30 -0500

    coverage

--- services/ui-src/src/components/reports/StandardReportPage.test.tsx
+++ services/ui-src/src/components/reports/StandardReportPage.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { axe } from "jest-axe";
+import userEvent from "@testing-library/user-event";
 // components
 import { ReportContext, StandardReportPage } from "components";
 // utils
@@ -24,6 +25,31 @@ describe("Test StandardReportPage", () => {
   test("StandardReportPage view renders", () => {
     render(standardPageSectionComponent);
     expect(screen.getByTestId("standard-page")).toBeVisible();
+  });
+
+  test("StandardReportPage correctly submits a valid form", async () => {
+    const result = render(standardPageSectionComponent);
+    const textFieldInput: HTMLInputElement = result.container.querySelector(
+      "[id='mock-text-field'"
+    )!;
+    await userEvent.type(textFieldInput, "ABC");
+    expect(textFieldInput.value).toEqual("ABC");
+    const continueButton = screen.getByText("Continue")!;
+    await userEvent.click(continueButton);
+  });
+
+  test("StandardReportPage navigates to next route onError", async () => {
+    const result = render(standardPageSectionComponent);
+    const currentPath = window.location.pathname;
+    const textFieldInput: HTMLInputElement = result.container.querySelector(
+      "[id='mock-text-field'"
+    )!;
+    await userEvent.type(textFieldInput, "      ");
+    const continueButton = screen.getByText("Continue")!;
+    await userEvent.click(continueButton);
+    // test that form navigates with an error in the field
+    const newPath = window.location.pathname;
+    expect(currentPath).not.toBe(newPath);
   });
 });
 
