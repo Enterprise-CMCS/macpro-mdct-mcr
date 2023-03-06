881a58870519db64819fbd350b89adbfa47c9ef8
karla-vm <karla@coforma.io>
2023-03-06 15:24:12 -0500

    lock and unlock reports

---
+++ services/app-api/handlers/reports/lock.test.ts
@@ -0,0 +1,95 @@
+import { fetchReport } from "./fetch";
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { proxyEvent } from "../../utils/testing/proxyEvent";
+import { StatusCodes } from "../../utils/types/types";
+import { mockReport } from "../../utils/testing/setupJest";
+import { error } from "../../utils/constants/constants";
+import { lockReport } from "./lock";
+
+jest.mock("../../utils/auth/authorization", () => ({
+  isAuthorized: jest.fn().mockResolvedValue(true),
+  hasPermissions: jest.fn(() => {}),
+}));
+
+const mockAuthUtil = require("../../utils/auth/authorization");
+
+jest.mock("../../utils/debugging/debug-lib", () => ({
+  init: jest.fn(),
+  flush: jest.fn(),
+}));
+
+jest.mock("./fetch");
+const mockedFetchReport = fetchReport as jest.MockedFunction<
+  typeof fetchReport
+>;
+
+const mockProxyEvent: APIGatewayProxyEvent = {
+  ...proxyEvent,
+  headers: { "cognito-identity-id": "test" },
+  pathParameters: { reportType: "mock-type", state: "AB", id: "testReportId" },
+  body: JSON.stringify(mockReport),
+};
+
+const lockEvent: APIGatewayProxyEvent = {
+  ...mockProxyEvent,
+  body: JSON.stringify({
+    ...mockReport,
+    locked: true,
+  }),
+};
+
+describe("Test lockReport method", () => {
+  beforeEach(() => {
+    // fail state and pass admin auth checks
+    mockAuthUtil.hasPermissions
+      .mockReturnValueOnce(true)
+      .mockReturnValueOnce(true)
+      .mockReturnValueOnce(false);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Test lock report passes with valid data", async () => {
+    mockedFetchReport.mockResolvedValue({
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "string",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(mockReport),
+    });
+    const res: any = await lockReport(lockEvent, null);
+    const body = JSON.parse(res.body);
+    expect(res.statusCode).toBe(StatusCodes.SUCCESS);
+    expect(body.locked).toBe(true);
+  });
+
+  test("Test lock report with no existing record throws 404", async () => {
+    mockedFetchReport.mockResolvedValue({
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "string",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: undefined!,
+    });
+    const res = await lockReport(lockEvent, null);
+    expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
+    expect(res.body).toContain(error.NO_MATCHING_RECORD);
+  });
+
+  test("Test lock report without admin permissions throws 403", async () => {
+    mockedFetchReport.mockResolvedValue({
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "string",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: undefined!,
+    });
+    const res = await lockReport(lockEvent, null);
+    expect(res.statusCode).toBe(StatusCodes.UNAUTHORIZED);
+    expect(res.body).toContain(error.UNAUTHORIZED);
+  });
+});
