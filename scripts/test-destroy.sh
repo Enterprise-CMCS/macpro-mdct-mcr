5d453e3ed14cdbbe366d3e80b6d1b782109e8d17
BearHanded <bcm3223@gmail.com>
2023-03-06 16:26:00 -0500

    trigger test

--- scripts/test-destroy.sh
+++ scripts/test-destroy.sh
@@ -20,47 +20,7 @@ if [[ $1 == "" ]] ; then
 fi
 stage=$1
 
-# A list of protected/important branches/environments/stages.
-protected_stage_regex="(^main$|^val$|^production)"
-if [[ $stage =~ $protected_stage_regex ]] ; then
-    echo """
-      ---------------------------------------------------------------------------------------------
-      ERROR:  Please read below
-      ---------------------------------------------------------------------------------------------
-      The regex used to denote protected stages matched the stage name you passed.
-      The regex holds names commonly used for important branches/environments/stages.
-      This indicates you're trying to destroy a stage that you likely don't really want to destroy.
-      Out of caution, this script will not continue.
-
-      If you really do want to destroy $stage, modify this script as necessary and run again.
-
-      Be careful.
-      ---------------------------------------------------------------------------------------------
-    """
-    exit 1
-fi
-
-echo "Collecting information on stage $stage before attempting a destroy... This can take a minute or two..."
-
 set -e
-
-
-if [ "$CI" != "true" ]; then
-  read -p "Do you wish to continue?  Re-enter the stage name to continue:  " -r
-  echo
-  if [[ ! $REPLY == "$stage" ]]
-  then
-      echo "Stage name not re-entered.  Doing nothing and exiting."
-      exit 1
-  fi
-fi
-
-# Find hanging api-gateway log group
-apiGatewayLogGroupName="/aws/api-gateway/app-api-$stage"
-apiGatewayLogGroupExists=(`aws logs describe-log-groups --log-group-name-prefix $apiGatewayLogGroupName | jq -r ".logGroups[] | length"`)
-if [[ -n $apiGatewayLogGroupExists ]] ; then
-    aws logs delete-log-group --log-group-name $apiGatewayLogGroupName
-fi
 
 # Cleanup bigmac topics for branch
 data='{"project":"mcr","stage":"'"$stage"'"}'
