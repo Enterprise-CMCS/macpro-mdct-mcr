1d765782cdacd5bf525596f95dab506fe703ef6a
karla-vm <karla@coforma.io>
2023-03-08 16:12:31 -0500

    some more factoring out

--- services/ui-src/src/types/index.ts
+++ services/ui-src/src/types/index.ts
@@ -2,4 +2,5 @@
 export * from "./banners";
 export * from "./formFields";
 export * from "./other";
+export * from "./reportContext";
 export * from "./users";
--- services/ui-src/src/types/other.ts
+++ services/ui-src/src/types/other.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { FormJson } from "./formFields";
+import { FormJson } from "types";
 
 // REPORT STRUCTURE
 
@@ -97,61 +97,6 @@ export interface ModalDrawerReportPageVerbiage
   entityUnfinishedMessage: string;
   enterEntityDetailsButtonText: string;
   editEntityDetailsButtonText: string;
-}
-
-// REPORT PROVIDER/CONTEXT
-
-export interface ReportKeys {
-  reportType: string;
-  state: string;
-  id: string;
-}
-
-export interface ReportMetadataShape extends ReportKeys {
-  reportType: string;
-  programName: string;
-  status: ReportStatus;
-  reportingPeriodStartDate: number;
-  reportingPeriodEndDate: number;
-  dueDate: number;
-  createdAt: number;
-  lastAltered: number;
-  lastAlteredBy: string;
-  combinedData: boolean;
-  submittedBy?: string;
-  submitterEmail?: string;
-  submittedOnDate?: number;
-  archived?: boolean;
-  locked?: boolean;
-}
-
-export interface ReportShape extends ReportMetadataShape {
-  formTemplate: ReportJson;
-  fieldData: AnyObject;
-}
-
-export interface ReportContextMethods {
-  fetchReport: Function;
-  fetchReportsByState: Function;
-  archiveReport: Function;
-  unlockReport?: Function;
-  createReport: Function;
-  updateReport: Function;
-  clearReportSelection: Function;
-  setReportSelection: Function;
-}
-
-export interface ReportContextShape extends ReportContextMethods {
-  report: ReportShape | undefined;
-  reportsByState: ReportMetadataShape[] | undefined;
-  errorMessage?: string | undefined;
-  lastSavedTime?: string | undefined;
-}
-
-export enum ReportStatus {
-  NOT_STARTED = "Not started",
-  IN_PROGRESS = "In progress",
-  SUBMITTED = "Submitted",
 }
 
 // OTHER
