d7897ace97b59a4691c76944c3c0a6587d350469
Oren Weingrod <oren.weingrod@coforma.io>
2023-03-08 14:40:58 -0500

    S3 copy

--- services/app-api/utils/s3/s3-lib.ts
+++ services/app-api/utils/s3/s3-lib.ts
@@ -1,7 +1,7 @@
 import { Credentials, S3, Endpoint } from "aws-sdk";
 import { ServiceConfigurationOptions } from "aws-sdk/lib/service";
 import { buckets } from "../constants/constants";
-import { S3Copy, S3Get, S3Put, States } from "../types/types";
+import { S3Copy, S3Get, S3Put, State } from "../types/types";
 
 export const createS3Client = () => {
   const s3Config: S3.ClientConfiguration &
@@ -52,19 +52,19 @@ export default {
     });
   },
   copy: async (params: S3Copy) => {
-    return new Promise((resolve, reject) => {
+    return new Promise<void>((resolve, reject) => {
       s3Client.copyObject(params, function (err: any, result: any) {
         if (err) {
           reject(err);
         }
         if (result) {
-          resolve(JSON.parse(result.Body));
+          resolve();
         }
       });
     });
   },
 };
 
-export function getFieldDataKey(state: States, fieldDataId: string) {
+export function getFieldDataKey(state: State, fieldDataId: string) {
   return `${buckets.FIELD_DATA}/${state}/${fieldDataId}.json`;
 }
