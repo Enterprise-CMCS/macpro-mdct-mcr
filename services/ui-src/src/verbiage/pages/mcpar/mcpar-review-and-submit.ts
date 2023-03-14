e1da09b3dbc30936f6b0252c06874fcd0b5918c4
Nicholas Summers <nick@coforma.io>
2023-03-14 10:24:14 -0400

    Swap to using table component from custom status stable

--- services/ui-src/src/verbiage/pages/mcpar/mcpar-review-and-submit.ts
+++ services/ui-src/src/verbiage/pages/mcpar/mcpar-review-and-submit.ts
@@ -9,6 +9,9 @@ export default {
       infoHeader: "Ready to Submit?",
       info: "Double check that everything in your MCPAR Report is accurate. You will be able to make edits after submitting, and resubmit. Once you’ve reviewed your report, certify that it’s in compliance with 42 CFR § 438.66(e).",
     },
+    table: {
+      headRow: ["Section", "Status", ""],
+    },
     modal: {
       structure: {
         heading: "Are you sure you want to submit MCPAR?",
