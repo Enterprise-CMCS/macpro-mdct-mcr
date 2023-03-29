5537e721bdbf1ec1db93c9d9446422734b9b3325
Thomas Blackwell <thomas.blackwell@coforma.io>
2023-03-29 13:44:12 -0400

    missed function signature in test

--- services/app-api/handlers/etl/formTemplateEntitiesUpdate.test.ts
+++ services/app-api/handlers/etl/formTemplateEntitiesUpdate.test.ts
@@ -73,7 +73,7 @@ describe("Test form template entities update", () => {
 
   test("Test write updated form template to S3", async () => {
     try {
-      await writeFormTemplateToS3(mockReportJsonWithId);
+      await writeFormTemplateToS3(mockReportJsonWithId,"foo","bar");
     } catch (e) {
       expect(e).toBeFalsy();
     }
