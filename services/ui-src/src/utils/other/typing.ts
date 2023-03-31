4d444e9115a8891f177cfd02d6d268ee90fd0cf5
benmartin-coforma <Ben.Martin@coforma.io>
2023-03-31 07:20:03 -0600

    Fix a formatting issue

--- services/ui-src/src/utils/other/typing.ts
+++ services/ui-src/src/utils/other/typing.ts
@@ -2,4 +2,4 @@
  * Instructs Typescript to complain if it detects that this function may be reachable.
  * Useful for the default branch of a switch statement that verifiably covers every case.
  */
-export const assertExhaustive = (_: never): void => { };
+export const assertExhaustive = (_: never): void => {};
