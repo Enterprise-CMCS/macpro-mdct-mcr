74ec59f036b38f9f7fc71fccab02d117c67a4378
karla-vm <karla@coforma.io>
2023-04-10 16:28:51 -0400

    review and submit verbiage updates, styling updates for MLR json

--- services/ui-src/src/verbiage/pages/mlr/mlr-review-and-submit.ts
+++ services/ui-src/src/verbiage/pages/mlr/mlr-review-and-submit.ts
@@ -1,13 +1,13 @@
 export default {
   print: {
     printPageUrl: "/mcpar/export",
-    printButtonText: "Review PDF",
+    printButtonText: "Download PDF",
   },
   review: {
     intro: {
       header: "Review & Submit",
       infoHeader: "Ready to Submit?",
-      info: "Double check that everything in your MLR Report is accurate. You will be able to make edits after submitting, and resubmit. Once you’ve reviewed your report, certify that it’s in compliance with 42 CFR § 438.66(e).",
+      info: 'Double check that everything in your MLR submission is accurate. You won’t be able to make edits after submitting, unless you send a request to CMS to unlock your report. After compliance review, a CMS representative will contact you if there are corrections to be made and your report status will change to “In revision” in the MLR dashboard. Once you’ve reviewed your report, certify that it’s in compliance with <a href="https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-B/section-438.74" target="_blank" aria-label="Link opens in new tab">42 CFR § 438.74</a>.',
     },
     modal: {
       structure: {
@@ -27,12 +27,12 @@ export default {
       infoHeader: "Thank you",
       additionalInfoHeader: "What happens now?",
       additionalInfo:
-        "No further action is needed at this point. CMS will reach out if in the case they have any questions.",
+        "No further action is needed at this point. CMS will contact you with questions or if corrections are needed. Your MLR dashboard will indicate the status of this report as “Submitted”.",
     },
   },
   alertBox: {
     title: "Your form is not ready for submission",
     description:
-      "Some sections of the MLR report have errors or are missing responses. Please ensure all fields are completed with valid responses before submitting.",
+      "Some sections of the MLR submission have errors or are missing required responses. Please ensure all required fields are completed with valid responses before submitting.",
   },
 };
