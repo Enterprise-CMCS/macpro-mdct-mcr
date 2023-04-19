af2e67299eaa5983e902df4e3abd18cb87ddad20
karla-vm <karla@coforma.io>
2023-04-19 10:56:29 -0400

    open links in new tab

--- services/ui-src/src/verbiage/pages/accordion.ts
+++ services/ui-src/src/verbiage/pages/accordion.ts
@@ -2,8 +2,57 @@ export default {
   MLR: {
     adminDashboard: {
       buttonLabel: "Instructions",
-      intro:
-        '<b>State User Instructions</b><br/><br/>As described at <a href="https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-B/section-438.74" target="_blank" aria-label="Link opens in new tab">42 CFR § 438.74</a>, states are required to report summary Medical Loss Ratio (MLR) reports to the Centers for Medicare & Medicaid Services (CMS). The summary MLR report submission coincides with the state’s submission of the annual base rate certification. The summary reports are based on the plans’ annual MLR reports to the state required under <a href="https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(k)" target="_blank" aria-label="Link opens in new tab">42 CFR § 438.8(k)</a>. If the state needs to revise MLR reports previously submitted using the online form, ask your CMS contact to reopen the related submission, which will change it to an “In revision” status. If the state needs to revise MLR reports previously submitted using the Excel Workbook or other format, reach out to your CMS contact to discuss next steps. <a href="https://www.medicaid.gov/medicaid/managed-care/guidance/medicaid-and-chip-managed-care-reporting/index.html#MLR" target="_blank" aria-label="Link opens in new tab">Learn more</a>. <br/> <br/> <b>CMS Admin Instructions</b>',
+      intro: [
+        {
+          type: "text",
+          as: "span",
+          content: "<b>State User Instructions</b><br/><br/>As described at ",
+        },
+        {
+          type: "externalLink",
+          content: "42 CFR § 438.74",
+          props: {
+            href: "https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-B/section-438.74",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+        {
+          type: "text",
+          as: "span",
+          content:
+            ", states are required to report summary Medical Loss Ratio (MLR) reports to the Centers for Medicare & Medicaid Services (CMS). The summary MLR report submission coincides with the state’s submission of the annual base rate certification. The summary reports are based on the plans’ annual MLR reports to the state required under ",
+        },
+        {
+          type: "externalLink",
+          content: "42 CFR § 438.8(k)",
+          props: {
+            href: "https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(k)",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+        {
+          type: "text",
+          as: "span",
+          content:
+            ". If the state needs to revise MLR reports previously submitted using the online form, ask your CMS contact to reopen the related submission, which will change it to an “In revision” status. If the state needs to revise MLR reports previously submitted using the Excel Workbook or other format, reach out to your CMS contact to discuss next steps. ",
+        },
+        {
+          type: "externalLink",
+          content: "Learn more",
+          props: {
+            href: "https://www.medicaid.gov/medicaid/managed-care/guidance/medicaid-and-chip-managed-care-reporting/index.html#MLR",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+        {
+          type: "text",
+          as: "span",
+          content: ". <br/> <br/> <b>CMS Admin Instructions</b>",
+        },
+      ],
       list: [
         `To allow a state to make corrections or edits to a submission use "Unlock" to release the submission. The status will change to "In revision".`,
         "Submission count is shown in the # column. Submissions started and submitted once have a count of 1. When a state resubmits a previous submission, the count increases by 1.",
@@ -13,22 +62,117 @@ export default {
     },
     stateUserDashboard: {
       buttonLabel: "Instructions",
-      intro:
-        'As described at <a href="https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-B/section-438.74" target="_blank" aria-label="Link opens in new tab">42 CFR § 438.74</a>, states are required to report summary Medical Loss Ratio (MLR) reports to the Centers for Medicare & Medicaid Services (CMS). The summary MLR report submission coincides with the state’s submission of the annual base rate certification. The summary reports are based on the plans’ annual MLR reports to the state required under <a href="https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(k)" target="_blank" aria-label="Link opens in new tab">42 CFR § 438.8(k)</a>. If the state needs to revise MLR reports previously submitted using the online form, ask your CMS contact to reopen the related submission, which will change it to an “In revision” status. If the state needs to revise MLR reports previously submitted using the Excel Workbook or other format, reach out to your CMS contact to discuss next steps. <a href="https://www.medicaid.gov/medicaid/managed-care/guidance/medicaid-and-chip-managed-care-reporting/index.html#MLR" target="_blank" aria-label="Link opens in new tab">Learn more</a>.',
+      intro: [
+        {
+          type: "text",
+          as: "span",
+          content: "As described at ",
+        },
+        {
+          type: "externalLink",
+          content: "42 CFR § 438.74",
+          props: {
+            href: "https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-B/section-438.74",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+        {
+          type: "text",
+          as: "span",
+          content:
+            ", states are required to report summary Medical Loss Ratio (MLR) reports to the Centers for Medicare & Medicaid Services (CMS). The summary MLR report submission coincides with the state’s submission of the annual base rate certification. The summary reports are based on the plans’ annual MLR reports to the state required under ",
+        },
+        {
+          type: "externalLink",
+          content: "42 CFR § 438.8(k)",
+          props: {
+            href: "https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(k)",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+        {
+          type: "text",
+          as: "span",
+          content:
+            ". If the state needs to revise MLR reports previously submitted using the online form, ask your CMS contact to reopen the related submission, which will change it to an “In revision” status. If the state needs to revise MLR reports previously submitted using the Excel Workbook or other format, reach out to your CMS contact to discuss next steps. ",
+        },
+        {
+          type: "externalLink",
+          content: "Learn more",
+          props: {
+            href: "https://www.medicaid.gov/medicaid/managed-care/guidance/medicaid-and-chip-managed-care-reporting/index.html#MLR",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+      ],
       list: [],
       text: "",
     },
     formIntro: {
       buttonLabel: "Instructions",
-      intro:
-        '<p>States must provide summary MLR report data for each managed care plan. These reports are based on the plans\' annual MLR reports to the state under <a href="https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(k)" target="_blank" aria-label="Link opens in new tab">42 CFR § 438.8(k)</a>. States have the option of reporting these data for each managed care program, statewide, or at another level of aggregation (e.g., eligibility groups). Managed care program is defined by a specified set of benefits and eligibility criteria that are articulated in a contract between the state and managed care plans. <b>In general, MLR data should not be aggregated across programs; however, if a managed care plan participates in more than one program with the state—the state can report the plan\'s MLRs for each program separately or combine the plan\'s results across managed care programs.</b> If a state combines the plan\'s reporting across programs, the report must use a consistent MLR reporting year.</p> <br/> <p>States must report MLRs for both credible and non-credible MCOs, PIHPs, and PAHPs. There is no exception for MLR reporting for non-credible managed care plans. Under <a href="https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(l)" target="_blank" aria-label="Link opens in new tab">42 CFR § 438.8(l)</a> a state may exclude a plan that is newly contracted with the state from this reporting for the first year of the plan\'s operation. These "new experience" plans must report MLRs during the next MLR reporting year in which the plan is in business with the state, even if the first year was not a full 12 months.</p>',
+      intro: [
+        {
+          type: "text",
+          as: "span",
+          content:
+            "<p>States must provide summary MLR report data for each managed care plan. These reports are based on the plans' annual MLR reports to the state under ",
+        },
+        {
+          type: "externalLink",
+          content: "42 CFR § 438.8(k)",
+          props: {
+            href: "https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(k)",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+        {
+          type: "text",
+          as: "span",
+          content:
+            ". States have the option of reporting these data for each managed care program, statewide, or at another level of aggregation (e.g., eligibility groups). Managed care program is defined by a specified set of benefits and eligibility criteria that are articulated in a contract between the state and managed care plans. <b>In general, MLR data should not be aggregated across programs; however, if a managed care plan participates in more than one program with the state—the state can report the plan's MLRs for each program separately or combine the plan's results across managed care programs.</b> If a state combines the plan's reporting across programs, the report must use a consistent MLR reporting year.</p> <br/>States must report MLRs for both credible and non-credible MCOs, PIHPs, and PAHPs. There is no exception for MLR reporting for non-credible managed care plans. Under ",
+        },
+        {
+          type: "externalLink",
+          content: "42 CFR § 438.8(l)",
+          props: {
+            href: "https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(l)",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+        {
+          type: "text",
+          as: "span",
+          content:
+            ' a state may exclude a plan that is newly contracted with the state from this reporting for the first year of the plan\'s operation. These "new experience" plans must report MLRs during the next MLR reporting year in which the plan is in business with the state, even if the first year was not a full 12 months.</p>',
+        },
+      ],
       list: [],
       text: "",
     },
     detailIntro: {
       buttonLabel: "Instructions",
-      intro:
-        "<p>States must report the five required MLR summary elements. Note that the form fields do not automatically calculate the MLR numerator, denominator, or MLR percentage. Each element must be entered manually. Fields marked as “optional” are included to allow states to report additional MLR data that states currently collect from MCOs, PIHPs, or PAHPs. </p> <br /> <p>Note: States that are reporting non-credible plans should enter member month values in section 3.1 as described below. States should report all other required MLR reporting elements (sections 1.3, 2.3, 3.4) with the value 0, and answer “No” for section 4.1 when reporting non-credible plan information. Reporting in this way will ensure that the progress indicators result in a “complete” status. Information on non-credible plans and credibility adjustment calculations is available from <a href='https://www.medicaid.gov/federal-policy-guidance/downloads/cib073117.pdf' target='_blank' aria-label='Link opens in new tab'>CMCS Informational Bulletin dated July 31, 2017</a>.</p>",
+      intro: [
+        {
+          type: "text",
+          as: "span",
+          content:
+            "<p>States must report the five required MLR summary elements. Note that the form fields do not automatically calculate the MLR numerator, denominator, or MLR percentage. Each element must be entered manually. Fields marked as “optional” are included to allow states to report additional MLR data that states currently collect from MCOs, PIHPs, or PAHPs. </p> <br /> <p>Note: States that are reporting non-credible plans should enter member month values in section 3.1 as described below. States should report all other required MLR reporting elements (sections 1.3, 2.3, 3.4) with the value 0, and answer “No” for section 4.1 when reporting non-credible plan information. Reporting in this way will ensure that the progress indicators result in a “complete” status. Information on non-credible plans and credibility adjustment calculations is available from ",
+        },
+        {
+          type: "externalLink",
+          content: "CMCS Informational Bulletin dated July 31, 2017",
+          props: {
+            href: "https://www.medicaid.gov/federal-policy-guidance/downloads/cib073117.pdf",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+      ],
       list: [],
       text: "",
     },
