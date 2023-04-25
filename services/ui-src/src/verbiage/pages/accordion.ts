15f15c425d39de4e0984853fecaa45d9529bca89
karla-vm <karla@coforma.io>
2023-04-25 11:46:15 -0400

    minor feedback items

--- services/ui-src/src/verbiage/pages/accordion.ts
+++ services/ui-src/src/verbiage/pages/accordion.ts
@@ -166,7 +166,7 @@ export default {
           type: "text",
           as: "span",
           content:
-            "<p>States must report the five required MLR summary elements. Note that the form fields do not automatically calculate the MLR numerator, denominator, or MLR percentage. Each element must be entered manually. Fields marked as “optional” are included to allow states to report additional MLR data that states currently collect from MCOs, PIHPs, or PAHPs. </p> <br /> <p>Note: States that are reporting non-credible plans should enter member month values in section 3.1 as described below. States should report all other required MLR reporting elements (sections 1.3, 2.3, 3.4) with the value 0, and answer “No” for section 4.1 when reporting non-credible plan information. Reporting in this way will ensure that the progress indicators result in a “complete” status. Information on non-credible plans and credibility adjustment calculations is available from ",
+            "<p>States must report the five required MLR summary elements. Note that the form fields do not automatically calculate the MLR numerator, denominator, or MLR percentage. Each element must be entered manually. Fields marked as “optional” are included to allow states to report additional MLR data that states currently collect from MCOs, PIHPs, or PAHPs. </p> <br />Note: States that are reporting non-credible plans should enter member month values in section 3.1 as described below. States should report all other required MLR reporting elements (sections 1.3, 2.3, 3.4) with the value 0, and answer “No” for section 4.1 when reporting non-credible plan information. Reporting in this way will ensure that the progress indicators result in a “complete” status. Information on non-credible plans and credibility adjustment calculations is available from ",
         },
         {
           type: "externalLink",
@@ -176,6 +176,11 @@ export default {
             target: "_blank",
             ariaLabel: "Link opens in new tab",
           },
+        },
+        {
+          type: "text",
+          as: "span",
+          content: ".",
         },
       ],
       list: [],
