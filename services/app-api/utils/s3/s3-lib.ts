23d96539b10dcbb649170162d97fafbef6d552d4
Oren Weingrod <oren.weingrod@coforma.io>
2023-06-29 14:51:51 -0400

    Update types for s3 copy

--- services/app-api/utils/s3/s3-lib.ts
+++ services/app-api/utils/s3/s3-lib.ts
@@ -1,7 +1,7 @@
 import { Credentials, S3, Endpoint } from "aws-sdk";
 import { ServiceConfigurationOptions } from "aws-sdk/lib/service";
 import { buckets } from "../constants/constants";
-import { S3Copy, S3Get, S3Put, State } from "../types/other";
+import { S3Get, S3Put, State } from "../types/other";
 
 export const createS3Client = () => {
   const s3Config: S3.ClientConfiguration &
@@ -51,7 +51,7 @@ export default {
       });
     });
   },
-  copy: async (params: S3Copy) => {
+  copy: async (params: S3.CopyObjectRequest) => {
     return new Promise<void>((resolve, reject) => {
       s3Client.copyObject(params, function (err: any, result: any) {
         if (err) {
