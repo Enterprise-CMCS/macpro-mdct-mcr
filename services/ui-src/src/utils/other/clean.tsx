51daccbb09f31979fd6ea91d306d149e352493f7
Ajaita Saini <ajaita.saini@coforma.io>
2023-07-07 17:27:40 -0400

    added regexes

--- services/ui-src/src/utils/other/clean.tsx
+++ services/ui-src/src/utils/other/clean.tsx
@@ -1,15 +1,77 @@
-// TO-DO: fix regexes
-const validStandardNumberRegex = new RegExp(""); // digits (0-9), any number of commas (,), at most one of $%-.
-const validRatioRegex = new RegExp(""); // at most one :.
+// REGEXES
+
+// basic check for all possible characters -- standard number
+const validCharactersStandardNumberRegex = new RegExp("/[0-9.,$%]+/");
+// basic check for all possible characters -- ratio
+const validCharactersRatioNumberRegex = new RegExp("/[0-9.,:]+/");
+// at most 1 decimal point
+const atMost1DecimalPointRegex = new RegExp("/^[^.]*.?[^.]*$/");
+// commas only exist before decimal point
+const validCommaLocationRegex = new RegExp("/^[,]*[.]*[^,]*$");
+// at most 1 $%
+const atMost1SpecialCharacterRegex = new RegExp("/[$%]?");
+// at most 1 $ at the beginning of the input
+const validDollarSignPlacementRegex = new RegExp("/^[$]?[^$%]+$/");
+// at most 1 % at the end of the input
+const validPercentSignPlacementRegex = new RegExp("/^[^%$]+[%]?$/");
+// at most 1 - at the beginning of the input (but after any potential $)
+const validNegativeSignPlacementRegex = new RegExp("/^[$]?[]?[^$%]+$/");
+// exactly one ratio character in between other characters
+const exactlyOneRatioCharacterRegex = new RegExp("/^[^:]+[:]{1}[^:]+$/");
 
 interface CleanedValue {
   isValid: boolean;
   cleanedValue: string;
 }
 
+export const checkStandardNumberInputAgainstRegexes = (
+  value: string
+): Boolean => {
+  if (
+    validCharactersStandardNumberRegex.test(value) ||
+    atMost1DecimalPointRegex.test(value) ||
+    validCommaLocationRegex.test(value) ||
+    atMost1SpecialCharacterRegex.test(value) ||
+    validDollarSignPlacementRegex.test(value) ||
+    validPercentSignPlacementRegex.test(value) ||
+    validNegativeSignPlacementRegex.test(value)
+  )
+    return false;
+  return true;
+};
+
+export const checkRatioInputAgainstRegexes = (value: string): Boolean => {
+  if (
+    validCharactersRatioNumberRegex.test(value) ||
+    exactlyOneRatioCharacterRegex.test(value)
+  )
+    return false;
+  return true;
+};
+
+export const checkLeftSideOfRatioAgainstRegexes = (value: string): Boolean => {
+  if (
+    validCharactersStandardNumberRegex.test(value) ||
+    atMost1DecimalPointRegex.test(value) ||
+    validCommaLocationRegex.test(value)
+  )
+    return false;
+  return true;
+};
+
+export const checkRightSideOfRatioAgainstRegexes = (value: string): Boolean => {
+  if (
+    validCharactersStandardNumberRegex.test(value) ||
+    atMost1DecimalPointRegex.test(value) ||
+    validCommaLocationRegex.test(value)
+  )
+    return false;
+  return true;
+};
+
 export const cleanStandardNumericalInput = (value: string): CleanedValue => {
   // Check if valid input against regex
-  const isValidNumber = validStandardNumberRegex.test(value);
+  const isValidNumber = checkStandardNumberInputAgainstRegexes(value);
   if (!isValidNumber) return { isValid: false, cleanedValue: value };
 
   // Remove all characters except digits and decimal points.
@@ -22,25 +84,26 @@ export const cleanStandardNumericalInput = (value: string): CleanedValue => {
 };
 
 export const cleanRatioInput = (value: string): CleanedValue => {
-  const isValidRatio = validRatioRegex.test(value);
+  const isValidRatio = checkRatioInputAgainstRegexes(value);
   if (!isValidRatio) return { isValid: false, cleanedValue: value };
 
   // Grab the left and right side of the ratio sign
   let values = value.split(":");
 
-  // Create the left side of the output and make the number (if provided) pretty
+  // Check left and right side for valid inputs
+  if (
+    !checkLeftSideOfRatioAgainstRegexes(values[0]) ||
+    !checkRightSideOfRatioAgainstRegexes(values[1])
+  )
+    return { isValid: false, cleanedValue: value };
+
+  // Clean the left side
   const cleanLeft = cleanStandardNumericalInput(values[0]);
-  if (!cleanLeft.isValid) return { isValid: false, cleanedValue: value };
   values[0] = cleanLeft.cleanedValue;
 
-  // Create the right side of the output and make the number (if provided) pretty
-
-  // is this if condition still necessary if we know the : will split the input correctly
-  if (values.length >= 2 && values[1] != "") {
-    const cleanRight = cleanStandardNumericalInput(values[1]);
-    if (!cleanRight.isValid) return { isValid: false, cleanedValue: value };
-    values[1] = cleanRight.cleanedValue;
-  }
+  // Clean the right side
+  const cleanRight = cleanStandardNumericalInput(values[1]);
+  values[1] = cleanRight.cleanedValue;
 
   const cleanedValue = values.join(":");
 
