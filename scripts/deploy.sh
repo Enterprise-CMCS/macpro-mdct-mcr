649be78dc589b43adb3a8b63451a195de49ee893
Ishmeal Baawuah <ibaawuah@ip-10-232-56-53.ec2.internal>
2023-08-16 13:42:39 -0400

    combining the deploy files into 1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,58 @@
+# #!/bin/bash
+
+# set -e
+
+# stage=${1:-dev}
+
+# services=(
+#   'database'
+#   'uploads'
+#   'topics'
+# )
+
+# install_deps() {
+#   if [ "$CI" == "true" ]; then # If we're in a CI system
+#     if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
+#       yarn install --frozen-lockfile
+#     fi
+#   else # We're not in a CI system, let's yarn install
+#     yarn install
+#   fi
+# }
+
+# deploy() {
+#   service=$1
+#   pushd services/$service
+#   install_deps
+#   serverless deploy  --stage $stage
+#   popd
+# }
+
+# install_deps
+# export PATH=$(pwd)/node_modules/.bin/:$PATH
+
+# for i in "${services[@]}"
+# do
+# 	deploy $i
+# done
+
+
+
 #!/bin/bash
-
 set -e
-
 stage=${1:-dev}
-
-services=(
+services_set1=(
   'database'
   'uploads'
   'topics'
 )
 
+services_set2=(
+  'app-api'
+  'ui'
+  'ui-auth'
+  'ui-src'
+)
 install_deps() {
   if [ "$CI" == "true" ]; then # If we're in a CI system
     if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
@@ -19,7 +62,6 @@ install_deps() {
     yarn install
   fi
 }
-
 deploy() {
   service=$1
   pushd services/$service
@@ -27,11 +69,27 @@ deploy() {
   serverless deploy  --stage $stage
   popd
 }
-
 install_deps
 export PATH=$(pwd)/node_modules/.bin/:$PATH
 
-for i in "${services[@]}"
-do
-	deploy $i
+
+# Deploy services from the first set
+for i in "${services_set1[@]}"; do
+  deploy $i
 done
+
+# Deploy services from the second set
+for i in "${services_set2[@]}"; do
+  deploy $i
+done
+
+
+pushd services
+
+echo """
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+Application endpoint:  `./output.sh ui CloudFrontEndpointUrl $stage`
+------------------------------------------------------------------------------------------------
+"""
+popd
