f6a36630fd26b501e84ff391d06e96bdb4010487
Ishmeal Baawuah <ibaawuah@Ishmeals-MBP.fios-router.home>
2023-08-17 10:07:20 -0400

    running deploy.sh in subshells

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,46 +1,8 @@
-# #!/bin/bash
-
-# set -e
-
-# stage=${1:-dev}
-
-# services=(
-#   'database'
-#   'uploads'
-#   'topics'
-# )
-
-# install_deps() {
-#   if [ "$CI" == "true" ]; then # If we're in a CI system
-#     if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
-#       yarn install --frozen-lockfile
-#     fi
-#   else # We're not in a CI system, let's yarn install
-#     yarn install
-#   fi
-# }
-
-# deploy() {
-#   service=$1
-#   pushd services/$service
-#   install_deps
-#   serverless deploy  --stage $stage
-#   popd
-# }
-
-# install_deps
-# export PATH=$(pwd)/node_modules/.bin/:$PATH
-
-# for i in "${services[@]}"
-# do
-# 	deploy $i
-# done
-
-
 
 #!/bin/bash
 set -e
 stage=${1:-dev}
+
 services_set1=(
   'database'
   'uploads'
@@ -53,6 +15,7 @@ services_set2=(
   'ui-auth'
   'ui-src'
 )
+
 install_deps() {
   if [ "$CI" == "true" ]; then # If we're in a CI system
     if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
@@ -62,27 +25,31 @@ install_deps() {
     yarn install
   fi
 }
+
 deploy() {
   service=$1
   pushd services/$service
   install_deps
-  serverless deploy  --stage $stage
+  serverless deploy --stage $stage
   popd
 }
+
 install_deps
 export PATH=$(pwd)/node_modules/.bin/:$PATH
 
+# Run the deployment of services_set1 in a subshell
+(
+  for i in "${services_set1[@]}"; do
+    deploy $i
+  done
+)
 
-# Deploy services from the first set
-for i in "${services_set1[@]}"; do
-  deploy $i
-done
-
-# Deploy services from the second set
-for i in "${services_set2[@]}"; do
-  deploy $i
-done
-
+# Run the deployment of services_set2 in a subshell
+(
+  for i in "${services_set2[@]}"; do
+    deploy $i
+  done
+)
 
 pushd services
 
