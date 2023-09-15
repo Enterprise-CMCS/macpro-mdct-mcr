2303ea00e5db2cfdc1182bda786dd6a9737727bd
Casie Siekman <casie.siekman@gmail.com>
2023-09-15 10:15:13 -0500

    Mdct 2864 yoy specific (#11459)
    Co-authored-by: Casie Siekman <casiesiekman@coformaesiekman.lan>
    Co-authored-by: benmartin-coforma <Ben.Martin@coforma.io>

--- services/app-api/handlers/reports/create.test.ts
+++ services/app-api/handlers/reports/create.test.ts
@@ -9,7 +9,7 @@ import {
 } from "../../utils/testing/setupJest";
 import { error } from "../../utils/constants/constants";
 // types
-import { AnyObject, StatusCodes } from "../../utils/types";
+import { StatusCodes } from "../../utils/types";
 import * as authFunctions from "../../utils/auth/authorization";
 import s3Lib from "../../utils/s3/s3-lib";
 
@@ -28,6 +28,16 @@ const mockProxyEvent = {
   ...proxyEvent,
   headers: { "cognito-identity-id": "test" },
   pathParameters: { reportType: "MCPAR", state: "AL" },
+};
+const mockInvalidStateProxyEvent = {
+  ...proxyEvent,
+  headers: { "cognito-identity-id": "test" },
+  pathParameters: { reportType: "MCPAR", state: "ALZ" },
+};
+const mockMlrProxyEvent = {
+  ...proxyEvent,
+  headers: { "cognito-identity-id": "test" },
+  pathParameters: { reportType: "MLRrrr", state: "AL" },
 };
 
 const creationEvent: APIGatewayProxyEvent = {
@@ -64,10 +74,91 @@ const creationEventWithInvalidData: APIGatewayProxyEvent = {
 const creationEventWithCopySource: APIGatewayProxyEvent = {
   ...mockProxyEvent,
   body: JSON.stringify({
-    fieldData: { stateName: "Alabama" },
+    fieldData: { stateName: "Alabama", programName: "New Program" },
     metadata: { copyFieldDataSourceId: "mockReportFieldData" },
   }),
 };
+
+const creationEventMlrReport: APIGatewayProxyEvent = {
+  ...mockMlrProxyEvent,
+  body: JSON.stringify({
+    fieldData: { stateName: "Alabama" },
+    metadata: {
+      reportType: "MLRrrr",
+    },
+  }),
+};
+
+const creationEventInvalidState: APIGatewayProxyEvent = {
+  ...mockInvalidStateProxyEvent,
+  body: JSON.stringify({
+    fieldData: { stateName: "Alabizama" },
+    metadata: {
+      reportType: "MCPAR",
+    },
+  }),
+};
+
+const mockBssEntities = [
+  {
+    id: "61b33e6-c168-cf5-015c-47d46ca20dd",
+    name: "Dynamic Fill",
+    bssEntity_entityType: [
+      {
+        key: "bssEntity_entityType-b8RT4wLcoU2yb0QgswyAfQ",
+        value: "State Government Entity",
+      },
+    ],
+    bssEntity_entityRole: [
+      {
+        key: "bss_key",
+        value: "Enrollment Broker/Choice Counseling",
+      },
+    ],
+    "bssEntity_entityType-otherText": "",
+    "bssEntity_entityRole-otherText": "",
+  },
+];
+const mockSanctions = [
+  {
+    id: "35210f-e322-bba2-d38b-16103d5eba2",
+    sanction_interventionType: [
+      {
+        key: "sanction_key",
+        value: "Civil monetary penalty",
+      },
+    ],
+    sanction_interventionTopic: [
+      {
+        key: "sanction_interventionTopic-x6Cwd4oSrki6De4SnpjrMQ",
+        value: "Discrimination",
+      },
+    ],
+    sanction_planName: {
+      label: "sanction_planName",
+      value: "1e15658-1e01-0531-a8d8-4a620e845c31",
+    },
+    sanction_interventionReason: "Text Fill",
+    "sanction_interventionType-otherText": "",
+    "sanction_interventionTopic-otherText": "",
+    sanction_noncomplianceInstances: "10",
+    sanction_dollarAmount: "44",
+    sanction_assessmentDate: "02/17/2023",
+    sanction_remediationCompleted: [
+      {
+        key: "sanction_key",
+        value: "Yes",
+      },
+    ],
+    sanction_correctiveActionPlan: [
+      {
+        key: "sanction_key",
+        value: "Yes",
+      },
+    ],
+    sanction_remediationDate: "02/17/2023",
+  },
+];
 
 mockDocumentClient.query.promise.mockReturnValue({
   Items: [],
@@ -91,7 +182,7 @@ describe("Test createReport API method", () => {
     expect(res.body).toContain(error.UNAUTHORIZED);
   });
 
-  test("Test successful run of report creation", async () => {
+  test("Test successful run of report creation, not copied", async () => {
     const res = await createReport(creationEvent, null);
 
     const body = JSON.parse(res.body);
@@ -106,6 +197,15 @@ describe("Test createReport API method", () => {
     expect(body.formTemplate.validationJson).toMatchObject({
       stateName: "text",
     });
+    expect(body.fieldData.plans).toBeUndefined();
+    expect(body.fieldData.sanctions).toBeUndefined();
+    expect(body.fieldData.state_statewideMedicaidEnrollment).toBeUndefined();
+    expect(
+      body.fieldData.state_statewideMedicaidManagedCareEnrollment
+    ).toBeUndefined();
+    expect(body.fieldData.bssEntities).toBeUndefined();
+    expect(body.fieldData.programName).toBeUndefined();
+    expect(body.fieldData.plans).toBeUndefined();
   });
 
   test("Test attempted report creation with invalid data fails", async () => {
@@ -145,7 +245,9 @@ describe("Test createReport API method", () => {
   test("Test report with copyFieldDataSourceId", async () => {
     jest.spyOn(s3Lib, "get").mockResolvedValueOnce({
       stateName: "Alabama",
-      plans: [{ plan_activeAppeals: "1", name: "name" }],
+      programName: "Old Program",
+      plans: [{ plan_programIntegrityReferralPath: "1", name: "name" }],
+      bssEntities: mockBssEntities,
     });
     const copyFieldDataSpy = jest.spyOn(reportUtils, "copyFieldDataFromSource");
     const res = await createReport(creationEventWithCopySource, null);
@@ -154,18 +256,33 @@ describe("Test createReport API method", () => {
     expect(copyFieldDataSpy).toBeCalled();
     expect(body.fieldDataId).not.toEqual("mockReportFieldData");
     expect(body.fieldData.plans).toBeDefined();
-    body.fieldData.plans.forEach((p: AnyObject) => {
-      expect(p).toEqual({
-        name: "name",
-        plan_activeAppeals: "1",
-      });
+    expect(body.fieldData.plans.length).toBe(1);
+    expect(body.fieldData.plans[0]).toEqual({
+      name: "name",
+      plan_programIntegrityReferralPath: "1",
     });
+    expect(body.fieldData.bssEntities).toEqual(mockBssEntities);
+    expect(body.fieldData.programName).toEqual("New Program");
+  });
+
+  test("Test that a non-existent state returns a 400", async () => {
+    const copyFieldDataSpy = jest.spyOn(reportUtils, "copyFieldDataFromSource");
+    const res = await createReport(creationEventInvalidState, null);
+    expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
+    expect(copyFieldDataSpy).not.toBeCalled();
+  });
+
+  test("Test that a non-existent report type returns a 400", async () => {
+    const copyFieldDataSpy = jest.spyOn(reportUtils, "copyFieldDataFromSource");
+    const res = await createReport(creationEventMlrReport, null);
+    expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
+    expect(copyFieldDataSpy).not.toBeCalled();
   });
 
   test("Test invalid fields removed when creating report with copyFieldDataSourceId", async () => {
     jest.spyOn(s3Lib, "get").mockResolvedValueOnce({
       stateName: "Alabama",
-      plan: [{ id: "foo", entityField: "bar", name: "name" }],
+      plans: [{ id: "foo", entityField: "bar", name: "name" }],
     });
     const copyFieldDataSpy = jest.spyOn(reportUtils, "copyFieldDataFromSource");
     const res = await createReport(creationEventWithCopySource, null);
@@ -173,7 +290,32 @@ describe("Test createReport API method", () => {
     expect(res.statusCode).toBe(StatusCodes.CREATED);
     expect(copyFieldDataSpy).toBeCalled();
     expect(body.fieldDataId).not.toEqual("mockReportFieldData");
-    expect(body.fieldData).toEqual({ stateName: "Alabama" });
-    expect(body.fieldData.entity).toBeUndefined();
+    expect(body.fieldData).toMatchObject({ stateName: "Alabama" });
+    expect(body.fieldData.plans).toBeDefined();
+    expect(body.fieldData.plans.length).toBe(1);
+    expect(body.fieldData.plans[0]).toEqual({ id: "foo", name: "name" });
+  });
+
+  test("Test entire entity gets removed if it has no valid fields", async () => {
+    jest.spyOn(s3Lib, "get").mockResolvedValueOnce({
+      stateName: "Alabama",
+      plans: [{ entityField: "bar", appeals_foo: "1" }],
+      state_statewideMedicaidEnrollment: "43",
+      state_statewideMedicaidManagedCareEnrollment: "34",
+      sanctions: mockSanctions,
+    });
+    const copyFieldDataSpy = jest.spyOn(reportUtils, "copyFieldDataFromSource");
+    const res = await createReport(creationEventWithCopySource, null);
+    const body = JSON.parse(res.body);
+    expect(res.statusCode).toBe(StatusCodes.CREATED);
+    expect(copyFieldDataSpy).toBeCalled();
+    expect(body.fieldDataId).not.toEqual("mockReportFieldData");
+    expect(body.fieldData).toMatchObject({ stateName: "Alabama" });
+    expect(body.fieldData.plans).toBeUndefined();
+    expect(body.fieldData.sanctions).toBeUndefined();
+    expect(body.fieldData.state_statewideMedicaidEnrollment).toBeUndefined();
+    expect(
+      body.fieldData.state_statewideMedicaidManagedCareEnrollment
+    ).toBeUndefined();
   });
 });
