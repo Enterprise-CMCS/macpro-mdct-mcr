af3223f1c636e716071b3f387e84f7817eb376f0
Nick Summers <19439679+ntsummers1@users.noreply.github.com>
2023-11-22 12:07:16 -0500

    Dont Dangerously Render HTML (#11526)
    Co-authored-by: Nicholas Summers <nick@coforma.io>

--- services/ui-src/src/utils/other/rendering.ts
+++ services/ui-src/src/utils/other/rendering.ts
@@ -1,11 +1,4 @@
-import React from "react";
 import { EntityShape } from "types";
-
-// render '<' special character
-export const renderHtml = (rawHTML: string) =>
-  React.createElement("span", {
-    dangerouslySetInnerHTML: { __html: rawHTML },
-  });
 
 // return MLR eligibility group text
 export const eligibilityGroup = (entity: EntityShape) => {
