f4c0cd847f475d87e4e9bc75593a4147b88c0ad1
karla-vm <karla@coforma.io>
2023-12-07 14:34:44 -0500

    move the auth configuration to index.tsx

--- services/ui-src/src/utils/auth/UserProvider.tsx
+++ services/ui-src/src/utils/auth/UserProvider.tsx
@@ -107,24 +107,6 @@ export const UserProvider = ({ children }: Props) => {
     }
   }, [isProduction, location]);
 
-  // single run configuration
-  useEffect(() => {
-    Auth.configure({
-      mandatorySignIn: true,
-      region: config.cognito.REGION,
-      userPoolId: config.cognito.USER_POOL_ID,
-      identityPoolId: config.cognito.IDENTITY_POOL_ID,
-      userPoolWebClientId: config.cognito.APP_CLIENT_ID,
-      oauth: {
-        domain: config.cognito.APP_CLIENT_DOMAIN,
-        redirectSignIn: config.cognito.REDIRECT_SIGNIN,
-        redirectSignOut: config.cognito.REDIRECT_SIGNOUT,
-        scope: ["email", "openid", "profile"],
-        responseType: "code",
-      },
-    });
-  }, []);
-
   // rerender on auth state change, checking router location
   useEffect(() => {
     checkAuthState();
