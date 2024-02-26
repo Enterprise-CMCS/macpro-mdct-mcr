f5741ee938401d6c47acef8dfe438b290d42f53d
Garrett Rabian <garrett@coforma.io>
2024-02-26 14:28:33 -0500

    remove comments

--- services/app-api/handlers/templates/fetch.ts
+++ services/app-api/handlers/templates/fetch.ts
@@ -11,31 +11,21 @@ export const fetchTemplate = handler(async (event, _context) => {
   }
   let key;
   if (event.pathParameters.templateName === "MCPAR") {
-    // eslint-disable-next-line no-console
-    console.log("getting mcpar");
     key = TemplateKeys.MCPAR;
   } else if (event.pathParameters.templateName === "MLR") {
     key = TemplateKeys.MLR;
   } else if (event.pathParameters.templateName === "NAAAR") {
     key = TemplateKeys.NAAAR;
   } else {
-    // eslint-disable-next-line no-console
-    console.log("no matching template name");
     throw new Error(error.INVALID_TEMPLATE_NAME);
   }
   // get the signed URL string
 
-  // eslint-disable-next-line no-console
-  console.log("process.env.TEMPLATE_BUCKET", process.env.TEMPLATE_BUCKET);
-  // eslint-disable-next-line no-console
-  console.log("key", key);
   const params = {
     Bucket: process.env.TEMPLATE_BUCKET!,
     Expires: 60,
     Key: key,
   };
   const url = await s3Lib.getSignedDownloadUrl(params);
-  // eslint-disable-next-line no-console
-  console.log("url", url);
   return { status: StatusCodes.SUCCESS, body: url };
 });
--- services/ui-src/src/utils/api/requestMethods/getTemplateUrl.ts
+++ services/ui-src/src/utils/api/requestMethods/getTemplateUrl.ts
@@ -8,9 +8,5 @@ export async function getSignedTemplateUrl(templateName: string) {
   };
 
   const response = await API.get("mcr", `/templates/${templateName}`, request);
-  // eslint-disable-next-line no-console
-  console.log("response", response);
-  // eslint-disable-next-line no-console
-  console.log("response.body", response?.body);
   return response;
 }
