3aafebe4fadcc7d80fdcca3b91910013866816e8
Doug White <dwhitestatiform@Dougs-MacBook-Pro.local>
2024-03-25 10:11:19 -0400

    removing refernces to dependabot

--- .github/setBranchName.sh
+++ .github/setBranchName.sh
@@ -7,9 +7,6 @@ GITHUB_REFNAME="${1}"
 [ -z "${GITHUB_REFNAME}" ] && echo "Error setting branch name.  No input given." && exit 1
 
 case ${GITHUB_REFNAME} in
-  $([[ "$GITHUB_REFNAME" =~ ^dependabot/.* ]] && echo ${GITHUB_REFNAME}))
-    echo ${GITHUB_REFNAME} | md5sum | head -c 10 | sed 's/^/x/'
-    ;;
   $([[ "$GITHUB_REFNAME" =~ ^snyk-* ]] && echo ${GITHUB_REFNAME}))
     echo ${GITHUB_REFNAME##*-} | head -c 10 | sed 's/^/s/'
     ;;
