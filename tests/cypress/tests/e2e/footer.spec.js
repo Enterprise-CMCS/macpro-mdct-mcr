a365dda165ff6a75d5830b16fbc6e4fd1cf7e988
Garrett Rabian <garrett.rabian@coforma.io>
2024-05-10 15:11:55 -0400

    remove redundant step

--- tests/cypress/tests/e2e/footer.spec.js
+++ tests/cypress/tests/e2e/footer.spec.js
@@ -6,10 +6,6 @@ beforeEach(() => {
   cy.authenticate("stateUser");
 });
 
-afterEach(() => {
-  cy.navigateToHomePage();
-});
-
 describe("Footer integration tests", () => {
   it("Footer help link navigates to /help", () => {
     cy.contains(helpLinkText).click();
