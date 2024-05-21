bc87776f9d9c9b14c51222976c9bdb24ec0524b8
Doug White <doug.white@stratiform.digital>
2024-05-21 15:01:37 -0400

    removing comments and what i think is not needed code

--- services/ui-src/scripts/configure-local.sh
+++ services/ui-src/scripts/configure-local.sh
@@ -44,10 +44,6 @@ export COGNITO_REDIRECT_SIGNIN=http://localhost:3000/
 export COGNITO_REDIRECT_SIGNOUT=http://localhost:3000/postLogout
 export S3_ATTACHMENTS_BUCKET_REGION=$s3_attachments_bucket_region
 export S3_ATTACHMENTS_BUCKET_NAME=$s3_attachements_bucket_name
-export CYPRESS_ADMIN_USER_EMAIL=$CYPRESS_ADMIN_USER_EMAIL
-export CYPRESS_ADMIN_USER_PASSWORD=$CYPRESS_ADMIN_USER_PASSWORD
-export CYPRESS_STATE_USER_EMAIL=$CYPRESS_STATE_USER_EMAIL
-export CYPRESS_STATE_USER_PASSWORD=$CYPRESS_STATE_USER_PASSWORD
 # This is set to false, as using this script points your local react server to Amazon
 export LOCAL_LOGIN=false
 ./configure-env.sh
