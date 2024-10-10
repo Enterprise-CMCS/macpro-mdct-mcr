781e44cf4abe8f0bac0d03fb9832066112327c28
Garrett Rabian <garrett.rabian@coforma.io>
2024-10-09 20:13:03 -0600

    fix a few tests

--- services/ui-src/src/utils/api/requestMethods/getTemplateUrl.test.ts
+++ services/ui-src/src/utils/api/requestMethods/getTemplateUrl.test.ts
@@ -2,8 +2,16 @@ import { getSignedTemplateUrl } from "./getTemplateUrl";
 
 const testTemplateName = "TestName";
 
+jest.mock("aws-amplify/api", () => ({
+  get: jest.fn().mockImplementation(() => {
+    return {
+      response: "success",
+    };
+  }),
+}));
+
 describe("Test template methods", () => {
-  test("getSignedTemplateUrl", () => {
-    expect(getSignedTemplateUrl(testTemplateName)).toBeTruthy();
+  test("getSignedTemplateUrl", async () => {
+    expect(await getSignedTemplateUrl(testTemplateName)).toBeTruthy();
   });
 });
