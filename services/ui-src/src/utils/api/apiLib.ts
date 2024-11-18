94cb7dea8c392d7eb1d7665e1239ccaf390d2a89
benmartin-coforma <126210497+benmartin-coforma@users.noreply.github.com>
2024-11-18 14:27:41 -0700

    Fix signout maybe (#11931)

--- services/ui-src/src/utils/api/apiLib.ts
+++ services/ui-src/src/utils/api/apiLib.ts
@@ -40,7 +40,7 @@ export async function getRequestHeaders(): Promise<RequestHeaders | undefined> {
 }
 
 export async function getTokens(): Promise<AuthTokens | undefined> {
-  return (await fetchAuthSession()).tokens;
+  return (await fetchAuthSession({ forceRefresh: true })).tokens;
 }
 
 export async function authenticateWithIDM(): Promise<void> {
