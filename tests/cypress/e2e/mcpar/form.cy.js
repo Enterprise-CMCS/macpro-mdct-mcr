de56a7c57ef82c32cb4056c2bdb1d92a725277cc
Bangbay Siboliban <bangbay@bluetiger.digital>
2024-11-22 17:01:28 -0500

    Remove waitForAnimation: false

--- tests/cypress/e2e/mcpar/form.cy.js
+++ tests/cypress/e2e/mcpar/form.cy.js
@@ -145,7 +145,7 @@ const traverseRoute = (route) => {
     completeDrawerForm(route.drawerForm);
 
     cy.get('button:contains("Continue")').as("mcparContinueButton").focus();
-    cy.get("@mcparContinueButton").click({ waitForAnimations: false });
+    cy.get("@mcparContinueButton").click();
   }
   //If this route has children routes, traverse those as well
   if (route.children) traverseRoutes(route.children);
