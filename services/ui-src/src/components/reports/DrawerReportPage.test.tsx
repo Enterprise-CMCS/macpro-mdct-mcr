82cadc7ea7204a5833aecf9c53fa4409feb40929
Garrett Rabian <garrett.rabian@coforma.io>
2025-01-15 13:55:00 -0500

    add test coverage

--- services/ui-src/src/components/reports/DrawerReportPage.test.tsx
+++ services/ui-src/src/components/reports/DrawerReportPage.test.tsx
@@ -384,44 +384,65 @@ describe("Test DrawerReportPage with custom entities", () => {
     expect(enterDefaultMethod).toBeVisible();
   });
 
-  it("Can shows statusing for custom analysis methods", async () => {
-    const mockNaaarReportContextWithCustomAnalysisMethods: any =
-      mockNaaarReportContext;
-    mockNaaarReportContextWithCustomAnalysisMethods.report.fieldData[
-      "analysisMethods"
-    ] = [
-      DEFAULT_ANALYSIS_METHODS[0],
-      {
-        id: "custom_entity",
-        name: "custom entity",
-      },
-    ];
+  describe("test analysis methods custom logic", () => {
+    beforeEach(() => {
+      const mockNaaarReportContextWithCustomAnalysisMethods: any =
+        mockNaaarReportContext;
+      mockNaaarReportContextWithCustomAnalysisMethods.report.fieldData[
+        "analysisMethods"
+      ] = [
+        DEFAULT_ANALYSIS_METHODS[0],
+        {
+          id: "custom_entity",
+          name: "custom entity",
+        },
+      ];
 
-    const mockCustomNaaarReportStore = {
-      ...mockNaaarReportStore,
-      report: mockNaaarReportContextWithCustomAnalysisMethods.report,
-      reportsByState: [mockNaaarReportContextWithCustomAnalysisMethods.report],
-    };
+      const mockCustomNaaarReportStore = {
+        ...mockNaaarReportStore,
+        report: mockNaaarReportContextWithCustomAnalysisMethods.report,
+        reportsByState: [
+          mockNaaarReportContextWithCustomAnalysisMethods.report,
+        ],
+      };
 
-    mockedUseStore.mockReturnValue({
-      ...mockStateUserStore,
-      ...mockCustomNaaarReportStore,
-      ...mockAnalysisMethodEntityStore,
+      mockedUseStore.mockReturnValue({
+        ...mockStateUserStore,
+        ...mockCustomNaaarReportStore,
+        ...mockAnalysisMethodEntityStore,
+      });
+
+      const drawerReportPageWithCustomEntities = (
+        <RouterWrappedComponent>
+          <ReportContext.Provider
+            value={mockNaaarReportContextWithCustomAnalysisMethods}
+          >
+            <DrawerReportPage route={mockNaaarAnalysisMethodsPageJson} />
+          </ReportContext.Provider>
+        </RouterWrappedComponent>
+      );
+
+      render(drawerReportPageWithCustomEntities);
     });
 
-    const drawerReportPageWithCustomEntities = (
-      <RouterWrappedComponent>
-        <ReportContext.Provider
-          value={mockNaaarReportContextWithCustomAnalysisMethods}
-        >
-          <DrawerReportPage route={mockNaaarAnalysisMethodsPageJson} />
-        </ReportContext.Provider>
-      </RouterWrappedComponent>
-    );
+    it("Can shows statusing for custom analysis methods", async () => {
+      const iconAltText = screen.getAllByAltText("Entity is incomplete");
+      expect(iconAltText.length).toBeGreaterThan(0);
+    });
 
-    render(drawerReportPageWithCustomEntities);
-    const iconAltText = screen.getAllByAltText("Entity is incomplete");
-    expect(iconAltText.length).toBeGreaterThan(0);
+    it("DrawerReportPage opens the delete modal on remove click", async () => {
+      const addCustomMethod = screen.getByText("Add other analysis method");
+      const removeButton = screen.getByTestId("delete-entity");
+      await userEvent.click(removeButton);
+      // click delete in modal
+      const deleteButton = screen.getByText("Yes, delete method");
+      await userEvent.click(deleteButton);
+
+      // verify that the field is removed
+      const inputBoxLabelAfterRemove = screen.queryAllByTestId("test-label");
+      expect(inputBoxLabelAfterRemove).toHaveLength(0);
+      expect(addCustomMethod).toBeVisible();
+    });
   });
 });
 
