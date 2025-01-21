ad137140bee8c1ff2e5ad9664cfa5c059ab17eed
Garrett Rabian <garrett.rabian@coforma.io>
2025-01-21 09:37:53 -0500

    update app components

--- services/ui-src/src/components/app/Error.test.tsx
+++ services/ui-src/src/components/app/Error.test.tsx
@@ -1,21 +1,18 @@
 import { render, screen } from "@testing-library/react";
-import { axe } from "jest-axe";
+// components
 import { Error } from "components";
+// utils
+import { testA11y } from "utils/testing/commonTests";
+// verbiage
 import error from "verbiage/pages/error";
 
 const errorView = <Error />;
 
-describe("Test Error view", () => {
+describe("<Error />", () => {
   test("Check that Error page renders", () => {
     render(errorView);
     expect(screen.getByText(error.header)).toBeVisible();
   });
-});
 
-describe("Test Error view accessibility", () => {
-  test("Should not have basic accessibility issues", async () => {
-    const { container } = render(errorView);
-    const results = await axe(container);
-    expect(results).toHaveNoViolations();
-  });
+  testA11y(errorView);
 });
