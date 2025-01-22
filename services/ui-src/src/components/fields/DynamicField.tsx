3c8e94c0fa6910b948d933e10b48e0c283dbc9b4
Garrett Rabian <garrett.rabian@coforma.io>
2025-01-22 13:02:33 -0500

    ensure plans are removed from analysis methods when deleted

--- services/ui-src/src/components/fields/DynamicField.tsx
+++ services/ui-src/src/components/fields/DynamicField.tsx
@@ -140,6 +140,19 @@ export const DynamicField = ({ name, label, ...props }: Props) => {
         }
       );
 
+      // filter analysis methods to remove deleted plans
+      const filteredAnalysisMethods = report?.fieldData?.analysisMethods?.map(
+        (method: EntityShape) => {
+          if (method.analysis_method_applicable_plans?.length) {
+            method.analysis_method_applicable_plans =
+              method.analysis_method_applicable_plans.filter(
+                (plan: AnyObject) => plan.key !== selectedRecord.id
+              );
+          }
+          return method;
+        }
+      );
+
       // delete ILOS data from corresponding plans
       const filteredPlans =
         name === "plans"
@@ -188,6 +201,7 @@ export const DynamicField = ({ name, label, ...props }: Props) => {
           sanctions: filteredSanctions,
           qualityMeasures: filteredQualityMeasures,
           plans: filteredPlans,
+          analysisMethods: filteredAnalysisMethods,
         },
       };
 
