581cd9df15aec7a2bbd6e7a5dbe093a328c9bf93
Bangbay Siboliban <bangbay@bluetiger.digital>
2025-03-18 12:34:37 -0700

    Reorganize tests

--- services/ui-src/src/utils/text/compareText.test.ts
+++ services/ui-src/src/utils/text/compareText.test.ts
@@ -1,55 +1,14 @@
 import { compareText, otherSpecify } from "./compareText";
 
-describe("utils/text/otherText", () => {
+describe("utils/text/compareText", () => {
   const otherSpecifyText = "Other, specify";
   const testText = "Test";
   const matchText = "Matched";
   const nonMatchText = "Not Matched";
 
-  describe("otherSpecify()", () => {
-    describe("matches", () => {
-      test("returns matchText", () => {
-        const result = otherSpecify(otherSpecifyText, matchText, nonMatchText);
-        expect(result).toBe(matchText);
-      });
-
-      test("returns null matchText", () => {
-        const result = otherSpecify(otherSpecifyText, null, nonMatchText);
-        expect(result).toBeNull();
-      });
-    });
-
-    describe("non-matches", () => {
-      test("returns nonMatchText", () => {
-        const result = otherSpecify(testText, matchText, nonMatchText);
-        expect(result).toBe(nonMatchText);
-      });
-
-      test("returns null nonMatchText", () => {
-        const result = otherSpecify(testText, matchText, null);
-        expect(result).toBeNull();
-      });
-
-      test("returns textToCompare for undefined nonMatchText", () => {
-        const result = otherSpecify(testText, matchText);
-        expect(result).toBe(testText);
-      });
-
-      test("returns nonMatchText for null textToCompare", () => {
-        const result = otherSpecify(null, matchText, nonMatchText);
-        expect(result).toBe(nonMatchText);
-      });
-
-      test("returns nonMatchText for undefined textToCompare", () => {
-        const result = otherSpecify(undefined, matchText, nonMatchText);
-        expect(result).toBe(nonMatchText);
-      });
-    });
-  });
-
   describe("compareText()", () => {
     describe("matches", () => {
-      test("returns ≈", () => {
+      test("returns matchText", () => {
         const result = compareText(testText, testText, matchText, nonMatchText);
         expect(result).toBe(matchText);
       });
@@ -94,6 +53,47 @@ describe("utils/text/otherText", () => {
       test("returns undefined nonMatchText for undefined textToCompare", () => {
         const result = compareText(testText, undefined, matchText);
         expect(result).toBe(undefined);
+      });
+    });
+  });
+
+  describe("otherSpecify()", () => {
+    describe("matches", () => {
+      test("returns matchText", () => {
+        const result = otherSpecify(otherSpecifyText, matchText, nonMatchText);
+        expect(result).toBe(matchText);
+      });
+
+      test("returns null matchText", () => {
+        const result = otherSpecify(otherSpecifyText, null, nonMatchText);
+        expect(result).toBeNull();
+      });
+    });
+
+    describe("non-matches", () => {
+      test("returns nonMatchText", () => {
+        const result = otherSpecify(testText, matchText, nonMatchText);
+        expect(result).toBe(nonMatchText);
+      });
+
+      test("returns null nonMatchText", () => {
+        const result = otherSpecify(testText, matchText, null);
+        expect(result).toBeNull();
+      });
+
+      test("returns textToCompare for undefined nonMatchText", () => {
+        const result = otherSpecify(testText, matchText);
+        expect(result).toBe(testText);
+      });
+
+      test("returns nonMatchText for null textToCompare", () => {
+        const result = otherSpecify(null, matchText, nonMatchText);
+        expect(result).toBe(nonMatchText);
+      });
+
+      test("returns nonMatchText for undefined textToCompare", () => {
+        const result = otherSpecify(undefined, matchText, nonMatchText);
+        expect(result).toBe(nonMatchText);
       });
     });
   });
--- services/ui-src/src/utils/text/compareText.ts
+++ services/ui-src/src/utils/text/compareText.ts
@@ -1,26 +1,26 @@
-export function otherSpecify(
-  textToCompare?: string | null,
-  matchText?: string | null,
-  nonMatchText?: string | null
-) {
-  return compareText("Other, specify", textToCompare, matchText, nonMatchText);
-}
-
 export function compareText(
   textToMatch: string | boolean | null,
   textToCompare?: string | boolean | null,
   matchText?: string | null,
   nonMatchText?: string | null
 ) {
-  // textToMatch is required, so early return no match
+  // textToMatch is required, so early return non-match
   if (textToCompare === undefined) {
     return nonMatchText;
   }
 
   return textToCompare === textToMatch
     ? matchText
-    : // return textToCompare only if undefined nonMatchText to allow null nonMatchText
+    : // return textToCompare only with undefined nonMatchText to allow null nonMatchText
     nonMatchText !== undefined
     ? nonMatchText
     : textToCompare;
+}
+
+export function otherSpecify(
+  textToCompare?: string | null,
+  matchText?: string | null,
+  nonMatchText?: string | null
+) {
+  return compareText("Other, specify", textToCompare, matchText, nonMatchText);
 }
