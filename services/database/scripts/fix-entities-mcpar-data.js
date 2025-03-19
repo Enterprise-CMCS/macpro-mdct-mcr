eaa4f784cefee8942bef17f53069705f90771fba
karla-vm <karla@coforma.io>
2025-03-19 15:43:00 -0400

    finally it works

--- services/database/scripts/fix-entities-mcpar-data.js
+++ services/database/scripts/fix-entities-mcpar-data.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 /*
  * Local:
- *    `DYNAMODB_URL="http://localhost:8000" S3_LOCAL_ENDPOINT="http://localhost:4569" node services/database/scripts/fix-entities-mcpar-data.js`
+ *    `DYNAMODB_URL="http://localhost:8000" S3_LOCAL_ENDPOINT="http://localhost:4569" node services/database/scripts/fix-entities-mcpar-data.js` {{state}} {{fieldDataId}}
  *  Branch:
- *    branchPrefix="YOUR BRANCH NAME" node services/database/scripts/fix-entities-mcpar-data.js
+ *    branchPrefix="YOUR BRANCH NAME" node services/database/scripts/fix-entities-mcpar-data.js {{state}} {{fieldDataId}}
  */
 
 const { buildS3Client, getObject, list, putObject } = require("./utils/s3.js");
@@ -13,6 +13,9 @@ const isLocal = !!process.env.DYNAMODB_URL;
 const mcparBucketName = isLocal
   ? "local-mcpar-form"
   : "database-" + process.env.branchPrefix + "-mcpar";
+
+const state = process.argv.slice(2, 3);
+const fieldDataId = process.argv.slice(3);
 
 async function handler() {
   try {
@@ -39,42 +42,26 @@ async function updateS3Items() {
   console.log(`Processing bucket ${mcparBucketName}`);
   const allObjects = await list({
     Bucket: mcparBucketName,
-    Prefix: "fieldData/",
+    Prefix: `fieldData/${state}/${fieldDataId}`,
   });
-  const filteredObjects = filterS3ObjectsById(allObjects);
-  await transformS3Objects(filteredObjects);
+  await transformS3Object(allObjects[0]);
   console.log(
-    `Touched ${filteredObjects.length} objects in bucket ${mcparBucketName}`
+    `Touched ${allObjects.length} objects in bucket ${mcparBucketName}`
   );
 }
 
-function filterS3ObjectsById(bucketObjects) {
-  return bucketObjects.filter((obj) =>
-    [
-      "2dxVLCY92B2LlM8DrHpEzqy4IOt", // pragma: allowlist secret
-      "2e0c9p1wDwKR7VHaC0OoA05ef7M", // pragma: allowlist secret
-      "2iQ4W9olixiQB6SfhaNlDyxPRx1", // pragma: allowlist secret
-      "2q25RuoPYKXf1tuJuVaUm1VHSVP", // pragma: allowlist secret
-      "2nkfD0lAyvuEEU4N4LqQBtcCi5H", // pragma: allowlist secret
-      "2nkg1D1PauC4yW8qGh4pENkjgJi", // pragma: allowlist secret
-    ].contains(obj.Id)
-  );
-}
-
-async function transformS3Objects(objects) {
-  for (const object of objects) {
-    const s3FieldData = await getObject({
-      Key: object.Key,
-      Bucket: mcparBucketName,
-    });
-    fixBadEntityData(s3FieldData);
-    await putObject({
-      Bucket: mcparBucketName,
-      Key: object.Key,
-      Body: JSON.stringify(s3FieldData),
-      ContentType: "application/json",
-    });
-  }
+async function transformS3Object(object) {
+  const s3FieldData = await getObject({
+    Key: object.Key,
+    Bucket: mcparBucketName,
+  });
+  fixBadEntityData(s3FieldData);
+  await putObject({
+    Bucket: mcparBucketName,
+    Key: object.Key,
+    Body: JSON.stringify(s3FieldData),
+    ContentType: "application/json",
+  });
 }
 
 // any matching "qualityMeasures" entities are set to empty arrays
