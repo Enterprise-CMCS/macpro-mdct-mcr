755c7c70e712149802cbc6f3fe02b5095cd13466
Garrett Rabian <garrett.rabian@coforma.io>
2025-04-08 09:38:26 -0400

    add naaar admin dashboard content

--- services/ui-src/src/verbiage/pages/naaar/naaar-dashboard.ts
+++ services/ui-src/src/verbiage/pages/naaar/naaar-dashboard.ts
@@ -36,6 +36,77 @@ export default {
         ],
       },
     ],
+    adminBody: [
+      {
+        type: "p",
+        children: [
+          {
+            type: "html",
+            content:
+              "States must complete one report for each managed care program operating in the state.  Medicaid and Medicare managed care plan MMPs are not exempt from NAAAR requirements at 42 CFR 438.207, and states must submit the tool for these plans. To reduce duplication, states can complete network adequacy sections of the tool (II.A.1-II.A.5) for Medicaid-only covered services. Reporting on Children’s Health Insurance Program (CHIP), Non-Emergency Medical Transportation (NEMT), Program of All-Inclusive Care for the Elderly (PACE) programs/plans is not required. ",
+          },
+          {
+            type: "externalLink",
+            content: "Learn more about NAAAR.",
+            props: {
+              href: "https://www.google.com",
+              target: "_blank",
+              "aria-label": "Learn more (link opens in new tab)",
+            },
+          },
+        ],
+      },
+      {
+        type: "p",
+        content: "CMS Admin Instructions",
+        props: {
+          style: {
+            marginTop: "1.5rem",
+            fontWeight: "600",
+          },
+        },
+      },
+      {
+        type: "ul",
+        props: {
+          style: {
+            paddingLeft: "1rem",
+          },
+        },
+        children: [
+          {
+            type: "li",
+            children: [
+              {
+                type: "html",
+                content:
+                  "To allow a state to make corrections or edits to a submission use “Unlock” to release the submission. The status will change to “In revision”.",
+              },
+            ],
+          },
+          {
+            type: "li",
+            children: [
+              {
+                type: "html",
+                content:
+                  "Submission count is shown in the # column. Submissions started and submitted once have a count of 1. When a state resubmits a previous submission, the count increases by 1.",
+              },
+            ],
+          },
+          {
+            type: "li",
+            children: [
+              {
+                type: "html",
+                content:
+                  "To archive a submission and hide it from a state’s dashboard, use “Archive”.",
+              },
+            ],
+          },
+        ],
+      },
+    ],
   },
   body: {
     table: {
