b18cc9220b97bb2aac961137e9954cb5c322338c
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-04-11 11:02:33 -0500

    Getting s3 local working again until CDK migration (#12107)

--- src/run.ts
+++ src/run.ts
@@ -68,6 +68,20 @@ async function run_api_locally(runner: LabeledProcessRunner) {
   );
 }
 
+// run_s3_locally runs s3 locally
+async function run_s3_locally(runner: LabeledProcessRunner) {
+  await runner.run_command_and_output(
+    "s3 yarn",
+    ["yarn", "install"],
+    "services/uploads"
+  );
+  runner.run_command_and_output(
+    "s3",
+    ["serverless", "s3", "start", "--stage", "local"],
+    "services/uploads"
+  );
+}
+
 // run_fe_locally runs the frontend and its dependencies locally
 async function run_fe_locally(runner: LabeledProcessRunner) {
   await runner.run_command_and_output(
@@ -89,6 +103,7 @@ async function run_all_locally() {
   const runner = new LabeledProcessRunner();
 
   run_db_locally(runner);
+  run_s3_locally(runner);
   run_api_locally(runner);
   run_fe_locally(runner);
 }
