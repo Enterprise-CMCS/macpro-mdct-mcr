bbc5983af8c7bf49c337ff2dd28947a17ffe8dae
Garrett Rabian <garrett.rabian@coforma.io>
2025-04-15 14:20:08 -0400

    list plans in export view

--- services/ui-src/src/components/export/ExportedPlanOverlayReportSection.tsx
+++ services/ui-src/src/components/export/ExportedPlanOverlayReportSection.tsx
@@ -1,12 +1,27 @@
 // components
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 // types
-import { PlanOverlayReportPageShape } from "types";
+import { EntityShape, PlanOverlayReportPageShape } from "types";
+// utils
+import { useStore } from "utils";
 
+/*
+ * Designed originally for the plan compliance portion of the NAAAR report
+ * Expected entity is plans
+ */
 export const ExportedPlanOverlayReportSection = ({ section }: Props) => {
+  const { report } = useStore();
+
+  const plans = report?.fieldData?.plans;
+
+  if (!plans) {
+    return null;
+  }
+
   return (
     <Box data-testid="exportedPlanOverlayReportSection">
       <Text>{section.name}</Text>
+      {displayPlansList(plans)}
     </Box>
   );
 };
@@ -14,3 +29,20 @@ export const ExportedPlanOverlayReportSection = ({ section }: Props) => {
 export interface Props {
   section: PlanOverlayReportPageShape;
 }
+
+const displayPlansList = (plans: EntityShape[]) => {
+  return plans.map((plan: EntityShape) => {
+    return (
+      <Heading as="h3" sx={sx.planNameHeading} key={plan.id}>
+        {plan.name}
+      </Heading>
+    );
+  });
+};
+
+const sx = {
+  planNameHeading: {
+    fontSize: "xl",
+    paddingBottom: "1.5rem",
+  },
+};
