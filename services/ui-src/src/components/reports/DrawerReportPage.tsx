0457f143cda337307515f1606a636d817ff2fb57
Britt <britt.bujacich@gmail.com>
2025-05-07 08:34:57 -0600

    Cmdct 4597 - remove filtering of provider types in the standards drawer (#12154)

--- services/ui-src/src/components/reports/DrawerReportPage.tsx
+++ services/ui-src/src/components/reports/DrawerReportPage.tsx
@@ -75,7 +75,6 @@ export const DrawerReportPage = ({ route, validateOnRender }: Props) => {
   const hasPlans = plans?.length > 0;
   const isReportingOnStandards =
     route.path === "/naaar/program-level-access-and-network-adequacy-standards";
-  const hasProviderTypes = report?.fieldData?.["providerTypes"]?.length > 0;
 
   // check if user has completed default analysis methods (NAAAR)
   const completedAnalysisMethods = () => {
@@ -242,8 +241,7 @@ export const DrawerReportPage = ({ route, validateOnRender }: Props) => {
       );
     } else if (
       (isAnalysisMethodsPage && !hasPlans) ||
-      (isReportingOnStandards && !completedAnalysisMethods()) ||
-      (isReportingOnStandards && !hasProviderTypes)
+      (isReportingOnStandards && !completedAnalysisMethods())
     ) {
       return (
         <Box sx={sx.missingEntity}>
@@ -259,20 +257,18 @@ export const DrawerReportPage = ({ route, validateOnRender }: Props) => {
     : "";
   const dashTitle = `${verbiage.dashboardTitle}${entityCount}`;
 
-  const isAddStandardsDisabled =
-    !hasProviderTypes || !completedAnalysisMethods();
   const addStandardsButton = (
     <Button
       sx={sx.addStandardsButton}
       leftIcon={
         <Image
           sx={sx.buttonIcons}
-          src={isAddStandardsDisabled ? addIconSVG : addIconWhite}
+          src={completedAnalysisMethods() ? addIconWhite : addIconSVG}
           alt="Add"
         />
       }
       onClick={() => openRowDrawer()}
-      disabled={isAddStandardsDisabled}
+      disabled={!completedAnalysisMethods()}
     >
       {verbiage.addEntityButtonText}
     </Button>
