0589b234f273e6438ddb23fed7811d7e8c0313be
Karla Valcárcel Martínez <99458559+karla-vm@users.noreply.github.com>
2025-05-09 10:15:31 -0400

    CMDCT-4529: Missing Information Text Updates (#12162)

--- services/ui-src/src/components/reports/OverlayReportPage.tsx
+++ services/ui-src/src/components/reports/OverlayReportPage.tsx
@@ -319,5 +319,15 @@ const sx = {
     ol: {
       paddingLeft: "1rem",
     },
+    ul: {
+      display: "contents",
+    },
+    li: {
+      marginLeft: "2rem",
+      lineHeight: "2rem",
+      "&:first-of-type": {
+        paddingTop: "0.75rem",
+      },
+    },
   },
 };
