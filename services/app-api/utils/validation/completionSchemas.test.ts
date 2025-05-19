4e766b94df7ff827b2836b580400b25122411d76
Bangbay Siboliban <bangbay@bluetiger.digital>
2025-05-19 11:52:00 -0400

    Add numberSuppressible to schema and suppressionText to constants

--- services/app-api/utils/validation/completionSchemas.test.ts
+++ services/app-api/utils/validation/completionSchemas.test.ts
@@ -5,139 +5,134 @@ import {
   validNumber,
   numberNotLessThanOne,
   numberNotLessThanZero,
-  validNAValues,
   dropdownOptional,
+  numberSuppressible,
 } from "./completionSchemas";
+// constants
+import { suppressionText } from "../constants/constants";
+// utils
+import {
+  badDropdownTestCases,
+  badNumberCompletionTestCases,
+  badRatioTestCases,
+  badValidNumberTestCases,
+  goodDropdownTestCases,
+  goodNumberCompletionTestCases,
+  goodPositiveNumberTestCases,
+  goodRatioTestCases,
+  goodValidNumberTestCases,
+  negativeNumberTestCases,
+  zeroTest,
+} from "../testing/mocks/mockSchemaValidation";
 
 describe("Schemas", () => {
-  const goodNumberTestCases = [
-    "123",
-    "123.00",
-    "123..00",
-    "1,230",
-    "1,2,30",
-    "1230",
-    "123450123..,,,.123123123123",
-    ...validNAValues,
-  ];
-  const badNumberTestCases = ["abc", "N", "", "!@#!@%"];
-
-  const zeroTest = ["0", "0.0"];
-
-  const goodPositiveNumberTestCases = [
-    "123",
-    "123.00",
-    "123..00",
-    "1,230",
-    "1,2,30",
-    "1230",
-    "123450123..,,,.123123123123",
-  ];
-
-  const negativeNumberTestCases = [
-    "-123",
-    "-123.00",
-    "-123..00",
-    "-1,230",
-    "-1,2,30",
-    "-1230",
-    "-123450123..,,,.123123123123",
-  ];
-
-  const goodRatioTestCases = [
-    "1:1",
-    "123:123",
-    "1,234:1.12",
-    "0:1",
-    "1:10,000",
-  ];
-
-  const badRatioTestCases = [
-    ":",
-    ":1",
-    "1:",
-    "1",
-    "1234",
-    "abc",
-    "N/A",
-    "abc:abc",
-    ":abc",
-    "abc:",
-    "%@#$!ASDF",
-  ];
-
-  const goodValidNumberTestCases = [1, "1", "100000", "1,000,000"];
-
-  const badValidNumberTestCases = ["N/A", "number", "foo"];
-
-  const testSchema = async (
+  const testSchema = async <T>(
     schemaToUse: MixedSchema,
-    testCases: Array<any>,
+    testCases: T[],
     expectedReturn: boolean
   ) => {
-    for (let testCase of testCases) {
-      let test = await schemaToUse.isValid(testCase);
+    for (const testCase of testCases) {
+      const test = await schemaToUse.isValid(testCase);
       expect(test).toEqual(expectedReturn);
     }
   };
 
+  const testAnySchema = (
+    schemaToUse: MixedSchema,
+    testCases: Array<any>,
+    expectedReturn: boolean
+  ) => {
+    testSchema<any>(schemaToUse, testCases, expectedReturn);
+  };
+
+  const testNumberSchema = (
+    schemaToUse: MixedSchema,
+    testCases: string[],
+    expectedReturn: boolean
+  ) => {
+    testSchema<string>(schemaToUse, testCases, expectedReturn);
+  };
+
+  const testTextSchema = (
+    schemaToUse: MixedSchema,
+    testCases: Array<string | undefined>,
+    expectedReturn: boolean
+  ) => {
+    testSchema<string | undefined>(schemaToUse, testCases, expectedReturn);
+  };
+
+  const testValidNumber = (
+    schemaToUse: MixedSchema,
+    testCases: Array<string | number>,
+    expectedReturn: boolean
+  ) => {
+    testSchema<string | number>(schemaToUse, testCases, expectedReturn);
+  };
+
   test("Evaluate Number Schema using number scheme", () => {
-    testSchema(number(), goodNumberTestCases, true);
-    testSchema(number(), badNumberTestCases, false);
+    testNumberSchema(number(), goodNumberCompletionTestCases, true);
+    testNumberSchema(number(), badNumberCompletionTestCases, false);
   });
 
   // testing numberNotLessThanOne scheme
   test("Evaluate Number Schema using numberNotLessThanOne scheme", () => {
-    testSchema(numberNotLessThanOne(), goodPositiveNumberTestCases, true);
-    testSchema(numberNotLessThanOne(), badNumberTestCases, false);
+    testNumberSchema(numberNotLessThanOne(), goodPositiveNumberTestCases, true);
+    testNumberSchema(
+      numberNotLessThanOne(),
+      badNumberCompletionTestCases,
+      false
+    );
   });
 
   test("Test zero values using numberNotLessThanOne scheme", () => {
-    testSchema(numberNotLessThanOne(), zeroTest, false);
+    testNumberSchema(numberNotLessThanOne(), zeroTest, false);
   });
 
   test("Test negative values using numberNotLessThanOne scheme", () => {
-    testSchema(numberNotLessThanOne(), negativeNumberTestCases, false);
+    testNumberSchema(numberNotLessThanOne(), negativeNumberTestCases, false);
   });
 
   // testing numberNotLessThanZero scheme
   test("Evaluate Number Schema using numberNotLessThanZero scheme", () => {
-    testSchema(numberNotLessThanZero(), goodPositiveNumberTestCases, true);
-    testSchema(numberNotLessThanZero(), badNumberTestCases, false);
+    testNumberSchema(
+      numberNotLessThanZero(),
+      goodPositiveNumberTestCases,
+      true
+    );
+    testNumberSchema(
+      numberNotLessThanZero(),
+      badNumberCompletionTestCases,
+      false
+    );
   });
 
   test("Test zero values using numberNotLessThanZero scheme", () => {
-    testSchema(numberNotLessThanZero(), zeroTest, true);
+    testNumberSchema(numberNotLessThanZero(), zeroTest, true);
   });
 
   test("Test negative values using numberNotLessThanZero scheme", () => {
-    testSchema(numberNotLessThanZero(), negativeNumberTestCases, false);
+    testNumberSchema(numberNotLessThanZero(), negativeNumberTestCases, false);
   });
 
   test("Evaluate Number Schema using ratio scheme", () => {
-    testSchema(ratio(), goodRatioTestCases, true);
-    testSchema(ratio(), badRatioTestCases, false);
+    testNumberSchema(ratio(), goodRatioTestCases, true);
+    testNumberSchema(ratio(), badRatioTestCases, false);
   });
 
   test("Test validNumber schema", () => {
-    testSchema(validNumber(), goodValidNumberTestCases, true);
-    testSchema(validNumber(), badValidNumberTestCases, false);
+    testValidNumber(validNumber(), goodValidNumberTestCases, true);
+    testValidNumber(validNumber(), badValidNumberTestCases, false);
   });
 
   test("Test DropdownOptional schema", () => {
-    const goodDropdownTestCases = [
-      undefined,
-      { label: "Select", value: "" },
-      { label: "Select", value: "Actual value" },
-    ];
-    const badDropdownTestCases = [
-      "",
-      { label: "", value: "" },
-      { label: "", value: "Actual value" },
-      1,
-      null,
-    ];
-    testSchema(dropdownOptional(), goodDropdownTestCases, true);
-    testSchema(dropdownOptional(), badDropdownTestCases, false);
+    testAnySchema(dropdownOptional(), goodDropdownTestCases, true);
+    testAnySchema(dropdownOptional(), badDropdownTestCases, false);
+  });
+
+  test("Test numberSuppressible schema", () => {
+    testValidNumber(numberSuppressible(), goodValidNumberTestCases, true);
+    testValidNumber(numberSuppressible(), badValidNumberTestCases, false);
+    testTextSchema(numberSuppressible(), [suppressionText], true);
+    testTextSchema(numberSuppressible(), ["badText", undefined], false);
   });
 });
