0d4189eefbd0b29b39335a52609b54358707651a
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2025-05-30 13:39:45 -0400

    Add cognito error messaging (#12213)

--- services/ui-src/src/components/logins/LoginCognito.test.tsx
+++ services/ui-src/src/components/logins/LoginCognito.test.tsx
@@ -5,10 +5,11 @@ import { LoginCognito } from "components";
 // utils
 import { RouterWrappedComponent } from "utils/testing/setupJest";
 import { testA11y } from "utils/testing/commonTests";
+import { loginError } from "verbiage/errors";
 
 const mockLoginUser = jest.fn();
 
-jest.mock("utils", () => ({
+jest.mock("utils/api/apiLib", () => ({
   loginUser: (username: string, password: string) =>
     mockLoginUser(username, password),
 }));
@@ -26,16 +27,33 @@ const loginCognitoComponent = (
 );
 
 describe("<LoginCognito />", () => {
-  test("LoginCognito login calls amplify auth login", async () => {
-    render(loginCognitoComponent);
+  const enterCredentialsAndLogIn = async (email: string, password: string) => {
     const emailInput = screen.getByLabelText("Email");
     const passwordInput = screen.getByLabelText("Password");
     const submitButton = screen.getByRole("button");
-    await userEvent.type(emailInput, "email@address.com");
-    await userEvent.type(passwordInput, "test");
+    await userEvent.type(emailInput, email);
+    await userEvent.type(passwordInput, password);
     await userEvent.click(submitButton);
+  };
+
+  test("LoginCognito login calls amplify auth login", async () => {
+    render(loginCognitoComponent);
+    await enterCredentialsAndLogIn("email@address.com", "test");
+
     expect(mockLoginUser).toHaveBeenCalledWith("email@address.com", "test");
     expect(mockUseNavigate).toHaveBeenCalledWith("/");
+  });
+
+  test("LoginCognito shows error alert for login error", async () => {
+    mockLoginUser.mockRejectedValue({
+      name: "SomethingWentWrong",
+    });
+    render(loginCognitoComponent);
+    await enterCredentialsAndLogIn("email@address.com", "test");
+
+    expect(mockLoginUser).toHaveBeenCalledWith("email@address.com", "test");
+    expect(screen.getByText(loginError.title)).toBeVisible();
+    expect(screen.getByText(loginError.description)).toBeVisible();
   });
 
   testA11y(loginCognitoComponent);
