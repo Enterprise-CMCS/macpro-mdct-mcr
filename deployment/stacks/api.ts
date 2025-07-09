bc40bed97b8afe9b0a1f8f75998744b062c65283
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-07-09 14:37:05 -0400

    Assign individual permissions to lambda functions in a more granual cleaner way

--- deployment/stacks/api.ts
+++ deployment/stacks/api.ts
@@ -1,12 +1,13 @@
 import { Construct } from "constructs";
 import {
   aws_apigateway as apigateway,
+  aws_dynamodb as dynamodb,
   aws_ec2 as ec2,
   aws_iam as iam,
   aws_logs as logs,
-  aws_wafv2 as wafv2,
   aws_s3 as s3,
   aws_s3_notifications as s3notifications,
+  aws_wafv2 as wafv2,
   CfnOutput,
   Duration,
   RemovalPolicy,
@@ -63,6 +64,52 @@ export function createApiComponents(props: CreateApiComponentsProps) {
       allowAllOutbound: true,
     }
   );
+
+  const bannerTable = dynamodb.Table.fromTableArn(
+    scope,
+    "BannerTableLookup",
+    tables.find((table) => table.id === "Banner")!.arn
+  );
+  const formTemplateVersionsTable = dynamodb.Table.fromTableArn(
+    scope,
+    "FormTemplateVersionsTableLookup",
+    tables.find((table) => table.id === "FormTemplateVersions")!.arn
+  );
+  const mcparReportsTable = dynamodb.Table.fromTableArn(
+    scope,
+    "McparReportsTableLookup",
+    tables.find((table) => table.id === "McparReports")!.arn
+  );
+  const mlrReportsTable = dynamodb.Table.fromTableArn(
+    scope,
+    "MlrReportsTableLookup",
+    tables.find((table) => table.id === "MlrReports")!.arn
+  );
+  const naaarReportsTable = dynamodb.Table.fromTableArn(
+    scope,
+    "NaaarReportsTableLookup",
+    tables.find((table) => table.id === "NaaarReports")!.arn
+  );
+
+  type Access = "read" | "write" | "readwrite";
+
+  function grantReportsTableAccess(grantee: iam.IGrantable, access: Access) {
+    const tables = [mcparReportsTable, mlrReportsTable, naaarReportsTable];
+    for (const table of tables) {
+      if (access === "read") table.grantReadData(grantee);
+      else if (access === "write") table.grantWriteData(grantee);
+      else table.grantReadWriteData(grantee);
+    }
+  }
+
+  function grantReportsBucketAccess(grantee: iam.IGrantable, access: Access) {
+    const buckets = [mcparFormBucket, mlrFormBucket, naaarFormBucket];
+    for (const bucket of buckets) {
+      if (access === "read") bucket.grantRead(grantee);
+      else if (access === "write") bucket.grantWrite(grantee);
+      else bucket.grantReadWrite(grantee);
+    }
+  }
 
   const logGroup = new logs.LogGroup(scope, "ApiAccessLogs", {
     removalPolicy: isDev ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
@@ -129,58 +176,11 @@ export function createApiComponents(props: CreateApiComponentsProps) {
     ),
   };
 
-  /*
-   * const additionalPolicies = [
-   *   new iam.PolicyStatement({
-   *     effect: iam.Effect.ALLOW,
-   *     actions: [
-   *       "dynamodb:BatchWriteItem",
-   *       "dynamodb:DeleteItem",
-   *       "dynamodb:GetItem",
-   *       "dynamodb:PutItem",
-   *       "dynamodb:Query",
-   *       "dynamodb:Scan",
-   *       "dynamodb:UpdateItem",
-   *     ],
-   *     resources: tables.map((table) => table.arn),
-   *   }),
-   */
-
-  /*
-   *   new iam.PolicyStatement({
-   *     effect: iam.Effect.ALLOW,
-   *     actions: [
-   *       "dynamodb:DescribeStream",
-   *       "dynamodb:GetRecords",
-   *       "dynamodb:GetShardIterator",
-   *       "dynamodb:ListShards",
-   *       "dynamodb:ListStreams",
-   *     ],
-   *     resources: tables.map((table) => table.streamArn).filter(isDefined),
-   *   }),
-   *   new iam.PolicyStatement({
-   *     effect: iam.Effect.ALLOW,
-   *     actions: ["dynamodb:Query", "dynamodb:Scan"],
-   *     resources: tables.map((table) => `${table.arn}/index/*`),
-   *   }),
-   *   new iam.PolicyStatement({
-   *     effect: iam.Effect.ALLOW,
-   *     actions: [
-   *       "cognito-idp:AdminGetUser",
-   *       "ses:SendEmail",
-   *       "ses:SendRawEmail",
-   *       "lambda:InvokeFunction",
-   *     ],
-   *     resources: ["*"],
-   *   }),
-   * ];
-   */
   const commonProps = {
     brokerString,
     stackName: `${service}-${stage}`,
     api,
     environment,
-    // additionalPolicies,
   };
 
   const requestValidator = new apigateway.RequestValidator(scope, `Validator`, {
@@ -190,32 +190,25 @@ export function createApiComponents(props: CreateApiComponentsProps) {
     validateRequestBody: true,
   });
 
-  new Lambda(scope, "fetchBanner", {
+  const fetchBanner = new Lambda(scope, "fetchBanner", {
     entry: "services/app-api/handlers/banners/fetch.ts",
     handler: "fetchBanner",
     path: "/banners",
     method: "GET",
-    additionalPolicies: [
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["dynamodb:Scan"],
-        resources: tables
-          .filter((table) => ["Banner"].includes(table.id))
-          .map((table) => table.arn),
-      }),
-    ],
     ...commonProps,
-  });
+  }).lambda;
+  bannerTable.grantReadData(fetchBanner);
 
-  new Lambda(scope, "createBanner", {
+  const createBanner = new Lambda(scope, "createBanner", {
     entry: "services/app-api/handlers/banners/create.ts",
     handler: "createBanner",
     path: "/banners",
     method: "POST",
     ...commonProps,
-  });
+  }).lambda;
+  bannerTable.grantWriteData(createBanner);
 
-  new Lambda(scope, "deleteBanner", {
+  const deleteBanner = new Lambda(scope, "deleteBanner", {
     entry: "services/app-api/handlers/banners/delete.ts",
     handler: "deleteBanner",
     path: "/banners/{bannerId}",
@@ -223,39 +216,22 @@ export function createApiComponents(props: CreateApiComponentsProps) {
     requestParameters: ["bannerId"],
     requestValidator,
     ...commonProps,
-  });
+  }).lambda;
+  bannerTable.grantWriteData(deleteBanner);
 
-  new Lambda(scope, "fetchReport", {
+  const fetchReport = new Lambda(scope, "fetchReport", {
     entry: "services/app-api/handlers/reports/fetch.ts",
     handler: "fetchReport",
     path: "/reports/{reportType}/{state}/{id}",
     method: "GET",
     requestParameters: ["reportType", "state", "id"],
     requestValidator,
-    additionalPolicies: [
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["dynamodb:GetItem"],
-        resources: tables
-          .filter((table) =>
-            ["McparReports", "MlrReports", "NaaarReports"].includes(table.id)
-          )
-          .map((table) => table.arn),
-      }),
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["s3:GetObject"],
-        resources: [
-          `${mcparFormBucket.bucketArn}/*`,
-          `${mlrFormBucket.bucketArn}/*`,
-          `${naaarFormBucket.bucketArn}/*`,
-        ],
-      }),
-    ],
     ...commonProps,
-  });
+  }).lambda;
+  grantReportsTableAccess(fetchReport, "read");
+  grantReportsBucketAccess(fetchReport, "read");
 
-  new Lambda(scope, "fetchReportsByState", {
+  const fetchReportsByState = new Lambda(scope, "fetchReportsByState", {
     entry: "services/app-api/handlers/reports/fetch.ts",
     handler: "fetchReportsByState",
     path: "/reports/{reportType}/{state}",
@@ -263,21 +239,11 @@ export function createApiComponents(props: CreateApiComponentsProps) {
     requestParameters: ["reportType", "state"],
     requestValidator,
     timeout: Duration.seconds(30),
-    additionalPolicies: [
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["dynamodb:Query"],
-        resources: tables
-          .filter((table) =>
-            ["McparReports", "MlrReports", "NaaarReports"].includes(table.id)
-          )
-          .map((table) => table.arn),
-      }),
-    ],
     ...commonProps,
-  });
+  }).lambda;
+  grantReportsTableAccess(fetchReportsByState, "read");
 
-  new Lambda(scope, "archiveReport", {
+  const archiveReport = new Lambda(scope, "archiveReport", {
     entry: "services/app-api/handlers/reports/archive.ts",
     handler: "archiveReport",
     path: "/reports/archive/{reportType}/{state}/{id}",
@@ -285,39 +251,23 @@ export function createApiComponents(props: CreateApiComponentsProps) {
     requestParameters: ["reportType", "state", "id"],
     requestValidator,
     ...commonProps,
-  });
+  }).lambda;
+  grantReportsTableAccess(archiveReport, "readwrite");
+  grantReportsBucketAccess(archiveReport, "readwrite");
 
-  new Lambda(scope, "releaseReport", {
+  const releaseReport = new Lambda(scope, "releaseReport", {
     entry: "services/app-api/handlers/reports/release.ts",
     handler: "releaseReport",
     path: "/reports/release/{reportType}/{state}/{id}",
     method: "PUT",
     requestParameters: ["state", "id"],
     requestValidator,
-    additionalPolicies: [
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["dynamodb:GetItem", "dynamodb:PutItem"],
-        resources: tables
-          .filter((table) =>
-            ["McparReports", "MlrReports", "NaaarReports"].includes(table.id)
-          )
-          .map((table) => table.arn),
-      }),
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["s3:GetObject", "s3:PutObject"],
-        resources: [
-          `${mcparFormBucket.bucketArn}/*`,
-          `${mlrFormBucket.bucketArn}/*`,
-          `${naaarFormBucket.bucketArn}/*`,
-        ],
-      }),
-    ],
     ...commonProps,
-  });
+  }).lambda;
+  grantReportsTableAccess(releaseReport, "readwrite");
+  grantReportsBucketAccess(releaseReport, "readwrite");
 
-  new Lambda(scope, "createReport", {
+  const createReport = new Lambda(scope, "createReport", {
     entry: "services/app-api/handlers/reports/create.ts",
     handler: "createReport",
     path: "/reports/{reportType}/{state}",
@@ -328,43 +278,18 @@ export function createApiComponents(props: CreateApiComponentsProps) {
       new iam.PolicyStatement({
         effect: iam.Effect.ALLOW,
         actions: ["dynamodb:Query"],
-        resources: [
-          ...tables
-            .filter((table) => ["FormTemplateVersions"].includes(table.id))
-            .map((table) => `${table.arn}/index/HashIndex`),
-          ...tables
-            .filter((table) => ["FormTemplateVersions"].includes(table.id))
-            .map((table) => table.arn),
-        ],
-      }),
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["dynamodb:PutItem"],
         resources: tables
-          .filter((table) =>
-            [
-              "McparReports",
-              "MlrReports",
-              "NaaarReports",
-              "FormTemplateVersions",
-            ].includes(table.id)
-          )
-          .map((table) => table.arn),
-      }),
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["s3:GetObject", "s3:PutObject"],
-        resources: [
-          `${mcparFormBucket.bucketArn}/*`,
-          `${mlrFormBucket.bucketArn}/*`,
-          `${naaarFormBucket.bucketArn}/*`,
-        ],
+          .filter((table) => ["FormTemplateVersions"].includes(table.id))
+          .map((table) => `${table.arn}/index/HashIndex`),
       }),
     ],
     ...commonProps,
-  });
+  }).lambda;
+  formTemplateVersionsTable.grantReadWriteData(createReport);
+  grantReportsTableAccess(createReport, "write");
+  grantReportsBucketAccess(createReport, "write");
 
-  new Lambda(scope, "updateReport", {
+  const updateReport = new Lambda(scope, "updateReport", {
     entry: "services/app-api/handlers/reports/update.ts",
     handler: "updateReport",
     path: "/reports/{reportType}/{state}/{id}",
@@ -373,30 +298,12 @@ export function createApiComponents(props: CreateApiComponentsProps) {
     requestValidator,
     memorySize: 2048,
     timeout: Duration.seconds(30),
-    additionalPolicies: [
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["dynamodb:GetItem", "dynamodb:PutItem"],
-        resources: tables
-          .filter((table) =>
-            ["McparReports", "MlrReports", "NaaarReports"].includes(table.id)
-          )
-          .map((table) => table.arn),
-      }),
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["s3:GetObject", "s3:PutObject"],
-        resources: [
-          `${mcparFormBucket.bucketArn}/*`,
-          `${mlrFormBucket.bucketArn}/*`,
-          `${naaarFormBucket.bucketArn}/*`,
-        ],
-      }),
-    ],
     ...commonProps,
-  });
+  }).lambda;
+  grantReportsTableAccess(updateReport, "readwrite");
+  grantReportsBucketAccess(updateReport, "readwrite");
 
-  new Lambda(scope, "submitReport", {
+  const submitReport = new Lambda(scope, "submitReport", {
     entry: "services/app-api/handlers/reports/submit.ts",
     handler: "submitReport",
     path: "/reports/submit/{reportType}/{state}/{id}",
@@ -405,28 +312,10 @@ export function createApiComponents(props: CreateApiComponentsProps) {
     requestValidator,
     memorySize: 2048,
     timeout: Duration.seconds(30),
-    additionalPolicies: [
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["dynamodb:GetItem", "dynamodb:PutItem"],
-        resources: tables
-          .filter((table) =>
-            ["McparReports", "MlrReports", "NaaarReports"].includes(table.id)
-          )
-          .map((table) => table.arn),
-      }),
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["s3:GetObject", "s3:PutObject"],
-        resources: [
-          `${mcparFormBucket.bucketArn}/*`,
-          `${mlrFormBucket.bucketArn}/*`,
-          `${naaarFormBucket.bucketArn}/*`,
-        ],
-      }),
-    ],
     ...commonProps,
-  });
+  }).lambda;
+  grantReportsTableAccess(submitReport, "readwrite");
+  grantReportsBucketAccess(submitReport, "readwrite");
 
   new LambdaDynamoEventSource(scope, "postKafkaData", {
     entry: "services/app-api/handlers/kafka/post/postKafkaData.ts",
