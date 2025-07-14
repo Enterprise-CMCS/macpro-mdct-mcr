88cf9400edb3b8d30d1374d2973592959748c166
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-07-14 16:28:44 -0400

    CMDCT-4364 - cdk conversion (DONT MERGE YET!) (#12293)
    Co-authored-by: Pete Dunlap <pete.dunlap@stratiform.digital>

--- src/write-ui-env-file.ts
+++ src/write-ui-env-file.ts
@@ -11,17 +11,24 @@ const region = "us-east-1";
 
 export async function writeLocalUiEnvFile(apiUrl: string) {
   const envVariables = {
-    SKIP_PREFLIGHT_CHECK: "true",
     API_REGION: region,
     API_URL: apiUrl.replace("https", "http"),
-    COGNITO_REGION: region,
+    APPLICATION_ENDPOINT: "http://localhost:3000/",
     COGNITO_IDENTITY_POOL_ID: process.env.COGNITO_IDENTITY_POOL_ID,
-    COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
-    COGNITO_USER_POOL_CLIENT_ID: process.env.COGNITO_USER_POOL_CLIENT_ID,
+    COGNITO_IDP_NAME: "Okta",
+    COGNITO_REDIRECT_SIGNIN: "http://localhost:3000/",
+    COGNITO_REDIRECT_SIGNOUT: "http://localhost:3000/postLogout",
+    COGNITO_REGION: region,
     COGNITO_USER_POOL_CLIENT_DOMAIN:
       process.env.COGNITO_USER_POOL_CLIENT_DOMAIN,
-    COGNITO_REDIRECT_SIGNIN: "http://localhost:3000/",
-    COGNITO_REDIRECT_SIGNOUT: "http://localhost:3000/",
+    COGNITO_USER_POOL_CLIENT_ID: process.env.COGNITO_USER_POOL_CLIENT_ID,
+    COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
+    LD_PROJECT_KEY: "mdct-mcr",
+    LOCAL_LOGIN: "false",
+    POST_SIGNOUT_REDIRECT: "http://localhost:3000/",
+    REACT_APP_LD_SDK_CLIENT: process.env.REACT_APP_LD_SDK_CLIENT,
+    SKIP_PREFLIGHT_CHECK: "true",
+    STAGE: "local",
   };
 
   await fs.rm(configFilePath, { force: true });
