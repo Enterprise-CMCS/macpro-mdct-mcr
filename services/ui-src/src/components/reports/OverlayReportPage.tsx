936878386c5a393ee2bb9af91e4b1acd62e9ca75
Garrett Rabian <garrett.rabian@coforma.io>
2025-07-16 14:02:29 -0400

    understanding feedback correctly

--- services/ui-src/src/components/reports/OverlayReportPage.tsx
+++ services/ui-src/src/components/reports/OverlayReportPage.tsx
@@ -285,7 +285,7 @@ const sx = {
     th: {
       paddingLeft: "1rem",
       paddingRight: "0",
-      borderColor: "palette.gray_lighter",
+      borderBottom: "1px solid var(--mdct-colors-palette-gray_lighter)",
       color: "palette.gray_medium",
       fontSize: "lg",
       fontWeight: "bold",
