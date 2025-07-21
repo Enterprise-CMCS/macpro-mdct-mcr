7bc13330053adf7fbbc924bc88f933a4ad00e3dd
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-07-21 14:47:11 -0400

    Update ui.ts

--- deployment/stacks/ui.ts
+++ deployment/stacks/ui.ts
@@ -87,7 +87,7 @@ export function createUiComponents(props: CreateUiComponentsProps) {
 
   const securityHeadersPolicy = new cloudfront.ResponseHeadersPolicy(
     scope,
-    "CloudFormationHeadersPolicy",
+    "CloudFrontHeadersPolicy",
     {
       responseHeadersPolicyName: `Headers-Policy-${stage}`,
       comment: "Add Security Headers",
