b64c892a3a8292e6fd9628e7f7f6ae8d2bec7cfb
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2025-07-25 09:52:53 -0400

    simplify and fix time comparison (#12329)

--- services/database/scripts/sync-kafka-2025.js
+++ services/database/scripts/sync-kafka-2025.js
@@ -6,33 +6,24 @@
  *   branchPrefix="YOUR BRANCH NAME" node services/database/scripts/sync-kafka-2025.js
  */
 
-const { buildDynamoClient, scan, update } = require("./utils/dynamodb.js");
 const { buildS3Client, list, putObjectTag } = require("./utils/s3.js");
 
 const isLocal = !!process.env.DYNAMODB_URL;
 const branch = isLocal ? "localstack" : process.env.branchPrefix;
-
-const mcparTableName = `${branch}-mcpar-reports`;
-const mlrTableName = `${branch}-mlr-reports`;
-const tables = [mcparTableName, mlrTableName];
 
 const mcparBucketName = `database-${branch}-mcpar`;
 const mlrBucketName = `database-${branch}-mlr`;
 const naaarBucketName = `database-${branch}-naaar`;
 const buckets = [mcparBucketName, mlrBucketName, naaarBucketName];
 
-// Maintaining consistency with the `lastAltered` field in the DB
-const dbSyncTime = Date.now();
 // Using a human readable format for easier debugging in the future
-const s3SyncTime = new Date().toISOString();
+const s3SyncTime = Date.now();
 // The time to compare against for filtering items
-const compareToTime = new Date("20 July 2025 00:00").toISOString();
+const compareToTime = new Date("20 July 2025 00:00").getTime();
 
 async function handler() {
   try {
     console.log("Searching for July 2025 modifications");
-    updateDbItems();
-    console.debug("Database data fix complete");
     updateS3Items();
     console.debug("S3 data fix complete");
 
@@ -47,37 +38,6 @@ async function handler() {
       body: err.message,
     };
   }
-}
-
-async function updateDbItems() {
-  buildDynamoClient();
-
-  for (const tableName of tables) {
-    console.log(`Processing table ${tableName}`);
-    const existingItems = await scan({
-      TableName: tableName,
-    });
-    const filteredItems = filterData(existingItems);
-    const transformedItems = await transform(filteredItems);
-    await update(tableName, transformedItems);
-    console.log(`Touched ${transformedItems.length} in table ${tableName}`);
-  }
-}
-
-function filterData(items) {
-  return items.filter(
-    (item) => new Date(item.lastAltered).toISOString() > compareToTime
-  );
-}
-
-async function transform(items) {
-  // Touch sync field only
-  const transformed = items.map((item) => {
-    const corrected = { ...item, ...{ lastSynced: dbSyncTime } };
-    return corrected;
-  });
-
-  return transformed;
 }
 
 async function updateS3Items() {
@@ -98,7 +58,7 @@ async function updateS3Items() {
 
 function filterS3Objects(bucketObjects) {
   return bucketObjects.filter(
-    (obj) => new Date(obj.LastModified).toISOString() > compareToTime
+    (obj) => new Date(obj.LastModified).getTime() > compareToTime
   );
 }
 
