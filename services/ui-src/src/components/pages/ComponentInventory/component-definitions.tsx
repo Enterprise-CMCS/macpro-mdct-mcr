98cdcd7c43e36909ed3e667d867807fcd99c16a9
Jess Eldredge <jess.eldredge@coforma.io>
2025-08-06 10:23:37 -0400

    Generate components for component inventory

---
+++ services/ui-src/src/components/pages/ComponentInventory/component-definitions.tsx
@@ -0,0 +1,67 @@
+import { Button } from "@chakra-ui/react";
+import { Alert } from "components";
+import { ReactNode } from "react";
+import { AlertTypes } from "types";
+
+type VariantDisplay = "inline" | "block";
+
+export enum ComponentTypes {
+  Button = "Button",
+  Alert = "Alert",
+}
+
+export const componentDefinitions: {
+  type: ComponentTypes;
+  description: string;
+  variants: ReactNode[];
+  display?: VariantDisplay;
+}[] = [
+  {
+    type: ComponentTypes.Button,
+    description: "The Button component is used to trigger an action or event.",
+    variants: [
+      <Button key="primary" variant="primary">
+        Primary
+      </Button>,
+      <Button key="transparent" variant="transparent">
+        Transparent
+      </Button>,
+      <Button key="outline" variant="outline">
+        Outline
+      </Button>,
+      <Button key="link" variant="link">
+        Link
+      </Button>,
+    ],
+    display: "inline",
+  },
+  {
+    type: ComponentTypes.Alert,
+    description:
+      "The Alert component is used to display important messages to the user.",
+    variants: [
+      <Alert
+        key="info"
+        status={AlertTypes.INFO}
+        description="This is an info alert."
+      />,
+      <Alert
+        key="warning"
+        status={AlertTypes.WARNING}
+        description="This is a warning alert."
+      />,
+      <Alert
+        key="error"
+        status={AlertTypes.ERROR}
+        description="This is an error alert."
+      />,
+      <Alert
+        key="error"
+        status={AlertTypes.ERROR}
+        title="This is the alert title"
+        description="This is the alert description"
+      />,
+    ],
+    display: "block",
+  },
+];
