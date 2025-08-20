f948c4dea80da254284aa5d7c263b3a79c049faf
Garrett Rabian <garrett.rabian@coforma.io>
2025-08-20 11:52:57 -0400

    add button theme file

---
+++ services/ui-src/src/styles/components/button.ts
@@ -0,0 +1,152 @@
+import { ComponentStyleConfig } from "@chakra-ui/react";
+import { svgFilters } from "styles/theme";
+
+const baseStyles = {
+  transition: "all 0.3s ease",
+  borderRadius: "0.25rem",
+  px: "1.5em",
+  py: "0.5em",
+  fontWeight: "bold",
+  width: "fit-content",
+  "&:disabled, &:disabled:hover": {
+    color: "gray",
+    backgroundColor: "gray_lighter",
+    opacity: "1",
+  },
+  ".mobile &": {
+    fontSize: "sm",
+  },
+};
+
+const solidVariant = {
+  backgroundColor: "primary",
+  color: "white",
+  "&:hover": {
+    backgroundColor: "primary_darker",
+  },
+  "&:disabled, &:disabled:hover": {
+    color: "gray",
+    backgroundColor: "gray_lighter",
+    opacity: 1,
+  },
+};
+
+const solidInverseVariant = {
+  backgroundColor: "white",
+  color: "primary",
+  "&:disabled, &:disabled:hover": {
+    color: "base",
+    backgroundColor: "gray_darker",
+    opacity: 1,
+  },
+};
+
+const outlineVariant = {
+  backgroundColor: "transparent",
+  border: "1px solid",
+  borderColor: "primary",
+  color: "primary",
+  textDecoration: "none",
+  "&:hover": {
+    backgroundColor: "gray_lightest",
+    color: "primary_darker",
+    span: {
+      filter: svgFilters.primary_darker,
+    },
+  },
+  "&:disabled, &:disabled:hover": {
+    color: "gray_lighter",
+    borderColor: "gray_lighter",
+    opacity: 1,
+  },
+};
+
+const outlineInverseVariant = {
+  backgroundColor: "transparent",
+  border: "1px solid",
+  borderColor: "white",
+  color: "white",
+  textDecoration: "none",
+  "&:disabled, &:disabled:hover": {
+    color: "gray_dark",
+    borderColor: "gray_dark",
+    backgroundColor: "transparent",
+    opacity: 1,
+  },
+};
+
+const ghostVariant = {
+  backgroundColor: "transparent",
+  color: "primary",
+  textDecoration: "underline",
+  padding: "0",
+  "&:hover": {
+    color: "primary_darker",
+    span: {
+      filter: svgFilters.primary_darker,
+    },
+  },
+  "&:visited, &:visited:hover": {
+    color: "primary",
+  },
+  "&:disabled, &:disabled:hover": {
+    color: "gray_light",
+  },
+};
+
+const ghostInverseVariant = {
+  backgroundColor: "transparent",
+  color: "white",
+  textDecoration: "underline",
+  padding: "0",
+  "&:disabled, &:disabled:hover": {
+    color: "gray_dark",
+  },
+};
+
+const fixedVariant = {
+  width: "5.5rem",
+  height: "2.25rem",
+};
+
+const variants = {
+  solid: solidVariant,
+  solidInverse: solidInverseVariant,
+  outline: outlineVariant,
+  outlineInverse: outlineInverseVariant,
+  ghost: ghostVariant,
+  ghostInverse: ghostInverseVariant,
+  fixed: fixedVariant,
+};
+
+const sizes = {
+  sm: {
+    fontSize: "sm",
+    fontWeight: "400",
+    px: "0.5em",
+    py: "0.25em",
+  },
+  md: {
+    fontSize: "md",
+    fontWeight: "700",
+    px: "1.5em",
+    py: "0.5em",
+  },
+  lg: {
+    fontSize: "lg",
+    px: "1.5em",
+    py: "1em",
+  },
+};
+
+const buttonTheme: ComponentStyleConfig = {
+  baseStyle: baseStyles,
+  sizes: sizes,
+  variants: variants,
+  defaultProps: {
+    variant: "primary",
+    size: "md",
+  },
+};
+
+export default buttonTheme;
