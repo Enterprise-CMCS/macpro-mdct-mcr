551bb8c0fe3daf4ea9dd5ca60752273dc1e7c1d2
Pete Dunlap <pete.dunlap@stratiform.digital>
2025-10-02 11:20:07 -0400

    CMDCT-5242 trimming down isDev branch names (#12442)

--- deployment/deployment-config.ts
+++ deployment/deployment-config.ts
@@ -24,9 +24,7 @@ export interface DeploymentConfigProperties {
 
 export const determineDeploymentConfig = async (stage: string) => {
   const project = process.env.PROJECT!;
-  const isDev =
-    isLocalStack ||
-    !["main", "master", "val", "prod", "production"].includes(stage);
+  const isDev = isLocalStack || !["main", "val", "production"].includes(stage);
   const secretConfigOptions = {
     ...(await loadDefaultSecret(project, stage)),
     ...(await loadStageSecret(project, stage)),
