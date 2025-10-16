39295bedc5088224c069c58b977034aa76307487
Source Files <action@github.com>
2025-10-16 21:07:25 +0000

    sync: update source files

--- deployment/prerequisites.ts
+++ deployment/prerequisites.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+// This file is managed by macpro-mdct-core so if you'd like to change it let's do it there
 import "source-map-support/register";
 import {
   App,
@@ -76,7 +77,7 @@ export class PrerequisiteStack extends Stack {
             "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
           },
           StringLike: {
-            "token.actions.githubusercontent.com:sub": `repo:Enterprise-CMCS/macpro-mdct-mcr:${branchFilter}`,
+            "token.actions.githubusercontent.com:sub": `repo:Enterprise-CMCS/macpro-mdct-${project}:${branchFilter}`,
           },
         },
         "sts:AssumeRoleWithWebIdentity"
@@ -115,10 +116,15 @@ async function main() {
     }),
   });
 
-  Tags.of(app).add("PROJECT", "MCR");
+  if (!process.env.PROJECT) {
+    throw new Error("PROJECT enironment variable is required but not set");
+  }
 
   const project = process.env.PROJECT!;
-  new PrerequisiteStack(app, "mcr-prerequisites", {
+
+  Tags.of(app).add("PROJECT", project.toUpperCase());
+
+  new PrerequisiteStack(app, `${project}-prerequisites`, {
     project,
     ...(await loadDefaultSecret(project)),
     env: {
