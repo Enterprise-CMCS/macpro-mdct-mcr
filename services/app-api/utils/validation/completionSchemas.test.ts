d841295c459e230674de2f894d5f39dd85f74030
Garrett Rabian <garrett.rabian@coforma.io>
2025-10-27 09:35:33 -0400

    empty array is valid radioOptional

--- services/app-api/utils/validation/completionSchemas.test.ts
+++ services/app-api/utils/validation/completionSchemas.test.ts
@@ -470,7 +470,7 @@ describe("Completion schemas", () => {
 
   test.each([
     { value: undefined, description: "undefined", expected: true },
-    // { value: [], description: "empty array", expected: false },
+    { value: [], description: "empty array", expected: true },
     { value: [choice], description: "one selection", expected: true },
     // { value: [choice, choice], description: "two selections", expected: false },
   ])("radioOptional $description -> $expected", ({ value, expected }) => {
