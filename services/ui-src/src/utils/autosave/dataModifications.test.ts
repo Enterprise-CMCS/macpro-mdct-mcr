0d9e055c27071ddd070a953c8fdcacd32b02af57
Bangbay Siboliban <bangbay@bluetiger.digital>
2025-11-10 10:05:16 -0800

    Update tests

--- services/ui-src/src/utils/autosave/dataModifications.test.ts
+++ services/ui-src/src/utils/autosave/dataModifications.test.ts
@@ -1,34 +1,186 @@
 import {
+  dataModifications,
   handlePriorAuthorization,
   updatePlanNames,
   updatePlansInAnalysisMethods,
   updatePlansInExemptions,
 } from "./dataModifications";
 // types
+import { ReportType } from "types";
 import { FieldDataTuple } from "./autosave";
 
-describe("utils/autosave/dataModifications", () => {
-  describe("handlePriorAuthorization()", () => {
-    it("allows prior authorization fields", () => {
-      const dataToWrite = {
-        id: "mock-id",
-        fieldData: {
-          plans: [
+const testCases = {
+  dataToWrite: {
+    fieldData: {
+      plans: [
+        {
+          id: "mock-plan-1",
+          name: "Mock Plan 1",
+        },
+      ],
+    },
+  },
+  updatePlansInAnalysisMethods: {
+    reportFieldData: {
+      analysisMethods: [
+        {
+          analysis_applicable: [
             {
-              id: "mock-plan-1",
-              name: "Mock Plan 1",
+              key: "analysis_applicable-mock",
+              value: "Yes",
             },
           ],
+          analysis_method_applicable_plans: [
+            {
+              key: "analysis_method_applicable_plans-mock-plan-1",
+              value: "Applicable Mock Plan 1",
+            },
+            {
+              key: "analysis_method_applicable_plans-mock-plan-2",
+              value: "Applicable Mock Plan 2",
+            },
+          ],
+          analysis_method_frequency: [
+            {
+              key: "analysis_method_frequency-mock",
+              value: "Weekly",
+            },
+          ],
+          isRequired: true,
         },
-      };
-      const reportFieldData = {
+      ],
+    },
+    expectedResult: {
+      fieldData: {
+        analysisMethods: [
+          {
+            analysis_applicable: [
+              {
+                key: "analysis_applicable-mock",
+                value: "Yes",
+              },
+            ],
+            analysis_method_applicable_plans: [
+              {
+                key: "analysis_method_applicable_plans-mock-plan-1",
+                value: "Mock Plan 1",
+              },
+            ],
+            analysis_method_frequency: [
+              {
+                key: "analysis_method_frequency-mock",
+                value: "Weekly",
+              },
+            ],
+            isRequired: true,
+          },
+        ],
         plans: [
           {
             id: "mock-plan-1",
             name: "Mock Plan 1",
           },
         ],
+      },
+    },
+  },
+  updatePlansInExemptions: {
+    reportFieldData: {
+      plansExemptFromQualityMeasures: [
+        {
+          key: "plansExemptFromQualityMeasures-mock-plan-1",
+          value: "Exempt Mock Plan 1",
+        },
+        {
+          key: "plansExemptFromQualityMeasures-mock-plan-2",
+          value: "Exempt Mock Plan 2",
+        },
+      ],
+    },
+    expectedResult: {
+      fieldData: {
+        plans: [
+          {
+            id: "mock-plan-1",
+            name: "Mock Plan 1",
+          },
+        ],
+        plansExemptFromQualityMeasures: [
+          {
+            key: "plansExemptFromQualityMeasures-mock-plan-1",
+            value: "Mock Plan 1",
+          },
+        ],
+      },
+    },
+  },
+};
+
+describe("utils/autosave/dataModifications", () => {
+  describe("dataModifications()", () => {
+    it("updates MCPAR", () => {
+      const input = dataModifications(
+        ReportType.MCPAR,
+        testCases.dataToWrite,
+        testCases.updatePlansInExemptions.reportFieldData
+      );
+
+      expect(input).toEqual(testCases.updatePlansInExemptions.expectedResult);
+    });
+
+    it("updates MLR", () => {
+      const reportFieldData = {};
+
+      const input = dataModifications(
+        ReportType.MLR,
+        testCases.dataToWrite,
+        reportFieldData
+      );
+
+      const expectedResult = {
+        ...testCases.dataToWrite,
       };
+
+      expect(input).toEqual(expectedResult);
+    });
+
+    it("updates NAAAR", () => {
+      const input = dataModifications(
+        ReportType.NAAAR,
+        testCases.dataToWrite,
+        testCases.updatePlansInAnalysisMethods.reportFieldData
+      );
+
+      expect(input).toEqual(
+        testCases.updatePlansInAnalysisMethods.expectedResult
+      );
+    });
+  });
+
+  describe("handlePriorAuthorization()", () => {
+    const dataToWrite = {
+      id: "mock-id",
+      fieldData: {
+        plans: [
+          {
+            id: "mock-plan-1",
+            name: "Mock Plan 1",
+            plan_totalNumberOfStandardPARequests: 1,
+          },
+        ],
+      },
+    };
+    const reportFieldData = {
+      plans: [
+        {
+          id: "mock-plan-1",
+          name: "Mock Plan 1",
+          plan_totalNumberOfStandardPARequests: 1,
+        },
+      ],
+    };
+
+    it("allows prior authorization fields", () => {
       const fieldsToSave = [
         [
           "reportingDataPriorToJune2026",
@@ -38,8 +190,18 @@ describe("utils/autosave/dataModifications", () => {
               value: "Yes",
             },
           ],
-        ] as FieldDataTuple,
-      ];
+        ],
+        [
+          "plans",
+          [
+            {
+              id: "mock-plan-1",
+              name: "Mock Plan 1",
+              plan_totalNumberOfStandardPARequests: 1,
+            },
+          ],
+        ],
+      ] as FieldDataTuple[];
       const input = handlePriorAuthorization(
         dataToWrite,
         reportFieldData,
@@ -48,6 +210,13 @@ describe("utils/autosave/dataModifications", () => {
       const expectedResult = {
         id: "mock-id",
         fieldData: {
+          plans: [
+            {
+              id: "mock-plan-1",
+              name: "Mock Plan 1",
+              plan_totalNumberOfStandardPARequests: 1,
+            },
+          ],
           reportingDataPriorToJune2026: [
             {
               key: "mock-choice-yes",
@@ -61,38 +230,27 @@ describe("utils/autosave/dataModifications", () => {
     });
 
     it("removes prior authorization fields", () => {
-      const dataToWrite = {
-        id: "mock-id",
-        fieldData: {
-          plans: [
+      const fieldsToSave = [
+        [
+          "reportingDataPriorToJune2026",
+          [
+            {
+              key: "mock-choice-no",
+              value: "No",
+            },
+          ],
+        ],
+        [
+          "plans",
+          [
             {
               id: "mock-plan-1",
               name: "Mock Plan 1",
               plan_totalNumberOfStandardPARequests: 1,
             },
           ],
-        },
-      };
-      const reportFieldData = {
-        plans: [
-          {
-            id: "mock-plan-1",
-            name: "Mock Plan 1",
-            plan_totalNumberOfStandardPARequests: 1,
-          },
         ],
-      };
-      const fieldsToSave = [
-        [
-          "reportingDataPriorToJune2026",
-          [
-            {
-              key: "mock-choice-yes",
-              value: "No",
-            },
-          ],
-        ] as FieldDataTuple,
-      ];
+      ] as FieldDataTuple[];
       const input = handlePriorAuthorization(
         dataToWrite,
         reportFieldData,
@@ -109,7 +267,7 @@ describe("utils/autosave/dataModifications", () => {
           ],
           reportingDataPriorToJune2026: [
             {
-              key: "mock-choice-yes",
+              key: "mock-choice-no",
               value: "No",
             },
           ],
@@ -122,81 +280,27 @@ describe("utils/autosave/dataModifications", () => {
 
   describe("updatePlansInAnalysisMethods()", () => {
     it("updates plans in analysis_method_applicable_plans", () => {
-      const dataToWrite = {
-        fieldData: {
-          plans: [
-            {
-              id: "mock-plan-1",
-              name: "Mock Plan 1",
-            },
-          ],
-        },
-      };
+      const input = updatePlansInAnalysisMethods(
+        testCases.dataToWrite,
+        testCases.updatePlansInAnalysisMethods.reportFieldData
+      );
+      expect(input).toEqual(
+        testCases.updatePlansInAnalysisMethods.expectedResult
+      );
+    });
 
+    it("does not modify dataToWrite", () => {
       const reportFieldData = {
-        analysisMethods: [
-          {
-            analysis_applicable: [
-              {
-                key: "analysis_applicable-mock",
-                value: "Yes",
-              },
-            ],
-            analysis_method_applicable_plans: [
-              {
-                key: "analysis_method_applicable_plans-mock-plan-1",
-                value: "Applicable Mock Plan 1",
-              },
-              {
-                key: "analysis_method_applicable_plans-mock-plan-2",
-                value: "Applicable Mock Plan 2",
-              },
-            ],
-            analysis_method_frequency: [
-              {
-                key: "analysis_method_frequency-mock",
-                value: "Weekly",
-              },
-            ],
-            isRequired: true,
-          },
-        ],
+        analysisMethods: [],
       };
 
-      const input = updatePlansInAnalysisMethods(dataToWrite, reportFieldData);
+      const input = updatePlansInAnalysisMethods(
+        testCases.dataToWrite,
+        reportFieldData
+      );
 
       const expectedResult = {
-        fieldData: {
-          analysisMethods: [
-            {
-              analysis_applicable: [
-                {
-                  key: "analysis_applicable-mock",
-                  value: "Yes",
-                },
-              ],
-              analysis_method_applicable_plans: [
-                {
-                  key: "analysis_method_applicable_plans-mock-plan-1",
-                  value: "Mock Plan 1",
-                },
-              ],
-              analysis_method_frequency: [
-                {
-                  key: "analysis_method_frequency-mock",
-                  value: "Weekly",
-                },
-              ],
-              isRequired: true,
-            },
-          ],
-          plans: [
-            {
-              id: "mock-plan-1",
-              name: "Mock Plan 1",
-            },
-          ],
-        },
+        ...testCases.dataToWrite,
       };
 
       expect(input).toEqual(expectedResult);
@@ -205,47 +309,11 @@ describe("utils/autosave/dataModifications", () => {
 
   describe("updatePlansInExemptions()", () => {
     it("updates plans in plansExemptFromQualityMeasures", () => {
-      const dataToWrite = {
-        fieldData: {
-          plans: [
-            {
-              id: "mock-plan-1",
-              name: "Mock Plan 1",
-            },
-          ],
-        },
-      };
-      const reportFieldData = {
-        plansExemptFromQualityMeasures: [
-          {
-            key: "plansExemptFromQualityMeasures-mock-plan-1",
-            value: "Exempt Mock Plan 1",
-          },
-          {
-            key: "plansExemptFromQualityMeasures-mock-plan-2",
-            value: "Exempt Mock Plan 2",
-          },
-        ],
-      };
-      const input = updatePlansInExemptions(dataToWrite, reportFieldData);
-      const expectedResult = {
-        fieldData: {
-          plans: [
-            {
-              id: "mock-plan-1",
-              name: "Mock Plan 1",
-            },
-          ],
-          plansExemptFromQualityMeasures: [
-            {
-              key: "plansExemptFromQualityMeasures-mock-plan-1",
-              value: "Mock Plan 1",
-            },
-          ],
-        },
-      };
-
-      expect(input).toEqual(expectedResult);
+      const input = updatePlansInExemptions(
+        testCases.dataToWrite,
+        testCases.updatePlansInExemptions.reportFieldData
+      );
+      expect(input).toEqual(testCases.updatePlansInExemptions.expectedResult);
     });
   });
 
