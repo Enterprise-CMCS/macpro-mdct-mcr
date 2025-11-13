d1ff4a2d53d350f139f50852dbf9d9ac77f347c6
karla-vm <karla@coforma.io>
2025-11-13 15:20:58 -0500

    add verbiage

---
+++ services/ui-src/src/verbiage/pages/mcpar/mcpar-quality-measures.ts
@@ -0,0 +1,7 @@
+export default {
+  tableHeaders: {
+    measureId: "Measure identification number or definition:",
+    dataVersion: "Data version:",
+    activities: "Activities the quality measure is used in:",
+  },
+};
