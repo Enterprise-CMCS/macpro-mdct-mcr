9fccdbbaf7d00904c182f0976b54cbdba5efb612
Garrett Rabian <garrett.rabian@coforma.io>
2025-11-17 11:37:42 -0500

    clean up form section content components

--- services/ui-src/src/components/forms/FormLayoutElements.test.tsx
+++ services/ui-src/src/components/forms/FormLayoutElements.test.tsx
@@ -1,72 +1,32 @@
-import { render, screen } from "@testing-library/react";
-import { testA11yAct } from "utils/testing/commonTests";
+import { render } from "@testing-library/react";
 // components
 import { SectionContent, SectionHeader } from "./FormLayoutElements";
+// utils
+import { testA11yAct } from "utils/testing/commonTests";
 
-const sectionHeaderComponentTopDivider = (
+const sectionHeaderComponent = (divider: "top" | "bottom" | "none") => (
   <SectionHeader
-    divider="top"
-    name="sectionHeader"
+    divider={divider}
     content="1. Section Header"
-    data-testid="test-section-header"
-  />
-);
-
-const sectionHeaderComponentBottomDivider = (
-  <SectionHeader
-    divider="bottom"
-    name="sectionHeader"
-    content="1. Section Header"
-    data-testid="test-section-header"
-  />
-);
-
-const sectionHeaderComponentNoDivider = (
-  <SectionHeader
-    divider="none"
-    name="sectionHeader"
-    content="1. Section Header"
-    data-testid="test-section-header"
+    hint="More information"
   />
 );
 
 const sectionContentComponent = <SectionContent content={"Foo"} />;
 
 describe("<SectionHeader />", () => {
-  test("Top should make the section divider on the top.", () => {
-    const { getByText } = render(sectionHeaderComponentTopDivider);
-    const sectionHeader = screen.getByTestId("test-section-header");
-
-    expect(sectionHeader).toBeVisible();
-    expect(getByText("1. Section Header")).toBeVisible();
+  test("Section header renders header and hint text", () => {
+    const { getByRole, getByText } = render(sectionHeaderComponent("none"));
+    expect(getByRole("heading", { name: "1. Section Header" })).toBeVisible();
+    expect(getByText("More information")).toBeVisible();
   });
 
-  test("Bottom should make the section divider on the bottom", () => {
-    const { getByText } = render(sectionHeaderComponentBottomDivider);
-    const sectionHeader = screen.getByTestId("test-section-header");
-
-    expect(sectionHeader).toBeVisible();
-    expect(getByText("1. Section Header")).toBeVisible();
-  });
-
-  test("None should make the section divider not there.", () => {
-    const { getByText, queryByRole } = render(sectionHeaderComponentNoDivider);
-    const sectionHeader = screen.getByTestId("test-section-header");
-
-    expect(sectionHeader).toBeVisible();
-    expect(getByText("1. Section Header")).toBeVisible();
-    expect(queryByRole("separator")).not.toBeInTheDocument();
-  });
-
-  testA11yAct(sectionHeaderComponentBottomDivider);
-  testA11yAct(sectionHeaderComponentTopDivider);
-  testA11yAct(sectionHeaderComponentNoDivider);
+  testA11yAct(sectionHeaderComponent("none"));
 });
 
 describe("<SectionContent />", () => {
   test("Component should be visible and render correct text.", () => {
     const { getByText } = render(sectionContentComponent);
-    expect(getByText("Foo")).toBeVisible();
     expect(getByText("Foo")).toBeVisible();
   });
 
