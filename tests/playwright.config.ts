6d01d63c07721b2d78c40ab3a4182e18a4a1a1b3
Marcus Myers <marcus.myers@coforma.io>
2025-11-24 16:36:26 -0500

    updated banner tests (#12526)

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -13,6 +13,12 @@ dotenv.config({ path: "../.env" });
 export default defineConfig({
   testDir: "playwright",
   testMatch: ["**/*.spec.js", "**/*.spec.ts"],
+  /* Timeout for each test (including beforeEach and such). 60000ms = 1 minute */
+  timeout: 60000,
+  expect: {
+    /* Timeout for each `expect()` call. 15000ms = 15 seconds */
+    timeout: 15000,
+  },
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -27,11 +33,14 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL || "http://localhost:3000",
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    /* Collect trace when a test fails. See https://playwright.dev/docs/trace-viewer */
+    trace: "retain-on-failure",
 
     /* Video recording configuration */
     video: "retain-on-failure",
+
+    /* Screenshot configuration */
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
