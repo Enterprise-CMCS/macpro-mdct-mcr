40e0bf22660aa6d6e1a12df7af7a1d1ba1d121b1
Bangbay Siboliban <bangbay@bluetiger.digital>
2025-11-26 10:40:19 -0800

    [CMDCT-5290] Add measure definition (#12527)

--- services/ui-src/src/verbiage/pages/mcpar/mcpar-quality-measures.ts
+++ services/ui-src/src/verbiage/pages/mcpar/mcpar-quality-measures.ts
@@ -1,6 +1,6 @@
 export default {
   tableHeaders: {
-    measure_identifierCmit: "Measure identification number or definition:",
+    measure_identifier: "Measure identification number or definition:",
     measure_dataVersion: "Data version:",
     measure_activities: "Activities the quality measure is used in:",
   },
