30435ac2378384744094f22031acbe321ab8ec96
Garrett Rabian <garrett.rabian@coforma.io>
2025-12-09 09:01:44 -0500

    enhance qm util tests

--- services/ui-src/src/utils/forms/qualityMeasures.test.ts
+++ services/ui-src/src/utils/forms/qualityMeasures.test.ts
@@ -1,4 +1,4 @@
-import { addRatesToForm } from "./qualityMeasures";
+import { addRatesToForm, createRateField } from "./qualityMeasures";
 // utils
 import { mockDrawerForm } from "utils/testing/setupJest";
 
@@ -13,10 +13,32 @@ const mockMeasure = {
   ],
 };
 
-describe("addRatesToForm()", () => {
-  test("adds rate field", () => {
-    expect(mockDrawerForm.fields.length).toBe(1);
-    const modifiedForm = addRatesToForm(mockDrawerForm, mockMeasure);
-    expect(modifiedForm.fields.length).toBe(2);
+describe("qualityMeasures utils", () => {
+  describe("addRatesToForm()", () => {
+    test("adds rate field", () => {
+      expect(mockDrawerForm.fields.length).toBe(1);
+      const modifiedForm = addRatesToForm(mockDrawerForm, mockMeasure);
+      expect(modifiedForm.fields.length).toBe(2);
+      expect(modifiedForm.fields[0]).toEqual(mockDrawerForm.fields[0]);
+      expect(modifiedForm.fields[1]).toEqual(
+        createRateField(
+          mockMeasure.measure_rates[0].id,
+          mockMeasure.measure_rates[0].name
+        )
+      );
+    });
+  });
+
+  describe("createRateField()", () => {
+    test("creates form field for quality measure rates", () => {
+      const rateField = createRateField(
+        mockMeasure.measure_rates[0].id,
+        mockMeasure.measure_rates[0].name
+      );
+      expect(rateField.id).toEqual("measure_rate_results-mock-rate-1");
+      expect(rateField.type).toEqual("number");
+      expect(rateField.validation).toEqual("numberOptional");
+      expect(rateField.props.label).toEqual("mock rate 1 results");
+    });
   });
 });
