dd4882bbb39df7b5551d770c0ae5f42ecfcc83af
Source Files <action@github.com>
2025-12-10 06:06:22 +0000

    sync: update source files

--- .github/create-pr.ts
+++ .github/create-pr.ts
@@ -1,7 +1,6 @@
 // This file is managed by macpro-mdct-core so if you'd like to change it let's do it there
 import { Octokit } from "@octokit/rest";
 import { createActionAuth } from "@octokit/auth-action";
-import { createPrBody } from "./commit-list.ts";
 
 const [owner, repo] = process.env.GITHUB_REPO!.split("/");
 const targetBranch = process.env.TARGET_BRANCH!;
@@ -45,8 +44,21 @@ async function run() {
     pull_number: pr.number,
     per_page: 100,
   });
+  const workDone = commits.map((c) => {
+    // remove anything after a new line and trailing PR link like (#12345)
+    return c.commit.message.split("\n")[0].replace(/\s*\(#\d+\)/, "");
+  });
 
-  const body = await createPrBody({ commits, octokit, owner, prTitle, repo });
+  const filteredWorkDone = workDone.filter(
+    // filter out release commits and merge commits
+    (msg) =>
+      !(msg.includes(appName) && msg.includes("release")) &&
+      !msg.startsWith("Merge branch")
+  );
+
+  let body = `## ${prLabel}\n\n`;
+  body += "### In this deployment:\n";
+  body += `- ${filteredWorkDone.reverse().join("\n- ")}`;
 
   octokit.rest.pulls.update({
     owner,
