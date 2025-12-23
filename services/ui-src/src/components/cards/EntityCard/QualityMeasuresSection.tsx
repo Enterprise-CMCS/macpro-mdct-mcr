5626bf8dd2dce1f2019c77dde074a7a02654ac81
karla-vm <karla@coforma.io>
2025-12-23 15:50:50 -0400

    separating code to new file

--- services/ui-src/src/components/cards/EntityCard/QualityMeasuresSection.tsx
+++ services/ui-src/src/components/cards/EntityCard/QualityMeasuresSection.tsx
@@ -2,7 +2,11 @@ import { ReactNode } from "react";
 import { Box, Flex, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
 import { useFlags } from "launchdarkly-react-client-sdk";
 // types
-import { AnyObject, SxObject } from "types";
+import { SxObject } from "types";
+import {
+  NewBottomQualityMeasuresSection,
+  NewTopQualityMeasuresSection,
+} from "./NewQualityMeasuresSection";
 
 const TopQualityMeasuresSection = ({
   formattedEntityData,
@@ -11,19 +15,10 @@ const TopQualityMeasuresSection = ({
   sx,
   newQualityMeasuresSectionEnabled,
 }: TopProps) => {
-  const getMeasureIdentifier = () => {
-    if (formattedEntityData.cmitNumber) {
-      return `CMIT: ${formattedEntityData.cmitNumber}`;
-    } else if (formattedEntityData.cbeNumber) {
-      return `CBE: ${formattedEntityData.cbeNumber}`;
-    }
-    return "No, it uses neither CMIT or CBE";
-  };
   return (
     <>
       {!newQualityMeasuresSectionEnabled ? (
         <>
-          {" "}
           <Heading as={isPDF ? "p" : "h4"} sx={sx.heading}>
             {`${printVersion ? "D2.VII.1 Measure Name: " : ""}${
               formattedEntityData.name
@@ -85,50 +80,11 @@ const TopQualityMeasuresSection = ({
         </>
       ) : (
         <>
-          <Heading as={isPDF ? "p" : "h4"} sx={sx.heading}>
-            {`${printVersion ? "D2.VII.2 Measure Name: " : ""}${
-              formattedEntityData.name
-            }`}
-          </Heading>
-          <Text sx={sx.subtitle}>
-            {`${
-              printVersion ? "D2.VII.3 " : ""
-            }Measure identification number or definition`}
-          </Text>
-          <Text sx={sx.subtext}>{getMeasureIdentifier()}</Text>
-          {formattedEntityData.description && (
-            <Box>
-              <Text sx={sx.subtitle}>
-                {`${
-                  printVersion ? "D2.VII.3c " : ""
-                }How is this measure defined?`}
-              </Text>
-              <Text sx={sx.subtext}>{formattedEntityData.description}</Text>
-              <Text sx={sx.subtitle}>
-                {`${
-                  printVersion ? "D2.VII.3d " : ""
-                }Link to the measure specification (optional)`}
-              </Text>
-              <Text sx={sx.subtext}>{formattedEntityData.identifierUrl}</Text>
-              <Text sx={sx.subtitle}>
-                {`${printVersion ? "D2.VII.3e " : ""}Measure domain`}
-              </Text>
-              <Text sx={sx.subtext}>
-                {formattedEntityData.identifierDomain}
-              </Text>
-            </Box>
-          )}
-          <Text sx={sx.subtitle}>
-            {`${printVersion ? "D2.VII.4 " : ""}Data version`}
-          </Text>
-          <Text sx={sx.subtext}>{formattedEntityData.dataVersion}</Text>
-
-          <Text sx={sx.subtitle}>
-            {`${
-              printVersion ? "D2.VII.5 " : ""
-            }Activities the quality measure is used in`}
-          </Text>
-          <Text sx={sx.subtext}>{formattedEntityData.activities}</Text>
+          <NewTopQualityMeasuresSection
+            formattedEntityData={formattedEntityData}
+            printVersion={printVersion}
+            sx={sx}
+          />
         </>
       )}
     </>
@@ -180,52 +136,11 @@ const BottomQualityMeasuresSection = ({
         </>
       ) : (
         <>
-          <Heading as={"p"} sx={sx.heading}>
-            D2.VII.7 Measure results
-          </Heading>
-          {formattedEntityData?.isPartiallyComplete && (
-            <Text sx={sx.missingResponseMessage}>
-              {verbiage?.entityMissingResponseMessage ||
-                (printVersion && notAnswered)}
-            </Text>
-          )}
-          {formattedEntityData?.measureResults?.map((result) => (
-            <Box
-              key={result.planName}
-              sx={sx.highlightContainer}
-              className={!result ? "error" : ""}
-            >
-              <Box sx={sx.highlightSection}>
-                <Text sx={sx.qualityMeasuresPlanName}>{result.planName}</Text>
-              </Box>
-              {result.notReporting ? (
-                <Box>
-                  <Text sx={sx.subtext}>
-                    Not reporting: {result.notReportingReason}
-                  </Text>
-                </Box>
-              ) : (
-                <Box>
-                  <Text sx={sx.subtitle}>D2.VII.8 Data collection method</Text>
-                  {!result.dataCollectionMethod ? (
-                    <Text sx={sx.notAnswered}>Not answered</Text>
-                  ) : (
-                    <Text sx={sx.subtext}>{result.dataCollectionMethod}</Text>
-                  )}
-                  {result.rateResults.map((rate: AnyObject) => (
-                    <Box key={rate.rate}>
-                      <Text sx={sx.subtitle}>{rate.rate}</Text>
-                      {!rate.rateResult ? (
-                        <Text sx={sx.notAnswered}>Not answered</Text>
-                      ) : (
-                        <Text sx={sx.subtext}>{rate.rateResult}</Text>
-                      )}
-                    </Box>
-                  ))}
-                </Box>
-              )}
-            </Box>
-          ))}
+          <NewBottomQualityMeasuresSection
+            formattedEntityData={formattedEntityData}
+            printVersion={printVersion}
+            sx={sx}
+          />
         </>
       )}
     </>
@@ -284,17 +199,6 @@ interface FormattedEntityData {
   nqfNumber?: string;
   reportingRateType?: string;
   reportingPeriod?: string;
-  // new Quality Measures entity data
-  id?: string;
-  identifierType?: string;
-  cmitNumber?: string;
-  cbeNumber?: string;
-  measureIdentifier?: string;
-  identifierDomain?: string;
-  identifierUrl?: string;
-  dataVersion?: string;
-  activities?: string;
-  measureResults?: AnyObject[];
 }
 interface BaseProps {
   formattedEntityData: FormattedEntityData;
