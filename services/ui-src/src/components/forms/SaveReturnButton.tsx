fad5cd433eecf02d7623ee0108778d4cb31963ce
Pete Dunlap <pete.dunlap@stratiform.digital>
2026-01-16 09:07:36 -0500

    CMDCT-5470 switch to oxcfmt from prettier (#12617)
    Co-authored-by: Karla Valcárcel Martínez <99458559+karla-vm@users.noreply.github.com>

--- services/ui-src/src/components/forms/SaveReturnButton.tsx
+++ services/ui-src/src/components/forms/SaveReturnButton.tsx
@@ -21,8 +21,8 @@ export const SaveReturnButton = ({
     !userIsEndUser
       ? { onClick: disabledOnClick }
       : onClick
-      ? { onClick }
-      : { form: formId! };
+        ? { onClick }
+        : { form: formId! };
 
   const buttonText = !userIsEndUser ? "Return" : "Save & return";
   return (
