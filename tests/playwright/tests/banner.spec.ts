1c07f2b8257585295e22da06431d5d55b11e4b4e
Marcus Myers <marcus.myers@coforma.io>
2026-03-03 12:41:22 -0500

    updated missing package for regression tests (#12683)

--- tests/playwright/tests/banner.spec.ts
+++ tests/playwright/tests/banner.spec.ts
@@ -6,9 +6,10 @@ import { formatDate } from "../utils/date-helpers";
 test.describe("admin user banner page", () => {
   test.beforeEach(async ({ adminPage }) => {
     await deleteAllBanners();
+    const getBannerResponse = adminPage.waitForResponse("/banners", "GET", 200);
     await adminPage.page.goto("/admin");
     await adminPage.checkAndReauthenticate();
-    await adminPage.waitForResponse("/banners", "GET", 200);
+    await getBannerResponse;
   });
 
   test("Should see the correct banner page as an admin user", async ({
