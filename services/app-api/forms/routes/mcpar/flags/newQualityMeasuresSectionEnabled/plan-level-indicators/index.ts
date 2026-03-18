503216fa0b0279ee3ca21845a9a8ee227ef0339b
karla-vm <karla@coforma.io>
2026-03-18 16:07:04 -0400

    revert financial performance route removal

--- services/app-api/forms/routes/mcpar/flags/newQualityMeasuresSectionEnabled/plan-level-indicators/index.ts
+++ services/app-api/forms/routes/mcpar/flags/newQualityMeasuresSectionEnabled/plan-level-indicators/index.ts
@@ -1,6 +1,7 @@
 import { ParentRoute } from "../../../../../../utils/types";
 import { appealsStateFairHearingsAndGrievancesRoute } from "../../../plan-level-indicators/appeals-state-fair-hearings-and-grievances";
 import { encounterDataReportRoute } from "../../../plan-level-indicators/encounter-data-report";
+import { financialPerformanceRoute } from "../../../plan-level-indicators/financial-performance";
 import { ilosRoute } from "../../../plan-level-indicators/ilos";
 import { patientAccessApiRoute } from "../../../plan-level-indicators/patient-access-api";
 import { priorAuthorizationRoute } from "../../../plan-level-indicators/prior-authorization";
@@ -14,6 +15,7 @@ export const planLevelIndicatorsRoute: ParentRoute = {
   path: "/mcpar/plan-level-indicators",
   children: [
     programCharacteristicsRoute,
+    financialPerformanceRoute,
     encounterDataReportRoute,
     appealsStateFairHearingsAndGrievancesRoute,
     qualityMeasuresRoute,
