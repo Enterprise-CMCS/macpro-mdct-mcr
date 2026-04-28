6fbc8e005a414f79946e1eba1d4fff8b8c512eff
Pete Dunlap <pete.dunlap@stratiform.digital>
2026-04-28 09:38:52 -0400

    fixing PR

--- .github/setBranchName.ts
+++ .github/setBranchName.ts
@@ -3,15 +3,20 @@
 // .github/setBranchName.ts dependabot/npm_and_yarn/some-package-1.2.3
 import { createHash } from "node:crypto";
 
-const githubRefName = process.argv[2];
+export const setBranchName = (githubRefName: string) => {
+  if (githubRefName.startsWith("dependabot/")) {
+    const hash = createHash("sha256").update(githubRefName).digest("hex");
+    return "x" + hash.substring(0, 10);
+  } else if (githubRefName.startsWith("snyk-")) {
+    const parts = githubRefName.split("-");
+    const lastPart = parts.at(-1);
+    return "s" + lastPart.substring(0, 10);
+  } else {
+    return githubRefName;
+  }
+};
 
-if (githubRefName.startsWith("dependabot/")) {
-  const hash = createHash("sha256").update(githubRefName).digest("hex");
-  console.log("x" + hash.substring(0, 10));
-} else if (githubRefName.startsWith("snyk-")) {
-  const parts = githubRefName.split("-");
-  const lastPart = parts.at(-1);
-  console.log("s" + lastPart.substring(0, 10));
-} else {
-  console.log(githubRefName);
+if (process.argv[2]) {
+  const name = setBranchName(process.argv[2]);
+  console.log(name);
 }
