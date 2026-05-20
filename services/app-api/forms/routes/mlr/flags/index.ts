20323dc56b42c5b7b9d962e1b2082855cd265663
Karla M. Valcárcel Martínez <karla.valcarcel-martinez@coforma.io>
2026-05-20 15:41:34 -0400

    update comment

--- services/app-api/forms/routes/mlr/flags/index.ts
+++ services/app-api/forms/routes/mlr/flags/index.ts
@@ -1,4 +1,4 @@
 // flagged routes
 // commented out export is intentional to preserve the flag for future use when the feature is ready to be enabled
-// export { mcparReportJson as newQualityMeasuresSectionEnabled } from "./newQualityMeasuresSectionEnabled/index";
+// export { mlrReportJson as summer2026SansQm } from "./summer2026SansQm/index";
 export { mlrReportJson as summer2026SansQm } from "./summer2026SansQm/index";
