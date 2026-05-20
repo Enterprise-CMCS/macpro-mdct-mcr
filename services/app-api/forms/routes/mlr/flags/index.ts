071bba5a64b5d1d506f44b4ed3c50603cc235dfa
Karla M. Valcárcel Martínez <karla.valcarcel-martinez@coforma.io>
2026-05-20 15:41:53 -0400

    actually update the comment

--- services/app-api/forms/routes/mlr/flags/index.ts
+++ services/app-api/forms/routes/mlr/flags/index.ts
@@ -1,4 +1,2 @@
 // flagged routes
-// commented out export is intentional to preserve the flag for future use when the feature is ready to be enabled
-// export { mlrReportJson as summer2026SansQm } from "./summer2026SansQm/index";
 export { mlrReportJson as summer2026SansQm } from "./summer2026SansQm/index";
