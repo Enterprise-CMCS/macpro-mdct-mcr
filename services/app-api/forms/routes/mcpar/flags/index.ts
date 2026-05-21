eea5d2854f1fd38aa1517b171be5aa5b26368e33
Bangbay Siboliban <bangbay@bluetiger.digital>
2026-05-20 20:20:22 -0700

    Add note in flags index files and remove unused compiled JSON route files

--- services/app-api/forms/routes/mcpar/flags/index.ts
+++ services/app-api/forms/routes/mcpar/flags/index.ts
@@ -1,4 +1,13 @@
-// flagged routes
+/** *
+ * If there are feature flags, export the report JSON as the flag name,
+ * matching how it is named in LaunchDarkly.
+ *
+ * Example:
+ * export { reportTypeReportJson as myFeatureFlag } from "./myFeatureFlag";
+ *
+ * If there are no feature flags, use: export default {};
+ */
+
 // commented out export is intentional to preserve the flag for future use when the feature is ready to be enabled
-// export { mcparReportJson as newQualityMeasuresSectionEnabled } from "./newQualityMeasuresSectionEnabled/index";
-export { mcparReportJson as summer2026SansQm } from "./summer2026SansQm/index";
+// export { mcparReportJson as newQualityMeasuresSectionEnabled } from "./newQualityMeasuresSectionEnabled";
+export { mcparReportJson as summer2026SansQm } from "./summer2026SansQm";
--- services/app-api/forms/routes/mlr/flags/index.ts
+++ services/app-api/forms/routes/mlr/flags/index.ts
@@ -1,3 +1,11 @@
-// flagged routes
-// oxlint-disable-next-line require-module-specifiers
-export {};
+/** *
+ * If there are feature flags, export the report JSON as the flag name,
+ * matching how it is named in LaunchDarkly.
+ *
+ * Example:
+ * export { reportTypeReportJson as myFeatureFlag } from "./myFeatureFlag";
+ *
+ * If there are no feature flags, use: export default {};
+ */
+
+export default {};
