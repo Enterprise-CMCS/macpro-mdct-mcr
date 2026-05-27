b42feb091300f6e30d2f5459bbacf09c05b09996
Britt <britt.bujacich@gmail.com>
2026-05-27 10:01:52 -0600

    Cmdct 5945 - naaar updates for July (#12747)
    Co-authored-by: Pete Dunlap <pete.dunlap@stratiform.digital>

---
+++ services/app-api/forms/routes/naaar/flags/summer2026SansQm/state-and-program-information/index.ts
@@ -0,0 +1,16 @@
+import { ParentRoute } from "../../../../../../utils/types";
+import { analysisMethodsRoute } from "../../../state-and-program-information/analysis-methods";
+import { addPlansRoute } from "./add-plans";
+import { providerTypeCoverageRoute } from "../../../state-and-program-information/provider-type-coverage";
+import { stateInformationAndReportingScenarioRoute } from "./state-information-and-reporting-scenario";
+
+export const stateAndProgramInformationRoute: ParentRoute = {
+  name: "I. State and program information",
+  path: "/naaar/state-and-program-information",
+  children: [
+    stateInformationAndReportingScenarioRoute,
+    addPlansRoute,
+    analysisMethodsRoute,
+    providerTypeCoverageRoute,
+  ],
+};
