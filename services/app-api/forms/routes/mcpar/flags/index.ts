af46263a904babc2e25b0111a1937741ece34ff0
Karla M. Valcárcel Martínez <karla.valcarcel-martinez@coforma.io>
2026-06-10 15:54:36 -0400

    enable new qm

--- services/app-api/forms/routes/mcpar/flags/index.ts
+++ services/app-api/forms/routes/mcpar/flags/index.ts
@@ -9,5 +9,5 @@
  */
 
 // commented out export is intentional to preserve the flag for future use when the feature is ready to be enabled
-// export { mcparReportJson as newQualityMeasuresSectionEnabled } from "./newQualityMeasuresSectionEnabled";
-export { mcparReportJson as summer2026SansQm } from "./summer2026SansQm";
+export { mcparReportJson as newQualityMeasuresSectionEnabled } from "./newQualityMeasuresSectionEnabled";
+//export { mcparReportJson as summer2026SansQm } from "./summer2026SansQm";
