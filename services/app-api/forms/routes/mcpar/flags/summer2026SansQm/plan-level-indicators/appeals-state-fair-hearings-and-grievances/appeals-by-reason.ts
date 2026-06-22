31a4c5be59f3a1e698716589450ba1680107e1b5
Karla M. Valcárcel Martínez <karla.valcarcel-martinez@coforma.io>
2026-06-22 14:29:49 -0400

    update appeals content and order of options

--- services/app-api/forms/routes/mcpar/flags/summer2026SansQm/plan-level-indicators/appeals-state-fair-hearings-and-grievances/appeals-by-reason.ts
+++ services/app-api/forms/routes/mcpar/flags/summer2026SansQm/plan-level-indicators/appeals-state-fair-hearings-and-grievances/appeals-by-reason.ts
@@ -76,16 +76,16 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.QUESTION,
         props: {
           content:
-            "D1.IV.6a Resolved appeals related to denial of authorization or limited authorization of a service",
+            "D1.IV.6a Outcome of resolved appeals related to denial of authorization or limited authorization of a service",
           hint: "Enter the total number of appeals resolved by the plan during the reporting year that were related to the plan's denial of authorization for a service not yet rendered or limited authorization of a service. (Appeals related to denial of payment for a service already rendered should be counted in indicator D1.IV.6c).",
         },
       },
       {
-        id: "plan_resolvedPreServiceAuthorizationDenialAppealsFavorable",
+        id: "plan_resolvedPreServiceAuthorizationDenialAppealsDenied",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Favorable to the enrollee",
+          label: "Appeals denied",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -95,17 +95,17 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Partially favorable to the enrollee",
+          label: "Appeals resolved in partial favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
       },
       {
-        id: "plan_resolvedPreServiceAuthorizationDenialAppealsDenied",
+        id: "plan_resolvedPreServiceAuthorizationDenialAppealsFavorable",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Denied",
+          label: "Appeals resolved in favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -115,16 +115,16 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.QUESTION,
         props: {
           content:
-            "D1.IV.6b Resolved appeals related to reduction, suspension, or termination of a previously authorized service",
+            "D1.IV.6b Outcome of resolved appeals related to reduction, suspension, or termination of a previously authorized service",
           hint: "Enter the total number of appeals resolved by the plan during the reporting year that were related to the plan's reduction, suspension, or termination of a previously authorized service.",
         },
       },
       {
-        id: "plan_resolvedReductionSuspensionTerminationOfPreviouslyAuthorizedServiceAppealsFavorable",
+        id: "plan_resolvedReductionSuspensionTerminationOfPreviouslyAuthorizedServiceAppealsDenied",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Favorable to the enrollee",
+          label: "Appeals denied",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -134,17 +134,17 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Partially favorable to the enrollee",
+          label: "Appeals resolved in partial favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
       },
       {
-        id: "plan_resolvedReductionSuspensionTerminationOfPreviouslyAuthorizedServiceAppealsDenied",
+        id: "plan_resolvedReductionSuspensionTerminationOfPreviouslyAuthorizedServiceAppealsFavorable",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Denied",
+          label: "Appeals resolved in favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -153,16 +153,17 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         id: "plan_resolvedPostServiceAuthorizationDenialAppeals",
         type: ReportFormFieldType.QUESTION,
         props: {
-          content: "D1.IV.6c Resolved appeals related to payment denial",
+          content:
+            "D1.IV.6c Outcome of resolved appeals related to payment denial",
           hint: "Enter the total number of appeals resolved by the plan during the reporting year that were related to the plan's denial, in whole or in part, of payment for a service that was already rendered.",
         },
       },
       {
-        id: "plan_resolvedPostServiceAuthorizationDenialAppealsFavorable",
+        id: "plan_resolvedPostServiceAuthorizationDenialAppealsDenied",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Favorable to the enrollee",
+          label: "Appeals denied",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -172,17 +173,17 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Partially favorable to the enrollee",
+          label: "Appeals resolved in partial favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
       },
       {
-        id: "plan_resolvedPostServiceAuthorizationDenialAppealsDenied",
+        id: "plan_resolvedPostServiceAuthorizationDenialAppealsFavorable",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Denied",
+          label: "Appeals resolved in favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -191,16 +192,17 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         id: "plan_resolvedServiceTimelinessAppeals",
         type: ReportFormFieldType.QUESTION,
         props: {
-          content: "D1.IV.6d Resolved appeals related to service timeliness",
+          content:
+            "D1.IV.6d Outcome of resolved appeals related to service timeliness",
           hint: "Enter the total number of appeals resolved by the plan during the reporting year that were related to the plan's failure to provide services in a timely manner (as defined by the state).",
         },
       },
       {
-        id: "plan_resolvedServiceTimelinessAppealsFavorable",
+        id: "plan_resolvedServiceTimelinessAppealsDenied",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Favorable to the enrollee",
+          label: "Appeals denied",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -210,17 +212,17 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Partially favorable to the enrollee",
+          label: "Appeals resolved in partial favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
       },
       {
-        id: "plan_resolvedServiceTimelinessAppealsDenied",
+        id: "plan_resolvedServiceTimelinessAppealsFavorable",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Denied",
+          label: "Appeals resolved in favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -230,16 +232,16 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.QUESTION,
         props: {
           content:
-            "D1.IV.6e Resolved appeals related to lack of timely plan response to an appeal or grievance",
+            "D1.IV.6e Outcome of resolved appeals related to lack of timely plan response to an appeal or grievance",
           hint: "Enter the total number of appeals resolved by the plan during the reporting year that were related to the plan's failure to act within the timeframes provided at 42 CFR §438.408(b)(1) and (2) regarding the standard resolution of grievances and appeals.",
         },
       },
       {
-        id: "plan_resolvedUntimelyResponseAppealsFavorable",
+        id: "plan_resolvedUntimelyResponseAppealsDenied",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Favorable to the enrollee",
+          label: "Appeals denied",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -249,17 +251,17 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Partially favorable to the enrollee",
+          label: "Appeals resolved in partial favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
       },
       {
-        id: "plan_resolvedUntimelyResponseAppealsDenied",
+        id: "plan_resolvedUntimelyResponseAppealsFavorable",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Denied",
+          label: "Appeals resolved in favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -269,16 +271,16 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.QUESTION,
         props: {
           content:
-            "D1.IV.6f Resolved appeals related to plan denial of an enrollee's right to request out-of-network care",
+            "D1.IV.6f Outcome of resolved appeals related to plan denial of an enrollee's right to request out-of-network care",
           hint: "Enter the total number of appeals resolved by the plan during the reporting year that were related to the plan's denial of an enrollee's request to exercise their right, under 42 CFR §438.52(b)(2)(ii), to obtain services outside the network (only applicable to residents of rural areas with only one MCO). If not applicable, enter “N/A.”",
         },
       },
       {
-        id: "plan_resolvedRightToRequestOutOfNetworkCareDenialAppealsFavorable",
+        id: "plan_resolvedRightToRequestOutOfNetworkCareDenialAppealsDenied",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED_OR_NA_NR,
         validation: ValidationType.NUMBER_OR_SUPPRESSED_OR_NA_NR,
         props: {
-          label: "Favorable to the enrollee",
+          label: "Appeals denied",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -288,17 +290,17 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED_OR_NA_NR,
         validation: ValidationType.NUMBER_OR_SUPPRESSED_OR_NA_NR,
         props: {
-          label: "Partially favorable to the enrollee",
+          label: "Appeals resolved in partial favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
       },
       {
-        id: "plan_resolvedRightToRequestOutOfNetworkCareDenialAppealsDenied",
+        id: "plan_resolvedRightToRequestOutOfNetworkCareDenialAppealsFavorable",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED_OR_NA_NR,
         validation: ValidationType.NUMBER_OR_SUPPRESSED_OR_NA_NR,
         props: {
-          label: "Denied",
+          label: "Appeals resolved in favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -308,16 +310,16 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.QUESTION,
         props: {
           content:
-            "D1.IV.6g Resolved appeals related to denial of an enrollee's request to dispute financial liability",
+            "D1.IV.6g Outcome of resolved appeals related to denial of an enrollee's request to dispute financial liability",
           hint: "Enter the total number of appeals resolved by the plan during the reporting year that were related to the plan's denial of an enrollee's request to dispute a financial liability.",
         },
       },
       {
-        id: "plan_resolvedRequestToDisputeFinancialLiabilityDenialAppealsFavorable",
+        id: "plan_resolvedRequestToDisputeFinancialLiabilityDenialAppealsDenied",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Favorable to the enrollee",
+          label: "Appeals denied",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
@@ -327,17 +329,17 @@ export const appealsByReasonRoute: DrawerFormRoute = {
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Partially favorable to the enrollee",
+          label: "Appeals resolved in partial favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
       },
       {
-        id: "plan_resolvedRequestToDisputeFinancialLiabilityDenialAppealsDenied",
+        id: "plan_resolvedRequestToDisputeFinancialLiabilityDenialAppealsFavorable",
         type: ReportFormFieldType.NUMBER_OR_SUPPRESSED,
         validation: ValidationType.NUMBER_OR_SUPPRESSED,
         props: {
-          label: "Denied",
+          label: "Appeals resolved in favor of enrollee",
           mask: "comma-separated",
           decimalPlacesToRoundTo: 0,
         },
