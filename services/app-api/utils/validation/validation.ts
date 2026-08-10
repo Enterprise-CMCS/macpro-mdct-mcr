daeb7964618c950289dc032b8e55b0637ccf92cf
Karla M. Valcárcel Martínez <karla.valcarcel-martinez@coforma.io>
2026-08-10 10:52:03 -0400

    update validiations for nested schema

--- services/app-api/utils/validation/validation.ts
+++ services/app-api/utils/validation/validation.ts
@@ -123,7 +123,8 @@ export const mapValidationTypesToSchema = (fieldValidationTypes: AnyObject) => {
 
 // return created nested field schema
 export const makeNestedFieldSchema = (fieldValidationObject: AnyObject) => {
-  const { type, parentFieldName, parentOptionId } = fieldValidationObject;
+  const { options, type, parentFieldName, parentOptionId } =
+    fieldValidationObject;
   const getSchema = dependentSchemas[type];
   if (getSchema) {
     return nested(
@@ -132,7 +133,10 @@ export const makeNestedFieldSchema = (fieldValidationObject: AnyObject) => {
       parentOptionId
     );
   } else {
-    const fieldValidationSchema = schemaMap[type];
+    const correspondingSchema = schemaMap[type];
+    const fieldValidationSchema = options
+      ? correspondingSchema(options)
+      : correspondingSchema;
     return nested(() => fieldValidationSchema, parentFieldName, parentOptionId);
   }
 };
--- services/ui-src/src/utils/validation/validation.ts
+++ services/ui-src/src/utils/validation/validation.ts
@@ -95,7 +95,8 @@ export const mapValidationTypesToSchema = (fieldValidationTypes: AnyObject) => {
 
 // return created nested field schema
 export const makeNestedFieldSchema = (fieldValidationObject: AnyObject) => {
-  const { type, parentFieldName, parentOptionId } = fieldValidationObject;
+  const { options, type, parentFieldName, parentOptionId } =
+    fieldValidationObject;
   const getSchema = dependentSchemas[type];
   if (getSchema) {
     return nested(
@@ -104,7 +105,10 @@ export const makeNestedFieldSchema = (fieldValidationObject: AnyObject) => {
       parentOptionId
     );
   } else {
-    const fieldValidationSchema = schemaMap[type];
+    const correspondingSchema = schemaMap[type];
+    const fieldValidationSchema = options
+      ? correspondingSchema(options)
+      : correspondingSchema;
     return nested(() => fieldValidationSchema, parentFieldName, parentOptionId);
   }
 };
