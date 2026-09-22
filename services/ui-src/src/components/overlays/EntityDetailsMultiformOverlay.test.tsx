771f29b438c483833156b26461fe2c603f5984b6
Karla Valcárcel Martínez <99458559+karla-vm@users.noreply.github.com>
2026-09-22 10:45:22 -0400

    CMDCT-6729: Add accessible name for "Plan compliance" action buttons (NAAAR) (#12921)

--- services/ui-src/src/components/overlays/EntityDetailsMultiformOverlay.test.tsx
+++ services/ui-src/src/components/overlays/EntityDetailsMultiformOverlay.test.tsx
@@ -95,7 +95,7 @@ async function setupChildTableFormTest(
   // Click Enter
   const updatedEnterButton = within(updatedEntityCellsIncomplete).getByRole(
     "button",
-    { name: "Enter" }
+    { name: "Enter mock aria label" }
   );
   await act(async () => {
     await userEvent.click(updatedEnterButton);
@@ -164,9 +164,8 @@ describe("<EntityDetailsMultiformOverlay />", () => {
           name: `Mock Cell${formId} Enter`,
         });
         const enterButton = within(entityCells).getByRole("button", {
-          name: "Enter",
+          name: "Enter mock aria label",
         });
-
         expect(entityTable).toBeVisible();
         expect(entityHeaders).toBeVisible();
         expect(entityCells).toBeVisible();
@@ -196,7 +195,7 @@ describe("<EntityDetailsMultiformOverlay />", () => {
     const updatedEnterButton = within(updatedEntityCellsIncomplete).getByRole(
       "button",
       {
-        name: "Enter",
+        name: "Enter mock aria label",
       }
     );
     await act(async () => {
@@ -309,7 +308,7 @@ describe("<EntityDetailsMultiformOverlay />", () => {
     const updatedEnterButton = within(entityCellsIncomplete).getByRole(
       "button",
       {
-        name: "Enter",
+        name: "Enter mock aria label",
       }
     );
     await act(async () => {
